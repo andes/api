e6b9465fa1f7a6fbaa5c7fd58ebee067653acc9b
Luis Parada <luisparadawagner@gmail.com>
2016-09-02 14:24:19 -0300

    Se crean métodos CRUD en profesional

--- routes/profesional.ts
+++ routes/profesional.ts
@@ -3,13 +3,53 @@ import * as profesional from '../schemas/profesional'
 
 var router = express.Router();
 
-    router.get('/profesional', function (req, res, next) {
-       profesional.find({},(err, data) => {
-           if (err) {
-               next(err);
-           };
-           res.json(data);
-       });
+router.get('/profesional', function (req, res, next) {
+    profesional.find({}, (err, data) => {
+        if (err) {
+            next(err);
+        };
+
+        res.json(data);
     });
+});
+
+router.get('/profesional/:_id', function (req, res, next) {
+    profesional.findById(req.params._id, function(err, data)  {
+        if (err) {
+            next(err);
+        };
+
+        res.json(data);
+    });
+});
+
+router.post('/profesional', function (req, res, next) {
+    var newProfesional = new profesional(req.body);
+    newProfesional.save((err) => {
+        if (err) {
+            next(err);
+        }
+        
+        res.json(newProfesional);
+    });
+});
+
+router.put('/profesional/:_id', function (req, res, next) {
+    profesional.findByIdAndUpdate(req.params._id, req.body, function (err, data) {
+        if (err)
+            return next(err);
+
+        res.json(data);
+    });
+});
+
+router.delete('/profesional/:_id', function (req, res, next) {
+    profesional.findByIdAndRemove(req.params._id, req.body, function (err, data) {
+        if (err)
+            return next(err);
+
+        res.json(data);
+    });
+})
 
 export = router;
