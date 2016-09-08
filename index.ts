24393da47eeee4f60f174b987a5e0204567e366a
Natalia Huenchumán <natalia.huenchuman@gmail.com>
2016-09-08 08:53:14 -0300

    Se agregan headers de acceso

--- index.ts
+++ index.ts
@@ -14,10 +14,20 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    if ('OPTIONS' == req.method){
+        return res.send(200);
+    }
+    next();
+});
+
 var routes = requireDir('./routes/');
 for (var route in routes)
     app.use('/api', routes[route]);
 
-var server = app.listen(3000, function () {
+var server = app.listen(3002, function () {
     console.log('Inicio web Server local http://127.0.0.1:3000/');
 });
