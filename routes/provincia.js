656328540ff33fefcdeb04f58fdf9049436b3437
kro <celeste.carolina.s@gmail.com>
2016-09-12 11:50:13 -0300

    Se agrega get por distintos campos en provincia.ts

--- routes/provincia.js
+++ routes/provincia.js
@@ -11,5 +11,28 @@ router.get('/provincia', function (req, res, next) {
         res.json(data);
     });
 });
+router.get('/provincia/:id*?', function (req, res, next) {
+    if (req.params.id) {
+        provincia.findById(req.params.id, function (err, data) {
+            if (err) {
+                next(err);
+            }
+            ;
+            res.json(data);
+        });
+    }
+    else {
+        var query;
+        query = provincia.find({});
+        if (req.query.nombre) {
+            query.where('nombre').equals(req.query.nombre);
+        }
+        query.exec(function (err, data) {
+            if (err)
+                return next(err);
+            res.json(data);
+        });
+    }
+});
 module.exports = router;
 //# sourceMappingURL=provincia.js.map
--- routes/provincia.ts
+++ routes/provincia.ts
@@ -12,4 +12,28 @@ router.get('/provincia', function (req, res, next) {
     });
 });
 
+router.get('/provincia/:id*?', function(req, res, next) {
+   if (req.params.id) {
+
+       provincia.findById(req.params.id, function (err, data) {
+       if (err) {
+           next(err);
+       };
+
+       res.json(data);
+   });
+   }
+   else{
+       var query;
+        query = provincia.find({});
+           if (req.query.nombre){
+               query.where('nombre').equals(req.query.nombre);
+          }
+           query.exec((err, data)=> {
+               if (err) return next(err);
+               res.json(data);
+           });
+   }
+});
+
 export = router;
