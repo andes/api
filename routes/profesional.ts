a9faef4bff15b5f47adc260d0215ba9aae1cef1d
hugo <hfernandez@neuquen.gov.ar>
2016-09-27 09:36:11 -0300

    update de api profesionales y metodos

---
+++ routes/profesional.ts
@@ -0,0 +1,98 @@
+import * as express from 'express'
+import * as profesional from '../schemas/profesional'
+import * as utils from '../utils/utils';
+
+var router = express.Router();
+
+router.get('/profesional/:_id*?', function(req, res, next) {
+    if (req.params.id) {
+        profesional.findById(req.params._id, function(err, data) {
+            if (err) {
+                next(err);
+            };
+
+            res.json(data);
+        });
+    } else {
+
+        var query;
+        var opciones={};
+
+        if (req.query.apellido) {
+             opciones = { 'apellido' : {'$regex': utils.makePattern(req.query.apellido)}};
+        }
+
+        if (req.query.fechaNacimiento){
+            opciones = {'fechaNacimiento': req.query.fechaNacimiento}
+        }
+
+        if (req.query)
+    }
+    
+    
+    query = profesional.find(opciones);
+
+// opciones = {
+            //     apellido: {
+            //         $regex: '/^' + req.query.apellido + '/i'
+            //     }
+            // }
+
+ // if (req.query.documento)
+        //     query.where('documento').equals(req.query.documento);
+        // if (req.query.apellido)
+        //       query.where('apellido').equals(req.query.apellido);
+        // if (req.query.fechaNacimiento)
+        //     query.where('fechaNacimiento').equals(req.query.fechaNacimiento);
+    //var opciones = {};
+    // if (req.query.search) {
+    // opciones['$or'] = [{
+
+    //             "name.text": {
+    //                 "$regex": utils.makePattern(req.query.search)
+    //             }
+    //         }, {
+    //             "document.value": {
+    //                 "$regex": utils.makePattern(req.query.search)
+    //             }
+    //         }]
+    // }
+    //query = profesional.find(opciones);
+
+    query.exec(function(err, data) {
+        if (err) return next(err);
+        res.json(data);
+    });
+
+});
+
+router.post('/profesional', function(req, res, next) {
+    var newProfesional = new profesional(req.body);
+    newProfesional.save((err) => {
+        if (err) {
+            next(err);
+        }
+
+        res.json(newProfesional);
+    });
+});
+
+router.put('/profesional/:_id', function(req, res, next) {
+    profesional.findByIdAndUpdate(req.params._id, req.body, function(err, data) {
+        if (err)
+            return next(err);
+
+        res.json(data);
+    });
+});
+
+router.delete('/profesional/:_id', function(req, res, next) {
+    profesional.findByIdAndRemove(req.params._id, req.body, function(err, data) {
+        if (err)
+            return next(err);
+
+        res.json(data);
+    });
+})
+
+export = router;
---
+++ schemas/profesional.js
@@ -0,0 +1,67 @@
+"use strict";
+var mongoose = require('mongoose');
+var ubicacionSchema = require('./ubicacion');
+var profesionalSchema = new mongoose.Schema({
+    documento: String,
+    activo: Boolean,
+    nombre: String,
+    apellido: String,
+    contacto: [{
+            tipo: {
+                type: String,
+                enum: ["telefonoFijo", "telefonoCelular", "email"]
+            },
+            valor: String,
+            ranking: Number,
+            ultimaActualizacion: Date,
+            activo: Boolean
+        }],
+    sexo: {
+        type: String,
+        enum: ["femenino", "masculino", "otro"]
+    },
+    genero: {
+        type: String,
+        enum: ["femenino", "masculino", "otro"]
+    },
+    fechaNacimiento: Date,
+    fechaFallecimiento: Date,
+    direccion: [{
+            valor: String,
+            codigoPostal: String,
+            ubicacion: ubicacionSchema,
+            ranking: Number,
+            geoReferencia: {
+                type: [Number],
+                index: '2d' // create the geospatial index
+            },
+            ultimaActualizacion: Date,
+            activo: Boolean
+        }],
+    estadoCivil: {
+        type: String,
+        enum: ["casado", "separado", "divorciado", "viudo", "soltero", "otro"]
+    },
+    foto: String,
+    rol: String,
+    especialidad: [{
+            id: mongoose.Schema.Types.ObjectId,
+            nombre: String
+        }],
+    matriculas: [{
+            numero: Number,
+            descripcion: String,
+            activo: Boolean,
+            periodo: {
+                inicio: Date,
+                end: Date
+            },
+        }],
+});
+//Defino Virtuals
+profesionalSchema.virtual('nombreCompleto').get(function () {
+    return this.nombre + ' ' + this.apellido;
+});
+var profesional = mongoose.model('profesional', profesionalSchema, 'profesional');
+module.exports = profesional;
+//# sourceMappingURL=profesional.js.map
