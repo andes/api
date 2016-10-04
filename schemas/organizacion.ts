52ad5ee5b5e7d5c37f829f4b8f30c9c316ee0317
kro <celeste.carolina.s@gmail.com>
2016-10-04 13:45:43 -0300

    Se cambia el tipo de codigo SISA a String

--- schemas/organizacion.ts
+++ schemas/organizacion.ts
@@ -4,7 +4,7 @@ import * as ubicacionSchema from './ubicacion';
 var organizacionSchema = new mongoose.Schema({
     codigo:{
         sisa: {
-            type: Number,
+            type: String,
             required: true
         },
         cuie: String,
