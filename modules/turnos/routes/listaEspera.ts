575c41830cbad5cd39d9657ffc82d8ce6efea611
NicoDino <nicodinolfo@gmail.com>
2017-02-07 12:16:18 -0300

    listaEspera modifico GET

--- modules/turnos/routes/listaEspera.ts
+++ modules/turnos/routes/listaEspera.ts
@@ -1,52 +1,14 @@
 import * as express from 'express'
-import * as listaespera from '../schemas/listaEspera'
+import * as listaEspera from '../schemas/listaEspera'
 import * as utils from '../../../utils/utils';
+import { defaultLimit, maxLimit } from './../../../config';
+import * as config from '../../../config';
 
 var router = express.Router();
 
-router.get('/listaespera/:id*?', function (req, res, next) {
+/*router.get('/listaEspera/:id*?', function (req, res, next) {
     if (req.params.id) {
-        listaespera.findById(req.params.id, function (err, data) {
-            if (err) {
-                next(err);
-            };
-
-            res.json(data);
-        });
-    } else {
-        var query;
-        query = listaespera.find({}); //Trae todos
-
-        if (req.query.fecha) {
-            query.where('fecha').gte(req.query.fecha);
-        }
-
-        if (req.query.fecha) {
-           query.where('vencimiento').lte(req.query.fecha);
-        }
-
-        if (req.query.paciente) {
-            query.where('paciente.id').equals(req.query.paciente);
-        }
-
-        if (req.query.profesional) {
-            query.where('profesional.id').equals(req.query.profesional);
-        }
-
-        if (req.query.prestacion) {
-            query.where('prestacion.id').equals(req.query.prestacion);
-        }
-
-        if (req.query.estado) {
-            query.where('estado').equals(req.query.estado);
-        }
-
-        if (!Object.keys(query).length) {
-            res.status(400).send("Debe ingresar al menos un parámetro");
-            return next(400);
-        }
-
-        query = listaespera.find(query).sort({
+        listaEspera.find(query).sort({
             estado: 1,
             fecha: 1
         });
@@ -57,11 +19,51 @@ router.get('/listaespera/:id*?', function (req, res, next) {
             res.json(data);
         });
     }
+});*/
+
+router.get('/listaEspera/:id*?', function (req, res, next) {
+    if (req.params.id) {
+        listaEspera.findById(req.params._id, function (err, data) {
+            if (err) {
+                next(err);
+            };
+            //console.log(data);
+            res.json(data);
+        });
+    } else {
+
+        var query;
+        var opciones = {};
+
+        /*if (req.query.paciente.nombre) {
+            opciones['paciente.nombre'] = {
+                '$regex': utils.makePattern(req.query.paciente.nombre)
+            };
+        }
+
+        if (req.query.paciente.apellido) {
+            opciones['paciente.apellido'] = {
+                '$regex': utils.makePattern(req.query.paciente.apellido)
+            };
+        }*/
+
+        if (req.query.documento) {
+            opciones['paciente.documento'] = utils.makePattern(req.query.documento)
+        }
+
+    }
+    let skip: number = parseInt(req.query.skip || 0);
+    let limit: number = Math.min(parseInt(req.query.limit || defaultLimit), maxLimit);
+    query = listaEspera.find(opciones).skip(skip).limit(limit);
+    query.exec(function (err, data) {
+        if (err) return next(err);
+        res.json(data);
+    });
+
 });
 
-
-router.post('/listaespera', function (req, res, next) {
-    var newItem = new listaespera(req.body);
+router.post('/listaEspera', function (req, res, next) {
+    var newItem = new listaEspera(req.body);
     newItem.save((err) => {
         if (err) {
             return next(err);
@@ -70,8 +72,8 @@ router.post('/listaespera', function (req, res, next) {
     })
 });
 
-router.put('/listaespera/:_id', function (req, res, next) {
-    listaespera.findByIdAndUpdate(req.params._id, req.body, { new: true }, function (err, data) {
+router.put('/listaEspera/:_id', function (req, res, next) {
+    listaEspera.findByIdAndUpdate(req.params._id, req.body, { new: true }, function (err, data) {
         if (err) {
             return next(err);
         }
@@ -79,8 +81,8 @@ router.put('/listaespera/:_id', function (req, res, next) {
     });
 });
 
-router.delete('/listaespera/:_id', function (req, res, next) {
-    listaespera.findByIdAndRemove(req.params._id, req.body, function (err, data) {
+router.delete('/listaEspera/:_id', function (req, res, next) {
+    listaEspera.findByIdAndRemove(req.params._id, req.body, function (err, data) {
         if (err)
             return next(err);
 
