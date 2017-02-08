db50ff626acc421a6f13ce25b00589abc7909b04
hugo <hfernandez@neuquen.gov.ar>
2017-02-08 11:15:13 -0300

    actualizo consultas matching auditoria

--- core/mpi/routes/auditoria/matching.ts
+++ core/mpi/routes/auditoria/matching.ts
@@ -1,11 +1,11 @@
 import * as express from 'express'
-import * as matching from '../../schemas/matching'
+import { paciente } from '../../schemas/paciente'
 
 var router = express.Router();
 
 router.get('/matching/:id*?', function (req, res, next) {
     if (req.params.id) {
-        matching.findById(req.params.id, function (err, data) {
+        paciente.findById(req.params.id, function (err, data) {
             if (err) {
                 next(err);
             };
@@ -14,10 +14,7 @@ router.get('/matching/:id*?', function (req, res, next) {
         });
     } else {
         var query;
-        query = matching.find({}).limit(50); //Trae todos 
-        // if (req.query.nombre) {
-        //     query.where('nombre').equals(RegExp('^.*' + req.query.nombre + '.*$', "i"));
-        // }
+        query = paciente.find({ matchSisa: { $gt: 0.7,  $lte: 0.99 }, estado:'temporal' });
         query.exec((err, data) => {
             if (err) return next(err);
             res.json(data);
@@ -25,32 +22,32 @@ router.get('/matching/:id*?', function (req, res, next) {
     }
 });
 
-router.post('/matching', function (req, res, next) {
-    var newMatching = new matching(req.body)
-    newMatching.save((err) => {
-        if (err) {
-            return next(err);
-        }
-        res.json(newMatching);
-    })
-});
+// router.post('/matching', function (req, res, next) {
+//     var newMatching = new paciente(req.body)
+//     newMatching.save((err) => {
+//         if (err) {
+//             return next(err);
+//         }
+//         res.json(newMatching);
+//     })
+// });
 
-router.put('/matching/:id', function (req, res, next) {
-    matching.findByIdAndUpdate(req.params.id, req.body, {new:true}, function (err, data) {
-        if (err) {
-            return next(err);
-        }
-        res.json(data);
-    });
-});
+// router.put('/matching/:id', function (req, res, next) {
+//     matching.findByIdAndUpdate(req.params.id, req.body, {new:true}, function (err, data) {
+//         if (err) {
+//             return next(err);
+//         }
+//         res.json(data);
+//     });
+// });
 
-router.delete('/matching/:_id', function (req, res, next) {
-    matching.findByIdAndRemove(req.params._id, function (err, data) {
-        if (err)
-            return next(err);
+// router.delete('/matching/:_id', function (req, res, next) {
+//     matching.findByIdAndRemove(req.params._id, function (err, data) {
+//         if (err)
+//             return next(err);
 
-        res.json(data);
-    });
-})
+//         res.json(data);
+//     });
+// })
 
 export = router;
