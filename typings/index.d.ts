d4d536822c8a94c35a76a7d6eda524e776adc217
Juan G <Juan G>
2017-03-05 17:23:05 -0300

    implementación de auth

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/body-parser/index.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/express-serve-static-core/index.d.ts" />
-/// <reference path="globals/express/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/mime/index.d.ts" />
-/// <reference path="modules/mongoose/index.d.ts" />
