e7ec3c70356f4e9a5e12414067f05d7c60110cfb
hugo <hfernandez@neuquen.gov.ar>
2017-06-09 13:46:37 -0300

    login cache

--- auth/schemas/permisos.ts
+++ auth/schemas/permisos.ts
@@ -2,6 +2,9 @@ import * as mongoose from 'mongoose';
 
 let schema = new mongoose.Schema({
     usuario: Number,
+    nombre: String,
+    apellido: String,
+    password: String,
     organizacion: mongoose.Schema.Types.ObjectId,
     roles: [String],
     permisos: [String]
