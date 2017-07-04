a4c27b99a52094828823bc4b2962d0c7882803ae
Mariano Botta <marianoabotta@gmail.com>
2017-07-04 09:54:08 -0300

    Split Agenda routes and controller

--- modules/turnosmobile/routes/pacienteApp.ts
+++ modules/turnosmobile/routes/pacienteApp.ts
@@ -4,6 +4,8 @@ import * as mongoose from 'mongoose';
 import * as moment from 'moment';
 import * as agenda from '../../turnos/schemas/agenda';
 import { paciente } from '../../../core/mpi/schemas/paciente';
+import * as agendaCtrl from '../../turnos/controller/agenda';
+
 
 let router = express.Router();
 
@@ -12,7 +14,6 @@ router.get('/turnos', function (req: any, res, next) {
     let pipelineTurno = [];
     let turnos = [];
     let turno;
-
     let matchTurno = {};
 
     matchTurno['bloques.turnos.paciente.id'] = mongoose.Types.ObjectId(req.user.idPaciente);
@@ -52,6 +53,7 @@ router.get('/turnos', function (req: any, res, next) {
     pipelineTurno.push({
         '$group': {
             '_id': { 'id': '$_id', 'bloqueId': '$bloques._id' },
+            'agenda_id': { $first: '$_id' },
             'turnos': { $push: '$bloques.turnos' },
             'profesionales': { $first: '$profesionales' },
             'espacioFisico': { $first: '$espacioFisico' },
@@ -61,6 +63,7 @@ router.get('/turnos', function (req: any, res, next) {
     pipelineTurno.push({
         '$group': {
             '_id': '$_id.id',
+            'agenda_id': { $first: '$agenda_id' },
             'bloques': { $push: { '_id': '$_id.bloqueId', 'turnos': '$turnos' } },
             'profesionales': { $first: '$profesionales' },
             'espacioFisico': { $first: '$espacioFisico' },
@@ -84,6 +87,7 @@ router.get('/turnos', function (req: any, res, next) {
                 turno.profesionales = elem.profesionales;
                 turno.organizacion = elem.organizacion;
                 turno.espacioFisico = elem.espacioFisico;
+                turno.agenda_id = elem.agenda_id;
                 turnos.push(turno);
 
                 /*
@@ -99,6 +103,40 @@ router.get('/turnos', function (req: any, res, next) {
             res.json(turnos);
         }
     );
+
+});
+
+router.post('/turnos/cancelar', function (req: any, res, next) {
+    let pacienteId = req.user.idPaciente;
+    let turnoId = req.body.turno_id;
+    let agendaId = req.body.agenda_id;
+
+    if (!mongoose.Types.ObjectId.isValid(agendaId)) {
+        return next('ObjectID Inválido');
+    }
+
+    agenda.findById(agendaId, function (err, agendaObj) {
+        if (err) {
+            return next(err);
+        }
+
+        let turno = agendaCtrl.getTurno(req, agendaObj, turnoId);
+        if (turno) {
+            console.log(turno);
+            if (turno.paciente.id === pacienteId) {
+                agendaCtrl.liberarTurno(req, agendaObj, turnoId);
+                return res.json({});
+            } else {
+                return res.status(422).send({ message: 'unauthorized' });
+            }
+        } else {
+            return res.status(422).send({ message: 'turno_id_invalid' });
+        }
+
+
+
+    });
+
 
 });
 
