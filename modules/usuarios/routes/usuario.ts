d93a94d78311e432d675c593f24b0294f25176f1
NicoDino <nicodinolfo@gmail.com>
2017-07-06 13:52:12 -0300

    consulta ldap desde api usuarios

--- modules/usuarios/routes/usuario.ts
+++ modules/usuarios/routes/usuario.ts
@@ -37,33 +37,39 @@ router.get('/ldap/:id', function (req, res, next) {
 
         } else {
             // Conecta a LDAP
-            let dn = 'uid=' + req.body.usuario + ',' + configPrivate.auth.ldapOU;
+            let dn = 'uid=' + '35864378' + ',' + configPrivate.auth.ldapOU;
             let ldap = ldapjs.createClient({
                 url: `ldap://${configPrivate.hosts.ldap}`
             });
             ldap.bind(dn, 'lxia1866', function (err) {
                 if (err) {
-                    console.log(err);
+                    console.log("ERROR--------------> ", err);
                     return next(ldapjs.InvalidCredentialsError ? 403 : err);
                 }
                 // Busca el usuario con el UID correcto.
                 ldap.search(dn, {
                     scope: 'sub',
-                    filter: '(uid=' + req.body.usuario + ')',
+                    filter: '(uid=' + '27381849' + ')',
                     paged: false,
                     sizeLimit: 1
                 }, function (err2, searchResult) {
+                    console.log("SEARCHRESULT------------------------>", searchResult);
                     if (err2) {
+                        console.log("ERR2----->", err2);
                         return next(err2);
                     }
+                    res.json({
+                        searchResult
+                    });
                     searchResult.on('searchEntry', function (entry) {
-                        console.log("ENTRY", entry);
+                        // console.log("ENTRY", entry);
                     });
                     searchResult.on('error', function (err3) {
                         return next(err3);
                     });
                 });
             });
+
         }
     });
 });
