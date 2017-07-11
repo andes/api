22b7511d4204d2b978bd90a9f1c9fb1f0497af4f
hugo <hfernandez@neuquen.gov.ar>
2017-07-11 15:52:41 -0300

    lintnig usuarios y agrego pushnotifications

--- modules/usuarios/routes/usuario.ts
+++ modules/usuarios/routes/usuario.ts
@@ -1,22 +1,21 @@
 import * as express from 'express';
 import * as configPrivate from '../../../config.private';
 import * as ldapjs from 'ldapjs';
-
-// import { Auth } from './../../../auth/auth.class';
+// Routes
 let router = express.Router();
 // Services
 import { Logger } from '../../../utils/logService';
 // Schemas
 import * as permisos from '../../../auth/schemas/permisos';
-import { Auth } from "../../../auth/auth.class";
-// import { log } from '../../log/schemas/log';
-
+// imports
+import { Auth } from '../../../auth/auth.class';
+// Constantes
 const isReachable = require('is-reachable');
 
 router.post('/alta', function (req, res, next) {
-    // if (!Auth.check(req, 'usuarios:post')) {
-    //     return next(403);
-    // }
+    if (!Auth.check(req, 'usuarios:post')) {
+        return next(403);
+    }
     let data = new permisos.model(req.body);
     data.save((err) => {
         if (err) {
@@ -31,9 +30,7 @@ router.post('/alta', function (req, res, next) {
         });
         res.json(data);
     });
-
 });
-
 router.put('/:id', function (req, res, next) {
     if (!Auth.check(req, 'usuarios:put')) {
         return next(403);
@@ -62,47 +59,35 @@ router.put('/:id', function (req, res, next) {
     }).catch((err) => {
         return next(err);
     });
-
-
 });
-
-// Simple mongodb query by ObjectId --> better performance
 router.get('/:id', function (req, res, next) {
     if (!Auth.check(req, 'usuarios:get:byId')) {
         return next(403);
     }
     permisos.model.findById(req.params.id).then((resultado: any) => {
         if (resultado) {
-            // Logger.log(req, 'mpi', 'query', {
-            //     mongoDB: resultado.paciente
-            // });
             res.json(resultado);
         }
     }).catch((err) => {
         return next(err);
     });
 });
-
 router.get('/local/:organizacion/:usuario', function (req, res, next) {
-    // if (!Auth.check(req, 'usuarios:get:byId:byOrganizacion')) {
-    //     return next(403);
-    // }
+    if (!Auth.check(req, 'usuarios:get:byId:byOrganizacion')) {
+        return next(403);
+    }
     let filtro = {
         usuario: req.params.usuario,
         organizacion: req.params.organizacion
     };
     permisos.model.find(filtro).then((resultado: any) => {
         if (resultado) {
-            // Logger.log(req, 'mpi', 'query', {
-            //     mongoDB: resultado.paciente
-            // });
             res.json(resultado);
         }
     }).catch((err) => {
         return next(err);
     });
 });
-
 router.get('/ldap/:id', function (req, res, next) {
     if (!Auth.check(req, 'usuarios:get:ldap')) {
         return next(403);
@@ -110,8 +95,7 @@ router.get('/ldap/:id', function (req, res, next) {
     let server = configPrivate.hosts.ldap + configPrivate.ports.ldapPort;
     isReachable(server).then(reachable => {
         if (!reachable) {
-            // console.log('LDAP no reachable');
-            return next('Error de Conexión');
+            return next('Error de Conexión con el servidor de LDAP');
         } else {
             let dn = 'uid=' + req.params.id + ',' + configPrivate.auth.ldapOU;
             let ldap = ldapjs.createClient({
@@ -121,7 +105,6 @@ router.get('/ldap/:id', function (req, res, next) {
                 if (err) {
                     return next(ldapjs.InvalidCredentialsError ? 403 : err);
                 }
-                // console.log('req.body.usuario: ' + req.params.id);
                 // Busca el usuario con el UID correcto.
                 ldap.search(dn, {
                     scope: 'sub',
@@ -130,38 +113,29 @@ router.get('/ldap/:id', function (req, res, next) {
                     sizeLimit: 1
                 }, function (err2, searchResult) {
                     if (err2) {
-                        // console.log('err2: ', err2);
                         return next(err2);
                     }
                     searchResult.on('searchEntry', function (entry) {
-                        // console.log('entry: ' + JSON.stringify(entry.object));
                         res.send(entry.object);
                     });
                     searchResult.on('error', function (err3) {
-                        // console.log('error: ' + err3.message);
                         return next('Usuario inexistente');
                     });
                 });
             });
-
         }
     });
 });
-
 router.get('', function (req, res, next) {
     if (!Auth.check(req, 'usuarios:get')) {
         return next(403);
     }
     permisos.model.find({}).then((resultado: any) => {
         if (resultado) {
-            // Logger.log(req, 'mpi', 'query', {
-            //     mongoDB: resultado.paciente
-            // });
             res.json(resultado);
         }
     }).catch((err) => {
         return next(err);
     });
 });
-
 export = router;
