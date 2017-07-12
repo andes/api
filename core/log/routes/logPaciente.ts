73c52b46e3bf653324cfa5b85ee7fc54d2a04e8d
nhuenchuman <natalia.huenchuman@gmail.com>
2017-07-12 10:10:07 -0300

    Se agrega log de Paciente para operaciones de turnos

---
+++ core/log/routes/logPaciente.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import { logPaciente } from '../schemas/logPaciente';
+
+let router = express.Router();
+
+router.get('/logPaciente:id*?', function(req, res, next){
+//     if (req.params.idPaciente){
+
+//     }
+//    logPaciente.findById()
+});
+
+
