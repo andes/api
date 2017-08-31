99e80e2e9b061721826f1c9b70d01e13f4437c03
kro <celeste.carolina.s@gmail.com>
2017-08-31 09:18:00 -0300

    Elimino campos vacíos del pacienteFHIR

--- fhir/patient/controller/parser.ts
+++ fhir/patient/controller/parser.ts
@@ -108,19 +108,21 @@ export function pacientesAFHIR(ids: any[]) {
                                 'family': data.apellido, // Family name (often called 'Surname')
                                 'given': data.nombre.split(' '), // Given names (not always 'first'). Includes middle names
                             }],
-                            'telecom': contactos,
                             'gender': genero, // male | female | other | unknown
                             'birthDate': data.fechaNacimiento,
                             'deceasedDateTime': data.fechaFallecimiento,
                             'address': direcciones, // Addresses for the individual
-                            'maritalStatus': {
-                                text: data.estadoCivil
-                            }, // Marital (civil) status of a patient
-                            'photo': [{
-                                data: data.foto
-                            }], // Image of the patient
                             'contact': relaciones
                         };
+                        if (data.estadoCivil){
+                            pacienteFHIR['maritalStatus'] = {text : data.estadoCivil};
+                        }
+                        if (data.foto){
+                            pacienteFHIR['photo'] = [{data: data.foto}];  // Image of the patient
+                        }
+                        if (contactos.length > 0){
+                            pacienteFHIR['telecom'] = contactos;  // Image of the patient
+                        }
                         pacientesFHIR.push(pacienteFHIR);
                     }
 
@@ -135,6 +137,135 @@ export function pacientesAFHIR(ids: any[]) {
             resolve(pacientesFHIR);
         });
 
+
+    });
+}
+
+export function FHIRapacientes(ids: any[]) {
+    return new Promise((resolve: any, reject: any) => {
+        let pacientesMPI = [];
+        let promises = [];
+
+        ids.forEach(function (id) {
+            promises.push(
+                pacienteMpi.findById(id)
+                .then((data: any) => {
+                    if (data) {
+                        /* Armar el recurso FHIR */
+                        let identificadores = data.documento ? [{assigner:'DU', value: data.documento}] : [];
+
+                        // Parsea contactos
+                        let contactos = data.contacto ? data.contacto.map(unContacto => {
+                            let cont = {
+                                resourceType: 'ContactPoint',
+                                value: unContacto.valor,
+                                rank: unContacto.ranking,
+                                // period ??
+                            }
+                            switch (unContacto.tipo) {
+                                case 'fijo':
+                                    {
+                                        cont['system'] = 'phone';
+                                    }
+                                    break;
+                                case 'celular':
+                                    {
+                                        cont['system'] = 'phone';
+                                    }
+                                    break;
+                                case 'email':
+                                    {
+                                        cont['system'] = 'email';
+                                    }
+                                    break;
+                            }
+    
+                            return cont;
+                        }) : [];
+
+                        // Parsea direcciones
+                        let direcciones = data.direcciones ? data.direcciones.map(unaDireccion => {
+                            let direc = {
+                                resourceType: 'Address',
+                                postalCode: unaDireccion.codigoPostal,
+                                line: [unaDireccion.valor],
+                                city: unaDireccion.ubicacion.localidad,
+                                state: unaDireccion.ubicacion.provincia,
+                                country: unaDireccion.ubicacion.pais,
+                            }
+                            return direc;
+                        }) : [];
+
+                        // Parsea relaciones
+                        let relaciones = data.relaciones ? data.relaciones.map(unaRelacion => {
+                            let relacion = {
+                                'relationship': [{
+                                    text: unaRelacion.relacion.nombre
+                                }], // The kind of relationship
+                                'name': {
+                                    'resourceType': 'HumanName',
+                                    'family': unaRelacion.apellido, // Family name (often called 'Surname')
+                                    'given': [unaRelacion.nombre], // Given names (not always 'first'). Includes middle names
+                                }
+                            }
+                            return relacion;
+                        }) : [];
+
+                        // enum: ['femenino', 'masculino', 'otro']
+                        let genero;
+                        switch (data.genero) {
+                            case 'femenino':
+                                {
+                                    genero = 'female';
+                                }
+                                break;
+                            case 'masculino':
+                                {
+                                    genero = 'male';
+                                }
+                                break;
+                            case 'otro':
+                                {
+                                    genero = 'other';
+                                }
+                                break;
+                        }
+
+                        let pacienteFHIR = {
+                            'resourceType': 'Patient',
+                            'identifier': identificadores,
+                            'active': data.activo, // Whether this patient's record is in active use
+                            'name': [{
+                                'resourceType': 'HumanName',
+                                'family': data.apellido, // Family name (often called 'Surname')
+                                'given': data.nombre.split(' '), // Given names (not always 'first'). Includes middle names
+                            }],
+                            'telecom': contactos,
+                            'gender': genero, // male | female | other | unknown
+                            'birthDate': data.fechaNacimiento,
+                            'deceasedDateTime': data.fechaFallecimiento,
+                            'address': direcciones, // Addresses for the individual
+                            'maritalStatus': {
+                                text: data.estadoCivil
+                            }, // Marital (civil) status of a patient
+                            'photo': [{
+                                data: data.foto
+                            }], // Image of the patient
+                            'contact': relaciones
+                        };
+                        pacientesMPI.push(pacienteFHIR);
+                    }
+
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+            );
+        });
+
+        Promise.all(promises).then(() => {
+            resolve(pacientesMPI);
+        });
 
     });
 }
