32e25190dcae3e68979fe0f49a094b989f958439
Manuel Urbano <manurbano.dev@gmail.com>
2017-09-01 09:10:42 -0300

    Agregar validación de vacunas por paciente que devuelvan un porcentaje de matching de 90% o más

--- modules/mobileApp/routes/vacunas.ts
+++ modules/mobileApp/routes/vacunas.ts
@@ -1,22 +1,82 @@
-import { vacunas } from '../schemas/vacunas';
 import * as mongoose from 'mongoose';
 import * as express from 'express';
 
+import { matching } from '@andes/match';
+import * as controller from './../../../core/mpi/controller/paciente';
+import { vacunas } from '../schemas/vacunas';
+
 let router = express.Router();
 
+// pesos para el matcheo
+const weights = {
+    identity: 0.3,
+    name: 0.2,
+    gender: 0.3,
+    birthDate: 0.2
+};
+
 router.get('/vacunas', function (req: any, res, next) {
-    let dni = req.query.dni;
+    let conditions = {};
 
-    const sort = {FechaAplicacion: -1};
+    const pacienteId = req.user.pacientes[0].id;
 
-    vacunas.find({ 'dni': dni }).sort(sort).exec( (err, data)  => {
-        if (err) {
-            return next(err);
+    // primero buscar paciente
+    controller.buscarPaciente(pacienteId).then(data => {
+        const pacienteMPI = data.paciente;
+        
+        // Filtramos por documento
+        if (req.query.documento) {
+            conditions['documento'] = req.query.documento;
         }
 
-        res.json(data);
-    });
+        const sort = {FechaAplicacion: -1};
 
+        // buscar vacunas
+        vacunas.find(conditions).sort(sort).exec( (err, resultados)  => {
+
+            resultados.forEach( (vacuna: any, index) => {
+
+                let pacienteVacuna = {
+                    nombre: vacuna.nombre,
+                    apellido: vacuna.apellido,
+                    documento: vacuna.documento,
+                    sexo: vacuna.sexo,
+                    fechaNacimiento: vacuna.fechaNacimiento
+                }
+
+                let match = new matching();
+                let resultadoMatching = match.matchPersonas(pacienteMPI, pacienteVacuna, weights, 'Levenshtein');
+
+                // no cumple con el numero del matching
+                if (resultadoMatching < 0.90) {
+                    resultados.splice(index, 1);
+                }
+            }); 
+
+            if (err) {
+                return next(err);
+            }
+
+            res.json(resultados);
+        });
+    }).catch(error => {
+        return next(error);
+    });
+});
+
+router.get('/vacunas/count', function (req: any, res, next) {
+    let dni = req.query.dni;
+      
+    // buscar vacunas
+    vacunas.find({ 'documento': dni }).count().exec( (err, count)  => {
+        if (err) {
+            return next(res);
+        }
+
+        res.json(count);
+        
+    });
+    
 });
 
 module.exports = router;
