0028df6422c1e8ee8de5c66b22cf9c889db6d5e1
Hugo <hfernandez@neuquen.gov.ar>
2017-11-08 16:24:05 -0300

    fix error codes

---
+++ fhir/patient/controller/errorCodes.ts
@@ -0,0 +1,10 @@
+const codes = {
+    status: {
+        sucess: '200',
+        badRequest: '400',
+        denied: '403',
+        notFound: '404',
+        error: '500'
+    },
+};
+export = codes;
