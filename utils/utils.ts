873fa9133ca4193f275a13d702f119fd40ee85fc
Mariano Botta <marianoabotta@gmail.com>
2018-01-03 14:03:20 -0300

    Listado de laboratorios para app mobile

--- utils/utils.ts
+++ utils/utils.ts
@@ -146,3 +146,52 @@ export function removeDiacritics(str: string): string {
     }
     return str;
 }
+
+
+/**
+ * XML To Json
+ */
+export function xmlToJson(xmlString) {
+
+    function _xmlToJson(xml) {
+
+        // Create the return object
+        let obj = {};
+
+        if (xml.nodeType === 1) { // element
+            // do attributes
+            if (xml.attributes.length > 0) {
+                obj['@attributes'] = {};
+                for (var j = 0; j < xml.attributes.length; j++) {
+                    var attribute = xml.attributes.item(j);
+                    obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
+                }
+            }
+        } else if (xml.nodeType === 3) { // text
+            obj = xml.nodeValue;
+        }
+
+        // do children
+        if (xml.hasChildNodes()) {
+            for (var i = 0; i < xml.childNodes.length; i++) {
+                var item = xml.childNodes.item(i);
+                var nodeName = item.nodeName;
+                if (typeof(obj[nodeName]) === 'undefined') {
+                    obj[nodeName] = _xmlToJson(item);
+                } else {
+                    if (typeof(obj[nodeName].push) === 'undefined') {
+                        var old = obj[nodeName];
+                        obj[nodeName] = [];
+                        obj[nodeName].push(old);
+                    }
+                    obj[nodeName].push(_xmlToJson(item));
+                }
+            }
+        }
+        return obj;
+    }
+
+    var DOMParser = require('xmldom').DOMParser;
+    var parser = new DOMParser();
+    return _xmlToJson(parser.parseFromString(xmlString, 'application/xml'));
+}
