e4ed82058a4df17b43f2eb73abb682f0affa6768
Mariano Botta <marianoabotta@gmail.com>
2018-02-15 13:26:15 -0300

    Comentarios

--- jobs/manual.ts
+++ jobs/manual.ts
@@ -1,3 +1,8 @@
+/** 
+ * Corre una Jobs de forma manual. Corre desde el root del proyecto.
+ * 
+ * node jobs/manual.js [Js File to run]
+*/
 let schedule = require('node-schedule');
 import { Connections } from './../connections';
 import { jobs } from '../config.private';
