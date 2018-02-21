382905e2079239faf9e1e14828c8e9638c49cc50
NicoDino <nicodinolfo@gmail.com>
2018-02-21 10:39:50 -0300

    comentarios agregados route arancelamiento

--- modules/arancelamiento/routes/arancelamiento.ts
+++ modules/arancelamiento/routes/arancelamiento.ts
@@ -24,12 +24,17 @@ router.get('/puco/:documento', async function (req, res, next) {
         }
     }
 });
-
+/**
+ * Obtiene los datos de la obra social asociada a un paciente
+ *
+ * @param {any} doc
+ * @returns
+ */
 async function getOs(doc) {
-    let osPac: any = await osPaciente.find({ documento: doc }).exec();
+    let osPac: any = await osPaciente.find({ documento: doc }).exec();  // obtiene el código de obra social asociado al paciente 
     if (osPac && osPac.length > 0) {
         let codigo: string = osPac[0].codigoPuco;
-        let result = await obraSocial.find({ codigoPuco: parseInt(codigo, 10) }).exec();
+        let result = await obraSocial.find({ codigoPuco: parseInt(codigo, 10) }).exec(); // obtiene la información de la obra social
         return result[0];
     } else {
         return null;
