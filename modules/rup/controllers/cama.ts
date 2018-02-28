58fd9008add9a1c8733bed5f5b70eb6f323a733d
silviroa <silviroa@gmail.com>
2018-02-28 08:40:09 -0300

    Agregar ruta para obtener ultima internacion de un paciente

--- modules/rup/controllers/cama.ts
+++ modules/rup/controllers/cama.ts
@@ -1,31 +1,20 @@
-
 import * as config from '../../../config';
 import * as configPrivate from '../../../config.private';
 import * as moment from 'moment';
 import * as mongoose from 'mongoose';
 import { model as cama } from '../../../core/tm/schemas/camas';
+import { toArray } from '../../../utils/utils';
 
-export async function buscarCamaInternacion(idInternacion, estado) {
-    // let query = cama.find({
-    //     $where: 'this.estados[this.estados.length - 1].estado ==  \"ocupada\" AND '
-    // });
+export function buscarCamaInternacion(idInternacion, estado) {
+    let query = cama.aggregate([
+        {
+            $project: {
+                ultimoEstado: { $arrayElemAt: ['$estados', -1] }, organizacion: 1, sector: 1,
+                habitacion: 1, nombre: 1, tipoCama: 1
+            }
+        },
+        { $match: { 'ultimoEstado.estado': estado, 'ultimoEstado.idInternacion': idInternacion } }
+    ]);
 
-
-    // let req = {
-    //     query: {
-    //         estado: 'asignado',
-    //         pacienteId: pacienteModified.id,
-    //         horaInicio: moment(new Date()).startOf('day').toDate() as any
-    //     }
-    // };
-    // let turnos: any = await turnosController.getTurno(req);
-    // if (turnos.length > 0) {
-    //     turnos.forEach(element => {
-    //         try {
-    //             agendaController.updatePaciente(pacienteModified, element);
-    //         } catch (error) {
-    //             return error;
-    //         }
-    //     });
-    // }
+    return toArray(query.cursor({}).exec());
 }
--- modules/rup/routes/internacion.ts
+++ modules/rup/routes/internacion.ts
@@ -2,52 +2,39 @@ import * as mongoose from 'mongoose';
 import * as express from 'express';
 import * as moment from 'moment';
 // import * as async from 'async';
+
+import * as internacionesController from './../controllers/internacion';
+import * as camasController from './../controllers/cama';
 import { Auth } from './../../../auth/auth.class';
 import { model as Prestacion } from '../schemas/prestacion';
 import { model as cama } from '../../../core/tm/schemas/camas';
 
 let router = express.Router();
 
-router.get('/internaciones/:idPaciente', function (req, res, next) {
-
-    let query;
-    if (req.query.estado) {
-        query = Prestacion.find({
-            $where: 'this.estados[this.estados.length - 1].tipo ==  \"' + req.query.estado + '\"'
+router.get('/internaciones/ultima/:idPaciente', function (req, res, next) {
+    // buscamos la ultima interncion del paciente
+    internacionesController.buscarUltimaInternacion(req.params.idPaciente, req.query.estado).then(
+        internacion => {
+            let salida = { ultimaInternacion: null, cama: null };
+            if (internacion && internacion.length > 0) {
+                let ultimaInternacion = internacion[0];
+                // Ahora buscamos si se encuentra asociada la internacion a una cama 
+                camasController.buscarCamaInternacion(mongoose.Types.ObjectId(ultimaInternacion.id), 'ocupada').then(
+                    camas => {
+                        salida = { ultimaInternacion: ultimaInternacion, cama: null };
+                        if (camas && camas.length > 0) {
+                            salida.cama = camas[0];
+                        }
+                        res.json(salida);
+                    }).catch(err => {
+                        return next(err);
+                    });
+            } else {
+                res.json(null);
+            }
+        }).catch(error => {
+            return next(error);
         });
-    } else {
-        query = Prestacion.find({}); // Trae todos
-    }
-
-    if (req.query.fechaDesde) {
-        query.where('ejecucion.fecha').gte(moment(req.query.fechaDesde).startOf('day').toDate() as any);
-    }
-    if (req.query.fechaHasta) {
-        query.where('ejecucion.fecha').lte(moment(req.query.fechaHasta).endOf('day').toDate() as any);
-    }
-    if (req.query.idProfesional) {
-        query.where('solicitud.profesional.id').equals(req.query.idProfesional);
-    }
-    if (req.params.idPaciente) {
-        query.where('paciente.id').equals(req.params.idPaciente);
-    }
-    if (req.query.organizacion) {
-        query.where('solicitud.organizacion.id').equals(req.query.organizacion);
-    }
-    // Ordenar por fecha de solicitud
-    if (req.query.ordenFecha) {
-        query.sort({ 'solicitud.fecha': -1 });
-    } else if (req.query.ordenFechaEjecucion) {
-        query.sort({ 'ejecucion.fecha': -1 });
-    }
-
-    if (req.query.limit) {
-        query.limit(parseInt(req.query.limit, 10));
-    }
-    query.then(internaciones => {
-        console.log(internaciones);
-        res.json(internaciones);
-    });
 });
 
 export = router;
