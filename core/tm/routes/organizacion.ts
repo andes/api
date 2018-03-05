9247dfd3f29f62d43ba7d6810da1d540f294fa9f
Andrrr <andresin@gmail.com>
2018-03-05 08:43:16 -0300

    Se elimina import de Cama Internación

--- core/tm/routes/organizacion.ts
+++ core/tm/routes/organizacion.ts
@@ -10,15 +10,9 @@ import { defaultLimit, maxLimit } from './../../../config';
 
 import * as configPrivate from '../../../config.private';
 import { Auth } from '../../../auth/auth.class';
-// import * as estadoCama from '../routes/camaEstado';
-// import {camaEstado} from './../schemas/camaEstado';
-import * as CamaEstadoModel from '../models/camaEstado';
 
 let GeoJSON = require('geojson');
 let router = express.Router();
-
-
-
 
 router.get('/organizaciones/georef/:id?', async function (req, res, next) {
     if (req.params.id) {
