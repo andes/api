c621dda6f35182661b55fb7199036e63da6f364d
nhuenchuman <natalia.huenchuman@gmail.com>
2018-03-13 10:33:32 -0300

    * cambios en patch de paciente mpi

--- jobs/integracionSipsJob.ts
+++ jobs/integracionSipsJob.ts
@@ -1,9 +1,7 @@
 import * as agendaCtrl from './../modules/turnos/controller/agendasCacheController';
-import * as agendaHPNCtrl from './../modules/turnos/controller/agendasHPNCacheController';
 
 function run() {
     agendaCtrl.integracionSips();
-    //agendaHPNCtrl.integracion();
 }
 
 export = run;
