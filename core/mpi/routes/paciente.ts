0b07b9e9bac1a6bf86c238da5be6192f3add5b7e
condorpiedra <natalia.huenchuman@gmail.com>
2018-03-13 10:52:36 -0300

    Integración: Separamos los jobs de integración (#233)

--- core/mpi/routes/paciente.ts
+++ core/mpi/routes/paciente.ts
@@ -226,8 +226,8 @@ router.get('/pacientes/dashboard/', async function (req, res, next) {
     ];
 
     result.paciente = await toArray(paciente.aggregate(estadoAggregate).cursor({}).exec());
-    result.pacienteMpi = await toArray(pacienteMpi.aggregate(estadoAggregate).cursor({batchSize: 1000}).exec());
-    result.logs = await toArray(log.aggregate(logAggregate).cursor({batchSize: 1000}).exec());
+    result.pacienteMpi = await toArray(pacienteMpi.aggregate(estadoAggregate).cursor({ batchSize: 1000 }).exec());
+    result.logs = await toArray(log.aggregate(logAggregate).cursor({ batchSize: 1000 }).exec());
     res.json(result);
 
     // paciente.aggregate(estadoAggregate).cursor({batchSize: 1000}).exec().on('data', function(doc) {
@@ -841,30 +841,35 @@ router.patch('/pacientes/mpi/:id', function (req, res, next) {
     if (!Auth.check(req, 'mpi:paciente:patchMpi')) {
         return next(403);
     }
-    controller.buscarPaciente(req.params.id).then((resultado: any) => {
+    controller.buscarPaciente(req.params.id).then((resultado) => {
         if (resultado) {
             switch (req.body.op) {
                 case 'updateCuil':
                     controller.updateCuil(req, resultado.paciente);
                     break;
+
             }
-            let pacienteMpi: any;
+            let pacMpi: any;
             if (resultado.db === 'mpi') {
-                pacienteMpi = resultado.paciente;
-                Auth.audit(pacienteMpi, req);
-                pacienteMpi.save(function (errPatch) {
+                pacMpi = new pacienteMpi(resultado.paciente);
+                Auth.audit(pacMpi, req);
+                pacMpi.save(function (errPatch) {
                     if (errPatch) {
                         return next(errPatch);
                     }
                     return res.json(pacienteMpi);
                 });
             } else {
-                return res.json(null);
+                return res.json({});
             }
+        } else {
+            return next(500);
         }
-    }).catch((err) => {
-        return next(err);
-    });
+
+    })
+        .catch((err) => {
+            return next(err);
+        });
 });
 
 
