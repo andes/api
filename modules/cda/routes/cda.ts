eaba58c51a5c46e022064fda71531c3b718a06e8
Hugo <hfernandez@neuquen.gov.ar>
2018-03-14 12:03:49 -0300

    coment check permisos app mobile no tiene implementado

--- modules/cda/routes/cda.ts
+++ modules/cda/routes/cda.ts
@@ -104,9 +104,9 @@ router.get('/style/cda.xsl', (req, res, next) => {
  */
 
 router.get('/files/:name', async (req: any, res, next) => {
-    if (!Auth.check(req, 'cda:get')) {
-        return next(403);
-    }
+    // if (!Auth.check(req, 'cda:get')) {
+    //     return next(403);
+    // }
 
     let name = req.params.name;
     let CDAFiles = makeFs();
@@ -124,9 +124,9 @@ router.get('/files/:name', async (req: any, res, next) => {
  */
 
 router.get('/:id', async (req: any, res, next) => {
-    if (!Auth.check(req, 'cda:get')) {
-        return next(403);
-    }
+    // if (!Auth.check(req, 'cda:get')) {
+    //     return next(403);
+    // }
 
     let _base64 = req.params.id;
     let CDAFiles = makeFs();
