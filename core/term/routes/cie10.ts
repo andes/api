1ec408838903139adbc1906b6651e43dca929830
condorpiedra <natalia.huenchuman@gmail.com>
2018-03-20 09:02:30 -0300

    mejora busqueda cie10 (#245)

--- core/term/routes/cie10.ts
+++ core/term/routes/cie10.ts
@@ -7,26 +7,27 @@ let router = express.Router();
 
 router.get('/cie10', function (req, res, next) {
     let query;
-    query = cie10.model.find({});
+    let conditions = {};
     let termino: String = '';
+    // conditions['$and'] = [];
+    conditions['$or'] = [];
     // separamos todas las palabras y eliminamos caracteres extraños
     let words = String(req.query.nombre).split(' ');
     words.forEach(function (word) {
         // normalizamos cada una de las palabras como hace SNOMED para poder buscar palabra a palabra
         word = word.replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, '\\$1').replace(/\x08/g, '\\x08');
         let expWord = utils.removeDiacritics(word) + '.*';
+        conditions['$or'].push({ 'words': { '$regex': '(?i)' + word } });
         // agregamos la palabra al término de búsqueda
         termino = termino + expWord;
     });
 
-    let busqueda = [
-        { 'codigo': RegExp('^.*' + termino + '.*$', 'i') },
-        { 'sinonimo': RegExp('^.*' + termino + '.*$', 'i') },
-        { 'nombre': RegExp('^.*' + termino + '.*$', 'i') },
-    ];
-    query.or(busqueda);
+    conditions['$or'].push({ 'codigo': RegExp('^.*' + termino + '.*$', 'i') });
+    conditions['$or'].push({ 'nombre': RegExp('^.*' + termino + '.*$', 'i') });
+    conditions['$or'].push({ 'sinonimo': RegExp('^.*' + termino + '.*$', 'i') });
+    query = cie10.model.find(conditions);
     let skip = parseInt(req.query.skip || 0, 10);
-    let limit = Math.min(parseInt(req.query.limit || defaultLimit, 10), maxLimit);
+    let limit = Math.min(parseInt(req.query.limit || defaultLimit, 15), maxLimit);
     query.skip(skip);
     query.limit(limit);
     query.exec(function (err, data) {
