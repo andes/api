da15ad59726776b339b4d6c0e5629e6af0380153
nico dinolfo <nicodinolfo@gmail.com>
2018-03-28 10:23:30 -0300

    fix Bug 43 - Agenda suspendida/sobreturnos (#253)
    * suspensión de sobreturnos al suspender agenda.
    * soporte sobreturnos para patch de aviso suspension

--- modules/turnos/controller/agenda.ts
+++ modules/turnos/controller/agenda.ts
@@ -435,6 +435,13 @@ export function actualizarEstado(req, data) {
 
                 });
             });
+            data.sobreturnos.forEach(sobreturno => {
+                if (sobreturno.estado !== 'turnoDoble') {
+                    sobreturno.estado = 'suspendido';
+                }
+                sobreturno.motivoSuspension = 'agendaSuspendida';
+                sobreturno.avisoSuspension = 'no enviado';
+            });
         }
 
     }
