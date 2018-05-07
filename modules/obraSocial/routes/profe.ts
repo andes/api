642cc07800e3168b42041677e7729785240905a4
lautaro_molina <lautaro.mlagos@gmail.com>
2018-05-07 13:57:19 -0300

    ruta+esquema

---
+++ modules/obraSocial/routes/profe.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+import * as express from 'express';
+import { profe } from '../schemas/profe';
+
+let router = express.Router();
+
+router.get('/profe', function (req, res, next) {
+    if (req.query.dni) {
+        profe.find({ dni: Number.parseInt(req.query.dni) }, function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            res.json(data);
+        });
+    }
+});
+
+module.exports = router;
