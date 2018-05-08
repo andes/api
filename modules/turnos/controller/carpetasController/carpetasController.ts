6e954cb44faca00eb7c7eef4e35f6aa9b819a9fb
NicoDino <nicodinolfo@gmail.com>
2018-05-08 15:48:22 -0300

    avances prometedores

--- modules/turnos/controller/carpetasController/carpetasController.ts
+++ modules/turnos/controller/carpetasController/carpetasController.ts
@@ -4,6 +4,7 @@ import * as config from './config';
 import * as sql from 'mssql';
 import { model as Organizaciones } from '../../../../core/tm/schemas/organizacion';
 import * as carpetaPaciente from '../../schemas/carpetaPaciente';
+import * as configPrivate from '../../../../config.private';
 
 import * as debug from 'debug';
 let logger = debug('carpetasJob');
@@ -38,7 +39,7 @@ const findUpdateCarpeta = async (paciente) => {
         }
 
         if (carpeta._id) {
-            logger('actualizo', documentoPaciente);
+            // logger('actualizo', documentoPaciente);
             carpetaPaciente.update({ '_id': carpeta._id }, {
                 $set:
                     { 'carpetaEfectores': carpeta.carpetaEfectores }
@@ -60,35 +61,63 @@ export async function migrar() {
     try {
         let efectores: any = await Organizaciones.find({ 'integracionActiva': true }).exec();
         if (efectores && efectores.length > 0) {
-            efectores.forEach(async element => {
-                logger('Migrando carpetas de pacientes en:  ', element.nombre);
-                let q_idSips = `select idEfector from dbo.sys_Efector as efector
-                                WHERE codigoSisa=` + element.codigo.sisa;
-                let q_limites = `select MIN(PAC.idPaciente) as min, COUNT(PAC.idPaciente) as max from dbo.sys_paciente as PAC inner
-                            join dbo.Sys_RelHistoriaClinicaEfector AS rhe ON rhe.idPaciente = pac.idPaciente
-                            WHERE PAC.activo = 1` + ' AND rhe.idEfector=' + element.codigo.idSips;
-                // Se busca la organización de la que se van a migrar las carpetas de pacientes
-                logger('codigo ', element.codigo.codigoSisa);
-                logger('Se actualizarán las carpetas de los pacientes desde SIPS de ', efectores[0].nombre);
 
-                organizacion = efectores[0];
-                if (element.codigo.idSips) {
+            logger('Efectores---->', efectores);
+            // ejecutamos la migracion para cada efector en paralelo.
+            for (let efector of efectores) {
+                await migrarEfector(efector);
+            }
 
-                    // let consulta = config.consultaPacienteSipsHC + ' AND efector.idEfector=' + element.codigo.idSips + ' AND PAC.idPaciente between @offset and @limit';
-                    let consulta = config.consultaCarpetaPacienteSips + ' AND rhe.idEfector=' + element.codigo.idSips;
-                    logger('EFECTOR', element.codigo.idSips, organizacion.nombre);
-                    // return utils.migrarOffset(consulta, q_limites, 100, insertCarpeta);
-                    await utils.migrar(consulta, q_limites, 10000, findUpdateCarpeta);
-                    logger('Migracion de datos completa desde ', organizacion.nombre);
-
-                } else {
-                    logger('Código de organización inválido, verifica el codigo Sisa ingresado');
-                }
-            });
         } else {
             logger('Código de organización inválido, verifica el codigo Sisa ingresado');
         }
     } catch (err) { logger('Error al obtener la organización', err); }
+
+    async function migrarEfector(element: any) {
+
+        logger('Migrando carpetas de pacientes en:  ', element.nombre);
+        let idSips = await getIdSips(element);
+        logger('codigo ', element.codigo.sisa);
+        let q_limites = `select MIN(PAC.idPaciente) as min, COUNT(PAC.idPaciente) as max from dbo.sys_paciente as PAC inner
+                            join dbo.Sys_RelHistoriaClinicaEfector AS rhe ON rhe.idPaciente = pac.idPaciente
+                            WHERE PAC.activo = 1` + ' AND rhe.idEfector=' + idSips;
+        logger('Se actualizarán las carpetas de los pacientes desde SIPS de ', element.nombre);
+        organizacion = element;
+        if (idSips) {
+            // let consulta = config.consultaPacienteSipsHC + ' AND efector.idEfector=' + element.codigo.idSips + ' AND PAC.idPaciente between @offset and @limit';
+            let consulta = config.consultaCarpetaPacienteSips + ' AND rhe.idEfector=' + idSips.idEfector;
+            logger('EFECTOR', idSips, organizacion.nombre);
+            // return utils.migrarOffset(consulta, q_limites, 100, insertCarpeta);
+            await utils.migrar(consulta, q_limites, 10000, findUpdateCarpeta);
+            logger('Migracion de datos completa desde ', organizacion.nombre);
+        } else {
+            logger('ID sips inválido');
+        }
+
+    }
+
+    async function getIdSips(efector) {
+
+        let connection = {
+            user: configPrivate.conSql.auth.user,
+            password: configPrivate.conSql.auth.password,
+            server: configPrivate.conSql.serverSql.server,
+            database: configPrivate.conSql.serverSql.database
+        };
+        try {
+            const connectionPool = await sql.connect(connection);
+            sql.on('error', err => {
+                logger('Error SQL---->', err);
+            });
+
+            let querySips = `select idEfector from dbo.sys_Efector as efector WHERE codigoSisa=` + `'` + String(efector.codigo.sisa) + `'`;
+            let resultado = await connectionPool.request()
+                .query(querySips);
+            resultado = resultado.recordset[0];
+            logger('IDSIPS', resultado);
+            return resultado;
+        } catch (err) { logger('Error obteniendo ID sips', err); }
+    }
 }
 
 
--- modules/turnos/controller/carpetasController/utils.ts
+++ modules/turnos/controller/carpetasController/utils.ts
@@ -21,7 +21,6 @@ export function migrar(q_objeto, q_limites, page_size, addNuevoObjeto) {
                 .query(q_objeto)
                 .then(objetos => {
                     if (objetos.recordset && objetos.recordset.length) {
-                        logger("ACA ESTAMOS", objetos.recordset)
                         let nuevosObjetos = objetos.recordset.map(o => addNuevoObjeto(o));
                         return Promise.all(nuevosObjetos).then(res =>
                             navegar(pool, offset + 1)
@@ -53,47 +52,3 @@ export function migrar(q_objeto, q_limites, page_size, addNuevoObjeto) {
 
     return runQuery();
 }
-
-
-// export function migrarOffset(q_objeto, q_limites, page_size, addNuevoObjeto) {
-//     let max;
-//     const connection = {
-//         user: config.sqlCarpetasJob.user,
-//         password: config.sqlCarpetasJob.password,
-//         server: config.sqlCarpetasJob.server,
-//         database: config.sqlCarpetasJob.database,
-//         // connectionTimeout: config.sqlCarpetasJob.connectionTimeout,
-//         requestTimeout: config.sqlCarpetasJob.requestTimeout
-//     };
-
-//     function jobMigracion(connection, i, page_size1, total) {
-//         logger(i, page_size1);
-//         if (i * page_size1 < total) {
-//             let offset = i * page_size1;
-//             return sqlserver.queryOffset(connection, q_objeto, page_size1, offset)
-//                 .then(objetos => {
-//                     if (objetos && objetos.length) {
-//                         logger('Cant pacientes', objetos.length);
-//                         let nuevosObjetos = objetos.map(o => addNuevoObjeto(o));
-//                         return Promise.all(nuevosObjetos).then(res =>
-//                             jobMigracion(connection, i + 1, page_size1, total)
-//                         );
-//                     } else {
-//                         return jobMigracion(connection, i + 1, page_size1, total);
-//                     }
-//                 });
-//         }
-//     }
-
-//     return sqlserver.query(connection, q_limites)
-//         .then(resultado => {
-//             if (resultado[0]) {
-//                 logger(resultado[0]['min'] + ' - ' + resultado[0]['max']);
-//                 let min = resultado[0]['min'];
-//                 max = resultado[0]['max'];
-//                 logger('MAX', max);
-//                 return jobMigracion(connection, 0, 5000, max);
-//             }
-//         });
-// }
-
