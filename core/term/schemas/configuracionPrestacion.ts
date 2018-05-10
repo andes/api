d85ee4a3c0a036070c5d4fb59a99254b235b79cf
Hugo <hfernandez@neuquen.gov.ar>
2018-05-10 13:37:58 -0300

    add idServicio sips

--- core/term/schemas/configuracionPrestacion.ts
+++ core/term/schemas/configuracionPrestacion.ts
@@ -14,6 +14,7 @@ let configuracionPrestacionSchema = new mongoose.Schema({
         codeSystemName: String,
         displayName: String
     },
+    idServicio: Number,
     organizacionesSips: [{
         _id: {
             type: mongoose.Schema.Types.ObjectId,
