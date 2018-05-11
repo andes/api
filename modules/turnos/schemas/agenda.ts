46f9fd7a1ed21c57a0733b921aded3e87d22b6fb
ndinolfo <nicodinolfo@gmail.com>
2018-05-11 14:28:57 -0300

    hotfix error pushall schema agenda mongoose

--- modules/turnos/schemas/agenda.ts
+++ modules/turnos/schemas/agenda.ts
@@ -59,7 +59,7 @@ let schema = new mongoose.Schema({
         default: true
     }
 
-}, { versionKey: false });
+}, { versionKey: false, usePushEach: true });
 
 // Defino Virtuals
 schema.virtual('turnosDisponibles').get(function () {
