1aaaafbb90ad126561ac4e402f6cebe726282d67
fersastre <fersastre23@gmail.com>
2018-06-19 09:34:37 -0300

    agrego el filtro solo por internacion, antes traia las epicrisis

--- modules/rup/controllers/internacion.ts
+++ modules/rup/controllers/internacion.ts
@@ -21,6 +21,7 @@ export function buscarUltimaInternacion(idPaciente, estado) {
     }
 
     query.where('solicitud.ambitoOrigen').equals('internacion');
+    query.where('solicitud.tipoPrestacion.conceptId').equals('32485007'); // Ver si encontramos otra forma de diferenciar las prestaciones de internacion
     return query.sort({ 'solicitud.fecha': -1 }).limit(1).exec();
 }
 
