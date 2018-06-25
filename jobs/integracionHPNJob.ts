cbebb1771ba4616093637326fc3265555e090be5
Hugo <hfernandez@neuquen.gov.ar>
2018-06-25 14:03:19 -0300

    add fixes

--- jobs/integracionHPNJob.ts
+++ jobs/integracionHPNJob.ts
@@ -1,7 +1,8 @@
 import * as agendaHPNCtrl from './../modules/turnos/controller/agendasHPNCacheController';
 
-function run() {
-    agendaHPNCtrl.integracion();
+function run(done) {
+    agendaHPNCtrl.integracion().then(done).catch(done);
 }
+
 
 export = run;
