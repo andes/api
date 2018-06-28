784ce2822016731622f2aea0703324f7a4eb4e5c
ndinolfo <nicodinolfo@gmail.com>
2018-06-28 13:31:10 -0300

    get ultima carpeta pac para sugerencia y post incrementar nro carpeta

--- modules/carpetas/routes/ultimaCarpeta.ts
+++ modules/carpetas/routes/ultimaCarpeta.ts
@@ -1,33 +1,35 @@
 import * as express from 'express';
 import * as ultimaCarpeta from '../schemas/ultimaCarpeta';
+import { mongooseDebugMode } from '../../../config.private';
+import * as mongoose from 'mongoose';
 
 let router = express.Router();
 
 router.get('/ultimaCarpeta', function (req: any, res, next) {
-
     if (req.user.organizacion && req.user.organizacion.id) {
-        ultimaCarpeta.find({ idEfector: req.user.organizacion.id }, function (err, data) {
+
+        let idOrganizacion = new mongoose.Types.ObjectId(req.user.organizacion.id);
+        ultimaCarpeta.findOne({ idEfector: idOrganizacion }, function (err, data: any) {
             if (err) {
                 return next(err);
             }
-            res.json(data);
+            data.ultimaCarpeta = data.ultimaCarpeta + 1;
+            res.json(data.ultimaCarpeta);
         });
     }
-
-
 });
 
 router.post('/incrementarCuenta', function (req: any, res, next) {
-
     if (req.user.organizacion && req.user.organizacion.id) {
-        ultimaCarpeta.findOne({ idEfector: req.user.organizacion.id }, function (err, data: any) {
+        let idOrganizacion = new mongoose.Types.ObjectId(req.user.organizacion.id);
+        ultimaCarpeta.findOne({ idEfector: idOrganizacion }, function (err, data: any) {
             if (err) {
                 return next(err);
             }
             let update = {
                 ultimaCarpeta: data.ultimaCarpeta + 1
             };
-            ultimaCarpeta.update({ idEfector: req.user.organizacion.id }, { $set: update }, { new: true }, function (errUpdate, dataUpdate) {
+            ultimaCarpeta.update({ idEfector: idOrganizacion }, { $set: update }, { new: true }, function (errUpdate, dataUpdate) {
                 if (err) {
                     return next(errUpdate);
                 }
@@ -35,9 +37,6 @@ router.post('/incrementarCuenta', function (req: any, res, next) {
             });
         });
     }
-
-
 });
-
 
 export = router;
