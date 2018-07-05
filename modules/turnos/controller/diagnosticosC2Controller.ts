4b83121cb40da0b89318cb2b45d35fb8147de2fc
kro <celeste.carolina.s@gmail.com>
2018-07-05 11:46:18 -0300

    se corrige bug en el match de sobreturnos

--- modules/turnos/controller/diagnosticosC2Controller.ts
+++ modules/turnos/controller/diagnosticosC2Controller.ts
@@ -148,7 +148,7 @@ export function getDiagnosticos(params) {
             $match: {
                 'sobreturnos.diagnostico.codificaciones.0.codificacionAuditoria': { $exists: true, $ne: {} },
                 'sobreturnos.diagnostico.codificaciones.0.codificacionAuditoria.c2': true,
-                'bloques.turnos.diagnostico.codificaciones.0.primeraVez': true,
+                'sobreturnos.diagnostico.codificaciones.0.primeraVez': true,
                 'horaInicio': { '$gte': new Date(params.horaInicio) },
                 'horaFin': { '$lte': new Date(params.horaFin) },
                 'organizacion._id': { '$eq': mongoose.Types.ObjectId(params.organizacion) }
