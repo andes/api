a765f126c47544d1aee63b46509d6f5b8150da55
Lautaro Molina <negro89_nqn@hotmail.com>
2018-07-06 15:46:08 -0300

    Se modifica la api para requerimientos de puco version 2

--- modules/obraSocial/routes/obraSocial.ts
+++ modules/obraSocial/routes/obraSocial.ts
@@ -1,5 +1,8 @@
 import * as express from 'express';
 import { puco } from '../schemas/puco';
+// import { Date } from 'core-js';
+import * as moment from 'moment';
+import { stringify } from 'querystring';
 
 let router = express.Router();
 
@@ -22,12 +25,35 @@ router.get('/puco/', async function (req, res, next) {
     }
 });
 
-router.get('/puco/version/', async function (req, res, next) {
-    // periodo especifico
+router.get('/puco/version/:periodo*?', async function (req, res, next) {
+    // se selecciona periodo especifico --> retorna padron de dicho periodo
     if (req.params.periodo) {
-        res.json({});
-    } else {    // no se ingresa periodo
-        res.json();
+        puco.find({ version: req.params.periodo }, function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            res.json(data);
+        });
+    } else {    // no se selecciona periodo --> retorna los padrones de los ultimos <ultimos> periodos
+        if (req.query.ultimos) {
+            // obtiene ultimo registro insertado
+            let ultimoReg: any = await puco.find({}).sort({ $natural: 1 }).limit(1);
+            let ultimaVersion = JSON.parse(JSON.stringify(ultimoReg[0])).version;
+
+            // Se crea un objeto Date para delegar el calculo de periodos hacia atras a buscar
+            let ultimaFecha = moment(new Date(String(ultimaVersion).substring(0, 4) + '/' + (String(ultimaVersion).substring(4, 6) + '/' + String(ultimaVersion).substring(6, 8)))).format('YYYY/MM/DD');
+            let fechaAux = moment(new Date(ultimaFecha)).subtract(req.query.ultimos, 'months').format('YYYYMMDD');
+            let fechaDesde = Number.parseInt(fechaAux);
+
+            puco.find({ version: { $gt: fechaDesde } }, function (err, data) {
+                if (err) {
+                    return next(err);
+                }
+                res.json({ ultimoPeriodo: ultimaVersion, padrones: data });
+            });
+        } else {
+            res.status(400).json({ msg: 'Parámetros incorrectos' });
+        }
     }
 });
 
