cae10bf6940818640eeaf0ebf8bb8e4fb7bad8d9
Andrés <andresin@gmail.com>
2018-07-17 08:17:25 -0300

    Mejora en carga de assets

--- modules/descargas/controller/descargas.ts
+++ modules/descargas/controller/descargas.ts
@@ -10,13 +10,14 @@ export class Documento {
     private static locale = 'es-ES';
     private static timeZone = 'America/Argentina/Buenos_Aires';
 
-    private static headerHTML = fs.readFileSync('./templates/andes/html/header.html');
-    private static footerHTML = fs.readFileSync('./templates/andes/html/footer.html');
+    // Se leen header y footer (si se le pasa un encoding, devuelve un string)
+    private static headerHTML = fs.readFileSync('./templates/andes/html/header.html', 'utf8');
+    private static footerHTML = fs.readFileSync('./templates/andes/html/footer.html', 'utf8');
 
     /**
      * Opciones default de PDF rendering
      */
-    private static options: any = {};
+    private static options: pdf.CreateOptions = {};
 
     /**
      *
@@ -25,19 +26,22 @@ export class Documento {
      */
     private static generarHTML(req) {
 
+        // SCSS según Tipo de Prestación
+        const scssFile = typeof req.body.scssFile !== 'undefined' ? req.body.scssFile : 'prestacion-print';
+
         // Se genera HTML para ser transformado en PDF
         let html = Buffer.from(req.body.html, 'base64').toString();
 
         // Se agregan los estilos CSS
-        html += this.generarCSS(path.join(__dirname, '../../../templates/rup/' + req.body.scssFile + '.scss'));
+        html += this.generarCSS(path.join(__dirname, '../../../templates/rup/' + scssFile + '.scss'));
 
         // Se cargan logos
         let logoAndes = fs.readFileSync(path.join(__dirname, '../../../templates/andes/logo-andes.png'));
         let logotipoAndes = fs.readFileSync(path.join(__dirname, '../../../templates/andes/logotipo-andes-blue.png'));
         let logoPDP = fs.readFileSync(path.join(__dirname, '../../../templates/andes/logo-pdp.png'));
 
-        html += this.headerHTML.toString();
-        html += this.footerHTML.toString();
+        html += this.headerHTML;
+        html += this.footerHTML;
 
         // Se reemplazan ciertos <!--placeholders--> por logos de ANDES y Dirección de Protección de Datos Personales
         // Y datos de sesión (organización, nombre del usuario, timestamp)
@@ -81,77 +85,55 @@ export class Documento {
      */
     static descargar(req, res, next, options = null) {
 
-        let html = '';
-        switch (req.params.tipo) {
-            case 'pdf':
-                // PhantomJS PDF rendering options
-                // https://www.npmjs.com/package/html-pdf
-                // http://phantomjs.org/api/webpage/property/paper-size.html
-                let phantomPDFOptions: any = {
-                    format: 'A4',
-                    border: {
-                        // default is 0, units: mm, cm, in, px
-                        top: '0.5cm',
-                        right: '0.5cm',
-                        bottom: '1.5cm',
-                        left: '1.5cm'
-                    },
-                    header: {
-                        height: '2.5cm',
-                    },
-                    footer: {
-                        height: '10mm',
-                        contents: {
-                        }
-                    },
-                    settings: {
-                        resourceTimeout: 30
-                    },
-                };
+        return new Promise((resolve, reject) => {
 
-                if (options !== null) {
-                    this.options = options;
-                } else {
-                    this.options = phantomPDFOptions;
-                }
+            let html = '';
+            switch (req.params.tipo) {
+                case 'pdf':
+                    // PhantomJS PDF rendering options
+                    // https://www.npmjs.com/package/html-pdf
+                    // http://phantomjs.org/api/webpage/property/paper-size.html
+                    let phantomPDFOptions: pdf.CreateOptions = {
+                        format: 'A4',
+                        border: {
+                            // default is 0, units: mm, cm, in, px
+                            top: '0.5cm',
+                            right: '0.5cm',
+                            bottom: '1.5cm',
+                            left: '1.5cm'
+                        },
+                        header: {
+                            height: '2.5cm',
+                        },
+                        footer: {
+                            height: '10mm',
+                            contents: {
+                                default: '<span class="text-muted">{{page}}</span>/<span>{{pages}}</span>'
+                            }
+                        },
+                    };
 
-                html = this.generarHTML(req);
-
-                pdf.create(html, this.options).toFile((err2, file) => {
-
-                    if (err2) {
-                        return next(err2);
-                    }
-
-                    res.download(file.filename, (err3) => {
-                        if (err3) {
-                            next(err3);
-                        } else {
-                            next();
-                        }
-                    });
-                });
-                break;
-            case 'html':
-                html = this.generarHTML(req);
-
-                let htmlfile = fs.writeFileSync('/tmp/rup.html', html);
-                res.download('/tmp/rup.html', (err) => {
-                    if (err) {
-                        next(err);
+                    if (options !== null) {
+                        this.options = options;
                     } else {
-                        next();
+                        this.options = phantomPDFOptions;
                     }
-                });
 
-                break;
-            case 'txt':
-                // TODO
-                break;
-            case 'png':
-                // TODO
-                break;
-        }
+                    html = this.generarHTML(req);
 
+                    pdf.create(html, this.options).toFile((err2, file): any => {
+
+                        if (err2) {
+                            reject(err2);
+                        }
+
+                        resolve(file.filename);
+
+                    });
+                    break;
+            }
+
+        });
     }
+
 }
