eb55d321295d1a4a2314e5b3f2fcb59b43ec38b7
Mariano Botta <marianoabotta@gmail.com>
2018-07-24 09:05:02 -0300

    filtro de tweets

--- modules/mobileApp/auth_routes/farmacias.ts
+++ modules/mobileApp/auth_routes/farmacias.ts
@@ -1,9 +1,7 @@
-import * as express from 'express';
-import * as mongoose from 'mongoose';
+import * as express from 'express'; 
 import * as moment from 'moment';
 
-import { farmaciasLocalidades, farmaciasTurnos } from '../schemas/farmacias';
-import * as farmaciaController from '../controller/FarmaciasTurnosDownloader';
+import { farmaciasLocalidades, farmaciasTurnos } from '../schemas/farmacias'; 
 
 let router = express.Router();
 
---
+++ modules/mobileApp/auth_routes/noticias.ts
@@ -0,0 +1,42 @@
+import * as moment from 'moment';
+import * as express from 'express';
+import * as Twitter from 'twitter';
+import { TwitterConfig } from '../../../config.private';
+
+let router = express.Router();
+
+router.get('/noticias/:name', (req, res) => {
+    let feedName = req.params.name;
+    if (feedName === 'puntosaludable') {
+        let client = new Twitter(TwitterConfig);
+        let params = { screen_name: 'SaludableMsNqn', count: 200, tweet_mode: 'extended' };
+        client.get('statuses/user_timeline', params, function (error, tweets) {
+            if (!error) {
+                let feeds = [];
+                tweets.forEach((item) => {
+                    if (!item.retweeted_status) {
+                        let text = item.full_text;
+                        if (item.display_text_range) {
+                            text = item.full_text.substring(item.display_text_range[0], item.display_text_range[1]);
+                        }
+                        let tw = {
+                            id: item.id_str,
+                            fecha: moment(item.created_at, 'ddd MMM DD HH:mm:ss ZZ YYYY'),
+                            text: text,
+                            hashtags: item.entities.hashtags ? item.entities.hashtags.map(i => i.text) : [],
+                            urls: item.entities.urls ? item.entities.urls.map(i => i.expanded_url) : [],
+                            images: item.entities.media ? item.entities.media.map(i => i.media_url_https) : [],
+                        };
+                        feeds.push(tw);
+
+                    }
+                });
+                res.json(feeds);
+            }
+        });
+    } else {
+        res.status(422).json({message: 'no_feed'});
+    }
+});
+
+export = router;
