b64c22605f32eefe0c651161c353e9fe216ef01e
Mariano Botta <marianoabotta@gmail.com>
2018-08-07 13:35:32 -0300

    probando testing utomatico

--- apps/mobile-app/test/auth.spec.ts
+++ apps/mobile-app/test/auth.spec.ts
@@ -4,26 +4,27 @@ import { initAPI } from '../../../initialize';
 import { PacienteApp } from '../schemas/pacienteApp';
 import { Auth } from '../../../auth/auth.class';
 
+
 const request = require('supertest');
 let app = express();
 
-describe('Test mobile app authentication', () => {
+describe('MobileApp - Login', () => {
     beforeAll((done) => {
         // [TODO] Change this to a promises style
         initAPI(app);
-        setTimeout(done, 2000);
+        setTimeout(done, 3000);
     });
 
     beforeEach(async () => {
         await PacienteApp.remove({ email: 'user@andes.gob.ar' });
     });
 
-    test('Login with no field must be 422 status', async () => {
+    test('login sin campoes debe devolver codigo 422', async () => {
         const response = await request(app).post('/api/modules/mobileApp/login');
         expect(response.statusCode).toBe(422);
     });
 
-    test('password miss = 422', async () => {
+    test('login sin password debe devolver codigo 422', async () => {
         const response = await request(app).post('/api/modules/mobileApp/login').send({
             'email': 'user1@g.com'
         });
@@ -31,7 +32,7 @@ describe('Test mobile app authentication', () => {
         expect(response.body.message).toBe('Debe ingresar una clave');
     });
 
-    test('login success', async () => {
+    test('login correcto', async () => {
         let pac = new PacienteApp({
             nombre: 'Perez',
             apellido: 'Juan',
@@ -54,7 +55,7 @@ describe('Test mobile app authentication', () => {
         generateTokenMock.mockRestore();
     });
 
-    test('login error', async () => {
+    test('login con diferente password', async () => {
         let pac = new PacienteApp({
             nombre: 'Perez',
             apellido: 'Juan',
@@ -69,10 +70,9 @@ describe('Test mobile app authentication', () => {
             password: 'asdasdasdasd'
         });
         expect(response.statusCode).toBe(422);
-
     });
 
-    test('prevent first login without new password', async () => {
+    test('primer login sin nueva contraseña devuelve codigo 422', async () => {
         let pac = new PacienteApp({
             nombre: 'Perez',
             apellido: 'Juan',
@@ -89,7 +89,7 @@ describe('Test mobile app authentication', () => {
         expect(response.statusCode).toBe(422);
     });
 
-    test('first login with new password', async (done) => {
+    test('primer login exige cambio de contraseña correcto', async () => {
         let pac = new PacienteApp({
             nombre: 'Perez',
             apellido: 'Juan',
@@ -110,7 +110,6 @@ describe('Test mobile app authentication', () => {
 
         pactemp.comparePassword('123456', async (_err, isMatch) => {
             expect(isMatch).toBe(true);
-            done();
         });
     });
 
@@ -120,3 +119,122 @@ describe('Test mobile app authentication', () => {
 
 });
 
+
+/**
+ * Reset password
+ */
+
+let acc;
+describe('MobileApp - reset password', () => {
+    beforeAll((done) => {
+        initAPI(app);
+        setTimeout(async () => {
+
+            acc = new PacienteApp({
+                nombre: 'Perez',
+                apellido: 'Juan',
+                email: 'user@andes.gob.ar',
+                password: 'asdasd',
+                activacionApp: true
+            });
+            await acc.save();
+
+            done();
+        }, 2000);
+    });
+
+    test('resetear contraseña sin email falla', async () => {
+        const response = await request(app).post('/api/modules/mobileApp/olvide-password');
+        expect(response.statusCode).toBe(422);
+    });
+
+    test('resetear contraseña con email correcto', async () => {
+
+        const authController = require('../controller/AuthController');
+
+        const generarCodigoVerificacionMock = jest.spyOn(authController, 'generarCodigoVerificacion');
+        const enviarCodigoCambioPasswordMock = jest.spyOn(authController, 'enviarCodigoCambioPassword');
+
+        enviarCodigoCambioPasswordMock.mockImplementation(() => {
+            return true;
+        });
+
+        const response = await request(app).post('/api/modules/mobileApp/olvide-password').send({
+            email: 'user@andes.gob.ar'
+        });
+        expect(response.statusCode).toBe(200);
+
+        expect(generarCodigoVerificacionMock).toBeCalled();
+        expect(enviarCodigoCambioPasswordMock).toBeCalled();
+
+        generarCodigoVerificacionMock.mockRestore();
+        enviarCodigoCambioPasswordMock.mockRestore();
+    });
+
+    test('token incorrecto al resetear contraseña', async (done) => {
+        await PacienteApp.findOneAndUpdate({ email: 'user@andes.gob.ar'}, { $set: {
+            restablecerPassword: {
+                codigo: '123345',
+                fechaExpiracion: new Date(Date.now() + 1000 * 60 * 60)
+            }
+        }});
+
+        const response = await request(app).post('/api/modules/mobileApp/reestablecer-password').send({
+            email: 'user@andes.gob.ar',
+            codigo: 'malcodigo',
+            password: 'asdasd',
+            password2: 'asdasd'
+        });
+        expect(response.statusCode).toBe(422);
+        done();
+    });
+
+    test('token expirado al resetear contraseña', async (done) => {
+        await PacienteApp.findOneAndUpdate({ email: 'user@andes.gob.ar'}, { $set: {
+            restablecerPassword: {
+                codigo: '123345',
+                fechaExpiracion: new Date(Date.now() - 1000 * 60 * 60)
+            }
+        }});
+
+        const response = await request(app).post('/api/modules/mobileApp/reestablecer-password').send({
+            email: 'user@andes.gob.ar',
+            codigo: '123345',
+            password: 'asdasd',
+            password2: 'asdasd'
+        });
+        expect(response.statusCode).toBe(422);
+        done();
+    });
+
+    test('resetear password correcta', async (done) => {
+        await PacienteApp.findOneAndUpdate({ email: 'user@andes.gob.ar'}, { $set: {
+            restablecerPassword: {
+                codigo: '123345',
+                fechaExpiracion: new Date(Date.now() + 1000 * 60 * 60)
+            }
+        }});
+
+        const response = await request(app).post('/api/modules/mobileApp/reestablecer-password').send({
+            email: 'user@andes.gob.ar',
+            codigo: '123345',
+            password: 'asdasd',
+            password2: 'asdasd'
+        });
+        expect(response.statusCode).toBe(200);
+
+        let pactemp = await PacienteApp.findById(acc._id);
+
+        pactemp.comparePassword('asdasd', async (_err, isMatch) => {
+            expect(isMatch).toBe(true);
+            done();
+        });
+
+    });
+
+    afterAll(async (done) => {
+        await PacienteApp.remove({ email: 'user@andes.gob.ar' });
+        mongoose.disconnect(done);
+    });
+
+});
