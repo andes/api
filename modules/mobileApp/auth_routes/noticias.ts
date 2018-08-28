75a47d81ac7917e8f244e5d32d13aa4405624b27
Hugo <hfernandez@neuquen.gov.ar>
2018-08-28 16:22:09 -0300

    fix circle ci and return

--- modules/mobileApp/auth_routes/noticias.ts
+++ modules/mobileApp/auth_routes/noticias.ts
@@ -17,32 +17,32 @@ router.get('/noticias/:name', async (req, res) => {
             tweet_mode: 'extended'
         };
         let tweets = await client.get('statuses/user_timeline', params);
-        if (!tweets) {
-            return res.status(422).json({
+        if (tweets) {
+            let feeds = [];
+            tweets.forEach((item) => {
+                if (!item.retweeted_status) {
+                    let text = item.full_text;
+                    if (item.display_text_range) {
+                        text = item.full_text.substring(item.display_text_range[0], item.display_text_range[1]);
+                    }
+                    let tw = {
+                        id: item.id_str,
+                        fecha: moment(item.created_at, 'ddd MMM DD HH:mm:ss ZZ YYYY'),
+                        text: text,
+                        hashtags: item.entities.hashtags ? item.entities.hashtags.map(i => i.text) : [],
+                        urls: item.entities.urls ? item.entities.urls.map(i => i.expanded_url) : [],
+                        images: item.entities.media ? item.entities.media.map(i => i.media_url_https) : [],
+                    };
+                    feeds.push(tw);
+
+                }
+            });
+            res.json(feeds);
+        } else {
+            res.status(422).json({
                 message: 'no_feed'
             });
         }
-        let feeds = [];
-        tweets.forEach((item) => {
-            if (!item.retweeted_status) {
-                let text = item.full_text;
-                if (item.display_text_range) {
-                    text = item.full_text.substring(item.display_text_range[0], item.display_text_range[1]);
-                }
-                let tw = {
-                    id: item.id_str,
-                    fecha: moment(item.created_at, 'ddd MMM DD HH:mm:ss ZZ YYYY'),
-                    text: text,
-                    hashtags: item.entities.hashtags ? item.entities.hashtags.map(i => i.text) : [],
-                    urls: item.entities.urls ? item.entities.urls.map(i => i.expanded_url) : [],
-                    images: item.entities.media ? item.entities.media.map(i => i.media_url_https) : [],
-                };
-                feeds.push(tw);
-
-            }
-        });
-        res.json(feeds);
     }
 });
-
 export = router;
