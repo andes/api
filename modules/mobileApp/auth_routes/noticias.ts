410a391b6c07ddb00818fbfcb7bbdc12efa6fb81
Hugo <hfernandez@neuquen.gov.ar>
2018-08-28 13:04:56 -0300

    aplicamos guia desarrollo

--- modules/mobileApp/auth_routes/noticias.ts
+++ modules/mobileApp/auth_routes/noticias.ts
@@ -1,44 +1,49 @@
 import * as moment from 'moment';
 import * as express from 'express';
 import * as Twitter from 'twitter';
-import { TwitterConfig } from '../../../config.private';
+import {
+    TwitterConfig
+} from '../../../config.private';
 
 let router = express.Router();
 
-router.get('/noticias/:name', (req, res) => {
+router.get('/noticias/:name', async (req, res) => {
     let feedName = req.params.name;
     if (feedName === 'puntosaludable') {
         let client = new Twitter(TwitterConfig);
-        let params = { screen_name: 'PSaludableNqn', count: 200, tweet_mode: 'extended' };
-        client.get('statuses/user_timeline', params, function (error, tweets) {
-            if (!error) {
-                let feeds = [];
-                tweets.forEach((item) => {
-                    if (!item.retweeted_status) {
-                        let text = item.full_text;
-                        if (item.display_text_range) {
-                            text = item.full_text.substring(item.display_text_range[0], item.display_text_range[1]);
-                        }
-                        let tw = {
-                            id: item.id_str,
-                            fecha: moment(item.created_at, 'ddd MMM DD HH:mm:ss ZZ YYYY'),
-                            text: text,
-                            hashtags: item.entities.hashtags ? item.entities.hashtags.map(i => i.text) : [],
-                            urls: item.entities.urls ? item.entities.urls.map(i => i.expanded_url) : [],
-                            images: item.entities.media ? item.entities.media.map(i => i.media_url_https) : [],
-                        };
-                        feeds.push(tw);
+        let params = {
+            screen_name: 'PSaludableNqn',
+            count: 200,
+            tweet_mode: 'extended'
+        };
+        // await client.get('statuses/user_timeline', params), (error, tweets) => {
+        // client.get('statuses/user_timeline', params, function (error, tweets) {
+        let tweets = await client.get('statuses/user_timeline', params);
+        if (!tweets) {
+            res.status(422).json({
+                message: 'no_feed'
+            });
+        }
+        let feeds = [];
+        tweets.forEach((item) => {
+            if (!item.retweeted_status) {
+                let text = item.full_text;
+                if (item.display_text_range) {
+                    text = item.full_text.substring(item.display_text_range[0], item.display_text_range[1]);
+                }
+                let tw = {
+                    id: item.id_str,
+                    fecha: moment(item.created_at, 'ddd MMM DD HH:mm:ss ZZ YYYY'),
+                    text: text,
+                    hashtags: item.entities.hashtags ? item.entities.hashtags.map(i => i.text) : [],
+                    urls: item.entities.urls ? item.entities.urls.map(i => i.expanded_url) : [],
+                    images: item.entities.media ? item.entities.media.map(i => i.media_url_https) : [],
+                };
+                feeds.push(tw);
 
-                    }
-                });
-                res.json(feeds);
-            } else {
-                // Para que no de error, entra acá cuando por ejemplo cambian el screen_name
-                res.json(null);
             }
         });
-    } else {
-        res.status(422).json({message: 'no_feed'});
+        res.json(feeds);
     }
 });
 
