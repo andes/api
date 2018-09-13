ada8cd1bd43444fdfa62ce8b7295476da264b056
Pablo Lammel <lammelpablo@gmail.com>
2018-09-13 14:21:19 -0300

    numero protocolo en solicitud

--- modules/laboratorio/controller/protocolo.ts
+++ modules/laboratorio/controller/protocolo.ts
@@ -7,10 +7,10 @@ export async function getUltimoNumeroProtocolo(idOrganizacion) {
     let pipeline = [
         {$match: {'solicitud.organizacion.id':idOrganizacion}},
         {$match: {'solicitud.tipoPrestacion.conceptId':'15220000'}},
-        {$match: {'ejecucion.registros.nombre':'numeroProtocolo'}},
-        {$unwind: '$ejecucion.registros'},
-        {$match: {'ejecucion.registros.nombre':'numeroProtocolo'}},
-        {$sort: {'ejecucion.registros.valor.numero':-1}},
+        {$unwind: '$solicitud.registros'},
+        {$match: {'solicitud.registros.concepto.conceptId':'15220000'}},
+        {$match: {'solicitud.registros.valor.numeroProtocolo': {$exists: true}}},
+        {$sort: {'solicitud.registros.valor.numeroProtocolo.numero':-1}},
         {$group: {
             _id: null,
             first: { $first: "$$ROOT" }}
@@ -20,7 +20,7 @@ export async function getUltimoNumeroProtocolo(idOrganizacion) {
     let resultados = await toArray(prestacion.aggregate(pipeline).cursor({}).exec());
     let ultimoNumero;
     console.log('resultados[0]',resultados[0])
-    if (resultados[0].first.ejecucion.registros.valor && resultados[0].first.ejecucion.registros.valor.numero) {
+    if (resultados[0] && resultados[0].first.ejecucion.registros.valor && resultados[0].first.ejecucion.registros.valor.numero) {
         ultimoNumero = resultados[0].first.ejecucion.registros.valor.numero;
     } else {
         ultimoNumero = 0;
