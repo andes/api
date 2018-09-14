0e6c016fb24af674329099c33ea6109fb9647b12
Mariano Botta <marianoabotta@gmail.com>
2018-09-14 13:32:41 -0300

    agrega interfaz IPrestamo
    Queda pendiente aplicar interfaz de paciente

--- modules/prestamosCarpetas/schemas/prestamo.ts
+++ modules/prestamosCarpetas/schemas/prestamo.ts
@@ -1,12 +1,40 @@
-import { tipoPrestacionSchema } from '../../../core/tm/schemas/tipoPrestacion';
-import { espacioFisicoSchema } from '../../../modules/turnos/schemas/espacioFisico';
+import { tipoPrestacionSchema, ITipoPrestacion } from '../../../core/tm/schemas/tipoPrestacion';
+import { espacioFisicoSchema, IEspacioFisico } from '../../../modules/turnos/schemas/espacioFisico';
 import * as nombreApellidoSchema from '../../../core/tm/schemas/nombreApellido';
 import * as constantes from './constantes';
-import { Document, Schema, SchemaTypes, model, Model } from 'mongoose';
+import { Document, Schema, Types, SchemaTypes, model, Model } from 'mongoose';
 import * as nombreSchema from '../../../core/tm/schemas/nombre';
 
 export interface IPrestamo extends Document {
-
+    paciente: any;
+    numero: String;
+    organizacion: { nombre: String; };
+    estado: String;
+    datosPrestamo: {
+        observaciones: String;
+        agendaId: Types.ObjectId;
+        turno: {
+            id: Types.ObjectId;
+            profesionales: [{ nombre: String; apellido: String; }];
+            tipoPrestacion: ITipoPrestacion;
+            espacioFisico: IEspacioFisico;
+        }
+    };
+    datosDevolucion: {
+        observaciones: String,
+        estado: {
+            type: String,
+            enum: ['Normal', 'En mal estado', 'Fuera de término', 'Hojas o documentación faltante']
+        }
+    };
+    datosSolicitudManual: {
+        espacioFisico: IEspacioFisico;
+        prestacion: ITipoPrestacion;
+        profesional: { nombre: String; apellido: String };
+        responsable: { nombre: String; apellido: String };
+        observaciones: String;
+        idSolicitud: Types.ObjectId;
+    };
 }
 
 export const PrestamoSchema = new Schema({
