1470a1e556aaeabc461158b95ff692725d718009
ndinolfo <nicodinolfo@gmail.com>
2018-09-17 15:03:58 -0300

    limpio console logs innecesarios

--- core/mpi/controller/regexChecker.ts
+++ core/mpi/controller/regexChecker.ts
@@ -16,45 +16,41 @@ export async function regexChecker(done) {
 
         let cursor = await pacienteMpi.find({ $or: [{ nombre: { $regex: regtest } }, { apellido: { $regex: regtest } }] }).cursor();
         let countPacienteError = 0;
+
         await cursor.eachAsync(async (pac: any) => {
             let band = regtest.test(pac.nombre);
             band = band || regtest.test(pac.apellido);
             if (band) {
-                console.log(pac.nombre, pac.apellido, pac.documento, pac._id);
                 countPacienteError = countPacienteError + 1;
 
                 // Obtenemos el paciente con errores desde MPI , lo borramos y le cambiamos el estado a temporal
                 // luego lo insertamos en ANDES
+                let matchSisaResult: any = await matchSisa(pac);
+                let porcentajeMatcheo: number = matchSisaResult.matcheos.matcheo;
 
-
-                try {
-                    let matchSisaResult: any = await matchSisa(pac);
-                    let porcentajeMatcheo: number = matchSisaResult.matcheos.matcheo;
-                    console.log('----------------------->', matchSisaResult.matcheos.matcheo);
-
-                    if (porcentajeMatcheo < 95) {
-                        pac.estado = 'temporal';
-                    } else {
-                        pac.nombre = matchSisaResult.matcheos.datosPaciente.nombre;
-                        pac.apellido = matchSisaResult.matcheos.datosPaciente.apellido;
-                    }
-                    let pacienteAndes = new paciente(pac.toObject());
-                    Auth.audit(pacienteAndes, (userScheduler as any));
-                    await pacienteAndes.save();
-                    await pacienteMpi.deleteOne({ _id: pac._id }).exec();
-                    const nuevoPac = JSON.parse(JSON.stringify(pacienteAndes));
-                    const connElastic = new ElasticSync();
-                    await connElastic.sync(nuevoPac);
-                } catch (error) {
-                    console.log('ERROR', error);
+                if (porcentajeMatcheo < 95) {
+                    pac.estado = 'temporal';
+                } else {
+                    pac.nombre = matchSisaResult.matcheos.datosPaciente.nombre;
+                    pac.apellido = matchSisaResult.matcheos.datosPaciente.apellido;
                 }
+                let pacienteAndes = new paciente(pac.toObject());
+                Auth.audit(pacienteAndes, (userScheduler as any));
+                await pacienteAndes.save();
+                await pacienteMpi.deleteOne({ _id: pac._id }).exec();
+                const nuevoPac = JSON.parse(JSON.stringify(pacienteAndes));
+                const connElastic = new ElasticSync();
+                await connElastic.sync(nuevoPac);
             }
+        }).catch(err => {
+            // console.log('ERROR', err);
+            done();
         });
-        console.log('----------------------------------------------------------FIN----------------------------------------------------------', countPacienteError);
+        // console.log('----------------------------------------------------------FIN----------------------------------------------------------', countPacienteError);
         done();
 
     } catch (error) {
-        console.log('ERROR', error);
+        // console.log('ERROR', error);
         done();
     }
 }
