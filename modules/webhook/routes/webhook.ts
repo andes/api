3c970cfa6a4a4d6ca53928cbb4ac1f2f3f20e2fc
Hugo <hfernandez@neuquen.gov.ar>
2018-09-18 20:57:38 -0300

    more linting

--- modules/webhook/routes/webhook.ts
+++ modules/webhook/routes/webhook.ts
@@ -16,7 +16,7 @@ function filterData(filter: any[], data) {
     return true;
 }
 
-EventCore.on(/.*/, async function (body) {
+EventCore.on(/.*/, async (body) => {
     const event = this.event;
     let subscriptions = await WebHook.find({
         event
