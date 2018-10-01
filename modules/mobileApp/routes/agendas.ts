cfaeb5b28c409cb6ced9db9df329319a8b3acac0
Hugo <hfernandez@neuquen.gov.ar>
2018-10-01 13:58:52 -0300

    obtenemos informacion de localizacion

--- modules/mobileApp/routes/agendas.ts
+++ modules/mobileApp/routes/agendas.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as agenda from '../../turnos/schemas/agenda';
+import { organizacionCache } from '../../../core/tm/schemas/organizacionCache';
+import * as organizacion from '../../../core/tm/schemas/organizacion';
 import * as agendaCtrl from '../../turnos/controller/agenda';
 import { Auth } from './../../../auth/auth.class';
 import { Logger } from '../../../utils/logService';
@@ -8,6 +10,7 @@ import * as recordatorioController from '../controller/RecordatorioController';
 import { LoggerPaciente } from '../../../utils/loggerPaciente';
 import { toArray } from '../../../utils/utils';
 import * as moment from 'moment';
+import { forEach } from 'async';
 
 const router = express.Router();
 
@@ -23,6 +26,8 @@ router.get('/agendasDisponibles', async (req: any, res, next) => {
 
     const pacienteId = req.user.pacientes[0].id;
 
+
+
     if (req.query.horaInicio) {
         matchAgendas['horaInicio'] = { $gte: new Date(moment().format('YYYY-MM-DD')) };
     }
@@ -33,27 +38,33 @@ router.get('/agendasDisponibles', async (req: any, res, next) => {
     pipelineAgendas.push({ $match: matchAgendas });
     pipelineAgendas.push({
         $group: {
-            _id: { id: '$organizacion.nombre' },
+            _id: { id: '$organizacion._id' },
+            id: { $first: '$organizacion._id' },
+            organizacion: { $first: '$organizacion.nombre' },
             agendas: { $push: '$$ROOT' }
         }
     });
     pipelineAgendas.push({
         $sort: { 'agendas.horaInicio': 1 }
     });
-
     const agendasResultado = await toArray(agenda.aggregate(pipelineAgendas).cursor({}).exec());
+    // URGENTE: Unificar la información de organizacion y organización cache, ahora hago esta búsqueda para obtener tanto el punto gps como la dirección según sisa.
     const promisesStack = [];
-
-    if (promisesStack.length === 0) {
-        promisesStack.push(Promise.resolve());
-    }
-
-    Promise.all(promisesStack).then(() => {
+    try {
+        for (let i = 0; i <= agendasResultado.length - 1; i++) {
+            const org: any = await organizacion.model.findById(agendasResultado[i].id);
+            if (org.codigo && org.codigo.sisa) {
+                const orgCache: any = await organizacionCache.findOne({ codigo: org.codigo.sisa });
+                agendasResultado[i].coordenadasDeMapa = orgCache.coordenadasDeMapa;
+                agendasResultado[i].domicilio = orgCache.domicilio;
+                promisesStack.push(orgCache);
+            }
+        }
+        await Promise.all(promisesStack);
         res.json(agendasResultado);
-    }).catch((err) => {
+    } catch (err) {
         res.status(422).json({ message: err });
-    });
-
+    }
 
 });
 
