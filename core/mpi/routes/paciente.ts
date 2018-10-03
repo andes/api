132e52e1156ceccc72bb350d1c04024703be539c
Lautaro Molina <negro89_nqn@hotmail.com>
2018-10-03 14:49:30 -0300

    geocodificacion de un paciente validado

--- core/mpi/routes/paciente.ts
+++ core/mpi/routes/paciente.ts
@@ -337,9 +337,9 @@ router.get('/pacientes/:id', (req, res, next) => {
     }
     controller.buscarPaciente(req.params.id).then((resultado: any) => {
         if (resultado) {
-            Logger.log(req, 'mpi', 'query', {
-                mongoDB: resultado.paciente
-            });
+            // Logger.log(req, 'mpi', 'query', {
+            //     mongoDB: resultado.paciente
+            // });
             res.json(resultado.paciente);
         } else {
             return next(500);
@@ -446,7 +446,7 @@ router.put('/pacientes/mpi/:id', (req, res, next) => {
 
     const match = new Matching();
 
-    pacienteMpi.findById(query, (err, patientFound: any) => {
+    pacienteMpi.findById(query, async (err, patientFound: any) => {
         if (err) {
             return next(404);
         }
@@ -454,16 +454,27 @@ router.put('/pacientes/mpi/:id', (req, res, next) => {
         const connElastic = new ElasticSync();
         if (patientFound) {
             const data = req.body;
-            controller.updatePacienteMpi(patientFound, data, req).then((p) => {
+            // se carga geo referencia desde api de google
+            if (patientFound.estado === 'validado' && patientFound.direccion[0].valor !== data.direccion[0].valor) {
+                let geoRef: any = await controller.geoRefPaciente(patientFound);
+                patientFound.direccion[0].geoReferencia = [geoRef.lat, geoRef.lng];
+            }
+
+            controller.updatePacienteMpi(patientFound, data, req).then(async (p: any) => {
                 res.json(p);
             }).catch(next);
-
         } else {
-            const newPatient = new pacienteMpi(req.body);
+            const newPatient: any = new pacienteMpi(req.body);
             const claves = match.crearClavesBlocking(newPatient);
             newPatient['claveBlocking'] = claves;
             newPatient['apellido'] = newPatient['apellido'].toUpperCase();
             newPatient['nombre'] = newPatient['nombre'].toUpperCase();
+
+            if (newPatient.estado === 'validado') {
+                // se carga geo referencia desde api de google
+                let geoRef: any = await controller.geoRefPaciente(newPatient);
+                newPatient.direccion[0].geoReferencia = [geoRef.lat, geoRef.lng];
+            }
 
             Auth.audit(newPatient, req);
             newPatient.save((err2) => {
@@ -474,16 +485,16 @@ router.put('/pacientes/mpi/:id', (req, res, next) => {
                 delete nuevoPac._id;
 
                 connElastic.create(newPatient._id.toString(), nuevoPac).then(() => {
+
                     Logger.log(req, 'mpi', 'elasticInsertInPut', newPatient);
-                    res.json(newPatient);
+                    res.json();
                 }).catch(error => {
                     return next(error);
                 });
 
+
             });
         }
-
-
     });
 });
 
@@ -638,7 +649,7 @@ router.put('/pacientes/:id', (req, res, next) => {
         _id: objectId
     };
 
-    paciente.findById(query, (err, patientFound: any) => {
+    paciente.findById(query, async (err, patientFound: any) => {
         if (err) {
             return next(404);
         }
@@ -651,14 +662,29 @@ router.put('/pacientes/:id', (req, res, next) => {
                 delete data.sexo;
                 delete data.fechaNacimiento;
             }
-            controller.updatePaciente(patientFound, data, req).then((p) => {
+            if (patientFound.estado === 'validado' && patientFound.direccion[0].valor !== data.direccion[0].valor) {
+                // se carga geo referencia desde api de google
+                let geoRef: any = await controller.geoRefPaciente(patientFound);
+                patientFound.direccion[0].geoReferencia = [geoRef.lat, geoRef.lng];
+            }
+
+            controller.updatePaciente(patientFound, data, req).then((p: any) => {
+
                 res.json(p);
             }).catch(next);
 
         } else {
             try {
                 req.body._id = req.body.id;
+
+                if (req.body.estado === 'validado') {
+                    // se carga geo referencia desde api de google
+                    let geoRef: any = await controller.geoRefPaciente(req.body);
+                    req.body.direccion[0].geoReferencia = [geoRef.lat, geoRef.lng];
+                }
+
                 const newPatient = new paciente(req.body);
+
                 // verifico si el paciente ya está en MPI
                 pacienteMpi.findById(query, (err3, patientFountMpi: any) => {
                     if (err3) {
@@ -674,6 +700,7 @@ router.put('/pacientes/:id', (req, res, next) => {
                         const nuevoPac = JSON.parse(JSON.stringify(newPatient));
                         // delete nuevoPac._id;
                         // delete nuevoPac.relaciones;
+
                         const connElastic = new ElasticSync();
                         connElastic.sync(newPatient).then(updated => {
                             if (updated) {
@@ -828,11 +855,18 @@ router.patch('/pacientes/:id', (req, res, next) => {
                 pacienteAndes = resultado.paciente;
             }
             Auth.audit(pacienteAndes, req);
+            if (pacienteAndes.estado === 'validado') {
+                // se carga geo referencia desde api de google
+                let geoRef: any = await controller.geoRefPaciente(pacienteAndes);
+                pacienteAndes.direccion[0].geoReferencia = [geoRef.lat, geoRef.lng];
+            }
+
             pacienteAndes.save((errPatch) => {
                 if (errPatch) {
                     return next(errPatch);
                 }
                 return res.json(pacienteAndes);
+
             });
         }
     }).catch((err) => {
@@ -851,12 +885,12 @@ router.patch('/pacientes/mpi/:id', (req, res, next) => {
                 case 'updateCuil':
                     controller.updateCuil(req, resultado.paciente);
                     break;
-
             }
             let pacMpi: any;
             if (resultado.db === 'mpi') {
                 pacMpi = new pacienteMpi(resultado.paciente);
                 Auth.audit(pacMpi, req);
+
                 pacMpi.save((errPatch) => {
                     if (errPatch) {
                         return next(errPatch);
