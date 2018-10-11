2ce6d55b945ccab236a3275f7fb31d6c1aa52e49
ndinolfo <nicodinolfo@gmail.com>
2018-10-11 14:44:41 -0300

    agrego log al regex chequer

--- core/mpi/controller/regexChecker.ts
+++ core/mpi/controller/regexChecker.ts
@@ -5,6 +5,7 @@ import { userScheduler } from '../../../config.private';
 import { Auth } from '../../../auth/auth.class';
 const regtest = /[^a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ ']+/;
 import { matchSisa } from '../../../utils/servicioSisa';
+import { Logger } from '../../../utils/logService';
 /**
  * Busca pacientes validados en MPI con errores de charset en nombre y apellido,
  *  intenta corregir los errores con SISA y si no puede los deja como temporales en la bd de temporales.
@@ -18,39 +19,53 @@ export async function regexChecker(done) {
         let countPacienteError = 0;
 
         await cursor.eachAsync(async (pac: any) => {
-            let band = regtest.test(pac.nombre);
-            band = band || regtest.test(pac.apellido);
-            if (band) {
-                countPacienteError = countPacienteError + 1;
+            const pacienteOld = pac;
+            countPacienteError = countPacienteError + 1;
 
-                // Obtenemos el paciente con errores desde MPI , lo borramos y le cambiamos el estado a temporal
-                // luego lo insertamos en ANDES
-                let matchSisaResult: any = await matchSisa(pac);
-                let porcentajeMatcheo: number = matchSisaResult.matcheos.matcheo;
+            // Obtenemos el paciente con errores desde MPI , lo borramos y le cambiamos el estado a temporal
+            // luego lo insertamos en ANDES
+            let matchSisaResult: any = await matchSisa(pac);
+            let porcentajeMatcheo: number = matchSisaResult.matcheos.matcheo;
 
-                if (porcentajeMatcheo < 95) {
-                    pac.estado = 'temporal';
-                } else {
-                    pac.nombre = matchSisaResult.matcheos.datosPaciente.nombre;
-                    pac.apellido = matchSisaResult.matcheos.datosPaciente.apellido;
-                }
-                let pacienteAndes = new paciente(pac.toObject());
-                Auth.audit(pacienteAndes, (userScheduler as any));
-                await pacienteAndes.save();
-                await pacienteMpi.deleteOne({ _id: pac._id }).exec();
-                const nuevoPac = JSON.parse(JSON.stringify(pacienteAndes));
-                const connElastic = new ElasticSync();
-                await connElastic.sync(nuevoPac);
+            if (porcentajeMatcheo < 95) {
+                pac.estado = 'temporal';
+            } else {
+                pac.nombre = matchSisaResult.matcheos.datosPaciente.nombre;
+                pac.apellido = matchSisaResult.matcheos.datosPaciente.apellido;
             }
+            let pacienteAndes = new paciente(pac.toObject());
+            Auth.audit(pacienteAndes, (userScheduler as any));
+            await pacienteAndes.save();
+            await pacienteMpi.deleteOne({ _id: pac._id }).exec();
+            const nuevoPac = JSON.parse(JSON.stringify(pacienteAndes));
+            const connElastic = new ElasticSync();
+            await connElastic.sync(nuevoPac);
+            Logger.log(userScheduler, 'scheduler', 'regexCheck', {
+                accion: 'fix nombres',
+                id_paciente: pac._id,
+                nombre_old: pacienteOld.nombre,
+                apellido_old: pacienteOld.apellido,
+                nombre: pac.nombre,
+                apellido: pac.apellido
+            });
+
         }).catch(err => {
             // console.log('ERROR', err);
+            Logger.log(userScheduler, 'scheduler', 'regexCheck', {
+                accion: 'error',
+                error: err
+            });
             done();
         });
         // console.log('----------------------------------------------------------FIN----------------------------------------------------------', countPacienteError);
         done();
 
-    } catch (error) {
+    } catch (err) {
         // console.log('ERROR', error);
+        Logger.log(userScheduler, 'scheduler', 'regexCheck', {
+            accion: 'error',
+            error: err
+        });
         done();
     }
 }
