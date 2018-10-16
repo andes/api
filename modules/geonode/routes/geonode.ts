4d9639d9c90518473476f6355311d1335b4958dd
Lautaro Molina <negro89_nqn@hotmail.com>
2018-10-16 11:03:42 -0300

    Dado un Point de cordenadas, obtiene el nombre del barrio desde WS de geonode.

--- modules/geonode/routes/geonode.ts
+++ modules/geonode/routes/geonode.ts
@@ -3,33 +3,38 @@ import { nextTick } from 'async';
 
 const router = express.Router();
 
-router.get('/geonode', (req, res) => {
-    // console.log('query: ', req.query.point);
+router.get('/geonode', (req, res, next) => {
     if (req.query.point) {
+        let geoRef = [Number(req.query.point[1]), Number(req.query.point[0])];
+
+        // Se realiza la conversión de las coordenadas desde mercator a gauss-krüger mediante la lib 'proj4' (http://proj4js.org/)
+        let proj4 = require('proj4');
+        let fromProjection = '+title=*GPS (WGS84) (deg) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees';
+        let toProjection = '+proj=tmerc +lat_0=-90 +lon_0=-69 +k=1 +x_0=2500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs';
+        let geoRefGK = proj4(fromProjection, toProjection, geoRef); // geo-referencia en coordenadas gauss-krüger
+        let geoBox = (geoRefGK[0] - 10) + '%2C' + (geoRefGK[1] - 10) + '%2C' + (geoRefGK[0] + 10) + '%2C' + (geoRefGK[1] + 10); // Polígono con las coordenadas obtenidas
+
+        // Se setea el curl-request
         const curl = new (require('curl-request'))();
         let user = 'admin';
         let pass = 'geonode';
         let auth = new Buffer(user + ':' + pass).toString('base64');
-        let fc = [2575161.89379, 5696513.34145];    // factor de corrección entre point de google y layer de geonode
-        let dp = 963.93719;   // distancia aceptada por la layer para delimitar un polígono
-        let geoRef = [Number(req.query.point[0]) + fc[0], Number(req.query.point[1]) + fc[1]];
-        // console.log('lat: ', geoRef[0]);
-        // console.log('lng: ', geoRef[1]);
-        let geoBox = (geoRef[0] - dp) + '%2C' + (geoRef[1] - dp) + '%2C' + (geoRef[0] + dp) + '%2C' + (geoRef[1] + dp);
         let url = encodeURI('http://geosalud.neuquen.gov.ar/geoserver/geonode/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=geonode%3Abarrios&LAYERS=geonode%3Abarrios&STYLES=barrios&FORMAT_OPTIONS=antialias%3Atext&INFO_FORMAT=application/json&FEATURE_COUNT=50&X=50&Y=50&SRS=EPSG%3A22182&WIDTH=101&HEIGHT=101&BBOX=' + geoBox);
 
-        curl.setHeaders(['Authorization: Basic ' + auth])
+        curl.setHeaders(['Authorization: Basic ' + auth, 'timeout : 2L'])
             .get(decodeURI(url))
             .then(({ statusCode, body, headers }) => {
-                console.log(geoBox);
-                res.json(JSON.parse(body).features[0].properties.NOMBRE);     // -38.9499593 (2584663.97094) -68.0226571 (5688563.20981) |
-            })                                                                // -38.9195472 (+2579200,8133372) -68.08712434 (+5692081,4131369) | 2579161.89379 5692013.34145
+                if (body.features) {
+                    res.json(JSON.parse(body).features[0].properties.NOMBRE);
+                } else {
+                    res.json('');
+                }
+            })
             .catch((e) => {
                 return e;
             });
+    } else {
+        return next('Parámetros incorrectos.');
     }
 });
-// Point de ej. en layer: (2578579.71390, 5686685.44107) | 2577615.776704708  5685721.5038770735  2579543.6510929214  5687649.378265288  MILITAR
-// Point de ej. en layer: 2580507.58829, 5690025.81749 alta barda
-// fc  2579161.89379, 5692013.34145
 export = router;
