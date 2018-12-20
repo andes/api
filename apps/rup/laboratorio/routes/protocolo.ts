fe0dff2d69b5a2c242707f36fee6f1b191d498c9
cesar linux <cesarlinux@gmail.com>
2018-12-20 14:22:51 -0300

    prefijo de organizacion nueva version y rutas de practicas de cobas

--- apps/rup/laboratorio/routes/protocolo.ts
+++ apps/rup/laboratorio/routes/protocolo.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
 import { model as Organizacion } from '../../../../core/tm/schemas/organizacion';
-import { getUltimoNumeroProtocolo } from '../controller/protocolo';
-import { getResultadosAnteriores } from '../controller/protocolo';
+import { model as Prestacion } from '../../../../modules/rup/schemas/prestacion';
+
+import { getUltimoNumeroProtocolo, getResultadosAnteriores, getPracticasCobasC311 } from '../controller/protocolo';
+import { Auth } from '../../../../auth/auth.class';
+import { stringify } from 'querystring';
+import { Types } from 'mongoose';
+
 
 let router = express.Router();
 
@@ -13,7 +18,7 @@ router.get('/protocolo/numero/', async (req, res, next) => {
     let prefijo;
     try {
         Organizacion.findOne(idEfector).then((organizacion: any) => {
-            prefijo = organizacion.prefijosLab.prefijo;
+            prefijo = organizacion.prefijo;
             ultimoNumeroProtocolo++;
             let nuevoNumeroProtocolo = {
                 numeroCompleto: ultimoNumeroProtocolo + '-' + prefijo + anio,
@@ -33,6 +38,70 @@ router.get('/practicas/resultadosAnteriores', async (req, res, next) => {
     let resultadosAnteriores = await getResultadosAnteriores(idPaciente, practicaConceptId);
 
     res.json(resultadosAnteriores);
+});
+
+router.get('/practicas/cobasc311', async (req, res, next) => {
+    // if (!Auth.check(req, 'laboratorio:analizador:*')) {
+    //     return next(403);
+    // }
+    let practicasCobas = await getPracticasCobasC311();
+    res.json(practicasCobas);
+});
+
+router.get('/practicas/cobasc311/:id', async (req, res, next) => {
+    Prestacion.findById(req.params.id, (err, data: any) => {
+        if (err) {
+            return next(err);
+        }
+        if (data) {
+            res.json(data);
+        }
+    });
+});
+
+router.patch('/practicas/cobasc311/:id', async (req, res, next) => {
+    // console.log('req.params:', req.params);
+    // console.log('req.body:', req.body.valor.resultado);
+    // console.log('req.body._id:', req.body._id);
+
+    Prestacion.findById(req.params.id, (err, data: any) => {
+        if (err) {
+            return next(err);
+        }
+        if (data) {
+
+            let id = new Types.ObjectId(req.body._id);
+            // console.log('id:', id);
+            // console.log(data.ejecucion.registros);
+            let index = data.ejecucion.registros.findIndex(x => {return  x._id.toString() === id.toString(); });
+            // console.log('index: ', index);
+
+            data.ejecucion.registros[index].valor = req.body.valor;
+            Auth.audit(data, req);
+            console.log(JSON.stringify(data.ejecucion.registros[index]));
+            data.save();
+            return res.json(data);
+        }
+    });
+
+    // let search = { _id: req.params.id, 'ejecucion.registros._id': req.body._id };
+    // let values = {
+    //     $set: {
+    //         'ejecucion.$.registros': req.body
+    //     }
+    // };
+    // let options = { runValidators: false };
+
+    // return Prestacion.findOneAndUpdate(search, values, options, (err, data: any) => {
+    //     if (err) {
+    //         return next(err);
+    //     }
+    //     if (data) {
+    //         Auth.audit(data, req);
+    //         data.save();
+    //         return res.json(data);
+    //     }
+    // });
 });
 
 export = router;
