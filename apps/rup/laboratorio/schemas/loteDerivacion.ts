06dd5ca5b0c337bb2b4e3b8475929c2dc5b5834b
cesar linux <cesarlinux@gmail.com>
2019-02-11 09:23:13 -0300

    agrego campos faltantes al schema

--- apps/rup/laboratorio/schemas/loteDerivacion.ts
+++ apps/rup/laboratorio/schemas/loteDerivacion.ts
@@ -2,13 +2,34 @@ import { model, Schema } from 'mongoose';
 import * as Registro from './../../../../modules/rup/schemas/prestacion.registro';
 
 export let schema = new Schema({
-    laboratorioOrigen: { type: Schema.Types.ObjectId, ref: 'organizacion', required: true },
-    laboratorioDestino: { type: Schema.Types.ObjectId, ref: 'organizacion', required: true },
+    numeroLote: { type: String, required: true },
+    fecha: { type: Date, required: true },
+    laboratorioOrigen:
+    {
+        nombre: { type: String, required: true },
+        id: { type: Schema.Types.ObjectId, ref: 'organizacion', required: true }
+    },
+    laboratorioDestino: {
+        nombre: { type: String, required: true },
+        id: { type: Schema.Types.ObjectId, ref: 'organizacion', required: true }
+    },
+    estado: {
+        type: String,
+        enum: ['preparado', 'en transporte', 'recibido']
+    },
     prestaciones: [{
         idPrestacion: String,
         numeroProtocolo: String,
+        paciente: {
+            id: { type: Schema.Types.ObjectId, ref: 'paciente', required: true },
+            documento: String,
+            apellido: String,
+            nombre: String
+        },
         registro: Registro
-    }]
+    }],
+    usuarioResponsablePreparacion: String,
+    usuarioEntrega: String
 });
 schema.plugin(require('../../../../mongoose/audit'));
 
