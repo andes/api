44ce73f81db4d22ae81857b57a205a1bcdd4ec56
ndinolfo <nicodinolfo@gmail.com>
2019-02-19 08:22:59 -0300

    valida que realmente se encuentre el paciente en mpi

--- core/mpi/jobs/elasticFix.ts
+++ core/mpi/jobs/elasticFix.ts
@@ -41,34 +41,36 @@ export async function elasticFix(done) {
                         let prestacionesPacienteElastic = await prestacionModel.find({ 'paciente.id': idElasticPaciente });
                         let agendasPacienteElastic = await agendaModel.find({ 'bloques.turnos.paciente.id': idElasticPaciente }).cursor();
                         let pacienteMongo = await pacienteMpi.findOne({ documento: hit.documento });
-                        try {
+                        if (pacienteMongo) {
+                            try {
 
-                            if (prestacionesPacienteElastic && prestacionesPacienteElastic.length > 0) {
-                                for (let prestacion of prestacionesPacienteElastic) {
-                                    dbg('Prestacion modificada---> ', (prestacion as any)._id);
-                                    (prestacion as any).paciente.id = pacienteMongo._id;
-                                    await prestacion.save();
-                                }
-                            }
-
-                            await agendasPacienteElastic.eachAsync(async agenda => {
-                                let turnos;
-                                let index = -1;
-                                for (let x = 0; x < (agenda as any).bloques.length; x++) {
-                                    // Si existe este bloque...
-                                    turnos = (agenda as any).bloques[x].turnos;
-                                    index = turnos.findIndex((t) => t.paciente._id.toString() === idElasticPaciente.toString());
-                                    if (index > -1) {
-                                        dbg('agenda modificada---> ', (agenda as any)._id);
-                                        (agenda as any).bloques[x].turnos[index].paciente.id = pacienteMongo._id;
-                                        await agenda.save();
+                                if (prestacionesPacienteElastic && prestacionesPacienteElastic.length > 0) {
+                                    for (let prestacion of prestacionesPacienteElastic) {
+                                        dbg('Prestacion modificada---> ', (prestacion as any)._id);
+                                        (prestacion as any).paciente.id = pacienteMongo._id;
+                                        await prestacion.save();
                                     }
                                 }
-                            });
-                            await connElastic.delete(idElasticPaciente);
-                        } catch (error) {
-                            dbg('ERROR -------------------->', error);
-                            await log(userScheduler, logKeys.elasticFix.key, hit, logKeys.elasticFix.operacion, pacienteMongo, idElasticPaciente);
+
+                                await agendasPacienteElastic.eachAsync(async agenda => {
+                                    let turnos;
+                                    let index = -1;
+                                    for (let x = 0; x < (agenda as any).bloques.length; x++) {
+                                        // Si existe este bloque...
+                                        turnos = (agenda as any).bloques[x].turnos;
+                                        index = turnos.findIndex((t) => t.paciente._id.toString() === idElasticPaciente.toString());
+                                        if (index > -1) {
+                                            dbg('agenda modificada---> ', (agenda as any)._id);
+                                            (agenda as any).bloques[x].turnos[index].paciente.id = pacienteMongo._id;
+                                            await agenda.save();
+                                        }
+                                    }
+                                });
+                                await connElastic.delete(idElasticPaciente);
+                            } catch (error) {
+                                dbg('ERROR -------------------->', error);
+                                await log(userScheduler, logKeys.elasticFix.key, hit, logKeys.elasticFix.operacion, pacienteMongo, idElasticPaciente);
+                            }
                         }
                     }
                 }
