c7bc5a515415019d9c28b1b87ab8bf16006e57cb
Mariano Botta <marianoabotta@gmail.com>
2019-03-14 13:58:22 -0300

    Update modules/descargas/controller/descargas.ts
    Co-Authored-By: condorpiedra <natalia.huenchuman@gmail.com>

--- modules/descargas/controller/descargas.ts
+++ modules/descargas/controller/descargas.ts
@@ -21,7 +21,6 @@ if (env.NODE_ENV !== 'production') {
     // tslint:disable-next-line:no-console
     process.on('unhandledRejection', r => console.log(r));
     // tslint:disable-next-line:no-console
-    // process.on('TypeError', r => console.log(r));
 }
 
 export class Documento {
