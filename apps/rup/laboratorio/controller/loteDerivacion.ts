2e942250ae806b0eb0ae70c21eeec2a3d5392f99
Pablo Lammel <lammelpablo@gmail.com>
2019-03-20 13:26:46 -0300

    patch de loteDerivacion / update registro ejecucion protocolo

--- apps/rup/laboratorio/controller/loteDerivacion.ts
+++ apps/rup/laboratorio/controller/loteDerivacion.ts
@@ -1,4 +1,37 @@
 
+import { EventCore } from '@andes/event-bus';
+import { LoteDerivacion } from './../schemas/loteDerivacion';
+/**
+ *
+ *
+ * @export
+ * @param {*} req
+ * @returns
+ */
+export async function updateLote(req) {
+    let result = await LoteDerivacion.updateOne(
+        { _id: req.params.id },
+        { $push: { estados : req.body.estado } }
+    );
+
+    if (req.body.estado.tipo === 'enviado') {
+        let loteDerivacion: any = await LoteDerivacion.findById(req.params.id);
+
+        EventCore.emitAsync('rup:laboratorio:prestacion:derivar', {
+            idOrganizacionOrigen: req.body.idOrganizacion,
+            derivaciones: loteDerivacion.itemsLoteDerivacion
+        });
+    }
+
+    return result;
+}
+
+/**
+ *
+ *
+ * @export
+ * @returns
+ */
 export function generarNumeroLoteDerivacion() {
     return 'XXX';
 }
