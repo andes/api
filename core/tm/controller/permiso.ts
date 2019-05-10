199b7b42663140bbba4059c6366866af7feaa046
LucasGonzalez <lucasgonzalezm3@gmail.com>
2019-05-10 15:32:08 -0300

    Agrega ruta la config

--- core/tm/controller/permiso.ts
+++ core/tm/controller/permiso.ts
@@ -10,13 +10,13 @@ import { Types } from 'mongoose';
  */
 export async function actualizarPermisosUsuario(idUsuario, idOrganizacion, modulo, nuevosPermisos) {
     let usuario: any = await authUsers.findOne({ _id: idUsuario });
-    let organizacionPermisos = usuario.organizaciones.find( o => o._id.toString() === Types.ObjectId(idOrganizacion).toString() );
+    let organizacionPermisos = usuario.organizaciones.find(o => o._id.toString() === Types.ObjectId(idOrganizacion).toString());
 
     if (!organizacionPermisos) {
-        usuario.organizaciones.push( { _id: idOrganizacion, permisos: nuevosPermisos });
+        usuario.organizaciones.push({ _id: idOrganizacion, permisos: nuevosPermisos }); // TODO: agregar ', activo: true' cuando se mergee el nuevo gestor de usuarios
     } else {
         organizacionPermisos.permisos = organizacionPermisos.permisos ?
-            organizacionPermisos.permisos.filter( p => p.indexOf(modulo) !== 0).concat(nuevosPermisos) : nuevosPermisos;
+            organizacionPermisos.permisos.filter(p => p.indexOf(modulo) !== 0).concat(nuevosPermisos) : nuevosPermisos;
     }
 
     return await usuario.save();
