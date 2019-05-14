14f3a2f04fa890d0058e6b121f72dafe3e3d14da
Mariano Andres Botta <marianoabotta@gmail.com>
2019-05-14 14:51:42 -0300

    feat(mpi): add GET and POST paciente routes
    Implementa las rutas:
    - GET /paciente
    - POST /pacientes
    - GET /pacientes/:id

--- core_v2/mpi/pacientes/paciente.routes.ts
+++ core_v2/mpi/pacientes/paciente.routes.ts
@@ -1,3 +1,71 @@
-export function postPaciente(req, res) {
+import { Router , Request, Response} from 'express';
+import { findById, findPaciente, search, suggest, createPaciente } from './paciente.controller';
+import { mpi } from '../../../config';
 
+const router = Router();
+
+export const findPacientes = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const options = {
+        fields: req.query.fields
+    };
+    const paciente = await findById(id, options);
+    res.json(paciente);
+};
+
+export const getPacientes = async (req: Request, res: Response) => {
+    const options = {
+        fields: req.query.fields
+    };
+    if (req.query.search) {
+        const pacientes = await search(req.query.search);
+        res.json(pacientes);
+    } else {
+        const conditions = req.query;
+        const pacientes = await findPaciente(conditions, options.fields);
+        res.json(pacientes);
+    }
+};
+
+/**
+ * Chequea si hay algun paciente con maching superior a la cota maxima de aceptacion.
+ */
+
+function isMatchingAlto(sugeridos: any[]) {
+    return sugeridos.some((paciente) => paciente._score > mpi.cotaMatchMax);
 }
+
+export const postPacientes = async (req: Request, res: Response, next) => {
+    const body = req.body;
+    if (body.documento) {
+        const sugeridos = await suggest(body);
+        if (isMatchingAlto(sugeridos)) {
+            return next(422);
+        }
+    }
+    body.activo = true;
+    const pacienteCreado = await createPaciente(body, req);
+    res.json(pacienteCreado);
+};
+
+export const postMatch = (req, res, next) => {
+
+};
+
+export const putPacientes = (req, res, next) => {
+    return next(500);
+};
+
+export const patchPacientes = (req, res, next) => {
+
+};
+
+export const deletePacientes = (req, res, next) => {
+
+};
+
+
+router.get('/pacientes', getPacientes);
+router.get('/pacientes/:id', findPacientes);
+
+export const Routing = router;
