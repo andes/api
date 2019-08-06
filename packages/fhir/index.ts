5793666f6edfe16b1890cc92d64ef4376fae8c9f
Hugo <hfernandez@neuquen.gov.ar>
2019-08-06 18:13:29 -0300

    refactor(FHIR): Removemos paquete local Fhir, reemplazandolo por @andes/fhir.

--- packages/fhir/index.ts
+++
@@ -1,8 +0,0 @@
-import * as Patient from './src/patient';
-import * as Practitioner from './src/practitioner';
-import * as Organization from './src/organization';
-
-
-export {
-    Patient, Practitioner, Organization
-};
