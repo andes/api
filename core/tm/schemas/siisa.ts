32a625ff94408f2fa822894034d263f74345bdd1
nicodino <nicodinolfo@gmail.com>
2019-09-19 10:36:42 -0300

    feat(especialidades): agrega interfaz y schema de especialidad

--- core/tm/schemas/siisa.ts
+++ core/tm/schemas/siisa.ts
@@ -15,14 +15,27 @@ export interface SIISAObject extends Document {
     tipoDeFormacion: String;
     habilitado: boolean;
 }
+export interface SIISAEspecialidad extends Document {
+    nombre: string;
+    codigo: { sisa: number };
+    tipoDeFormacion: String;
+    habilitado: boolean;
+}
 
+export const EspecialidadSIISASchema = new Schema({
+    nombre: String,
+    codigo: { sisa: Number },
+    tipoDeFormacion: String,
+    habilitado: Boolean,
+    profesionCodigoRef: Number
+});
 
 export const TipoDocumento = model<SIISAObject>('SIISATipoDocumento', ObjSIISASchema, 'siisa_tipoDocumento');
 export const Sexo = model<SIISAObject>('SIISASexo', ObjSIISASchema, 'siisa_sexo');
 export const Pais = model<SIISAObject>('SIISAPais', ObjSIISASchema, 'siisa_pais');
 export const Profesion = model<SIISAObject>('SIISAProfesion', ObjSIISASchema, 'profesion');
 export const EntidadFormadora = model<SIISAObject>('SIISAEntidadFormadora', ObjSIISASchema, 'siisa_entidadFormadora');
-export const Especialidad = model<SIISAObject>('SIISAEspecialidad', ObjSIISASchema, 'especialidad');
+export const Especialidad = model<SIISAEspecialidad>('SIISAEspecialidad', EspecialidadSIISASchema, 'especialidad');
 export const ModalidadCertificacionEspecialidad = model<SIISAObject>('SIISAModalidadCertificacionEspecialidad', ObjSIISASchema, 'siisa_modalidadCertificacionEspecialidad');
 export const EstablecimientoCertificador = model<SIISAObject>('SIISAEstablecimientoCertificador', ObjSIISASchema, 'siisa_establecimientoCertificador');
 
