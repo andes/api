795b0efae4a31409698e08fb25f1fa9fe43b14d4
silviroa <silviroa@gmail.com>
2019-10-30 14:44:35 -0300

    fix(auth): Agrega ruta refreshToken. Reutiliza ruta existente

--- auth/routes/routes.ts
+++ auth/routes/routes.ts
@@ -129,12 +129,6 @@ router.post('/login', async (req, res, next) => {
     }
 });
 
-
-/**
- * Refresca el token de un usuario
- * @param {string} token token de la cuenta
- * @post /api/auth/refreshToken
- */
 router.post('/refreshToken', Auth.authenticate(), async (req, res, next) => {
     try {
         const oldToken: string = req.body.token;
