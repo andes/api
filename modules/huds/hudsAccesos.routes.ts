50b2df2f994dccbeedc350828d491c2011053512
Gabo <cancellieri.gabriel@gmail.com>
2019-11-01 08:30:52 -0300

    feat(huds): nuevo endpoint para filtrar los accesos de los huds

--- modules/huds/hudsAccesos.routes.ts
+++ modules/huds/hudsAccesos.routes.ts
@@ -3,7 +3,7 @@ import { HudsAcceso } from './hudsAccesos.schema';
 import { Auth } from '../../auth/auth.class';
 import { MongoQuery, ResourceBase, ResourceNotFound } from '@andes/core';
 import { Request } from '@andes/api-tool';
-
+import * as utils from '../../utils/utils';
 
 class HudsAccesoResource extends ResourceBase {
     Model = HudsAcceso;
@@ -19,9 +19,9 @@ class HudsAccesoResource extends ResourceBase {
             field: 'paciente',
             fn: (value) => mongoose.Types.ObjectId(value)
         },
-        turno: {
-            field: 'turno',
-            fn: (value) => mongoose.Types.ObjectId(value)
+        anio: {
+            field: 'anio',
+            fn: (value) => value
         }
     };
 }
@@ -30,4 +30,51 @@ export const HudsAccesoRouter = HudsAccesoCtr.makeRoutes();
 
 HudsAccesoRouter.post('/huds-token', Auth.authenticate(), (req, res, next) => {
     return res.json({ token: Auth.generateHudsToken() });
+});
+
+HudsAccesoRouter.get('/accesos', Auth.authenticate(), async (req: any, res, next) => {
+    const filtros = {};
+
+    if (req.query.fechaDesde && req.query.fechaHasta) {
+        filtros['$and'] = [
+            { 'accesos.fecha': { $gte: new Date(req.query.fechaDesde) } },
+            { 'accesos.fecha': { $lte: new Date(req.query.fechaHasta) } }
+        ];
+    } else if (req.query.fechaDesde && !req.query.fechaHasta) {
+        filtros['$and'] = [
+            { 'accesos.fecha': { $gte: new Date(req.query.fechaDesde) } },
+        ];
+    } else if (!req.query.fechaDesde && req.query.fechaHasta) {
+        filtros['$and'] = [
+            { 'accesos.fecha': { $lte: new Date(req.query.fechaHasta) } }
+        ];
+    }
+
+    if (req.query.turno) {
+        filtros['accesos.turno'] = req.query.turno;
+    }
+
+    if (req.query.prestacion) {
+        filtros['accesos.prestacion'] = req.query.prestacion;
+    }
+
+    if (req.query.organizacion) {
+        filtros['accesos.organizacion.nombre'] = {
+            $regex: utils.makePattern(req.query.organizacion)
+        };
+    }
+
+    if (req.query.usuario) {
+        filtros['accesos.usuario.nombre'] = {
+            $regex: utils.makePattern(req.query.usuario)
+        };
+    }
+
+    try {
+        const huds: any = await HudsAcceso.find(filtros);
+
+        return res.json(huds);
+    } catch (err) {
+        return next(err);
+    }
 });
