7e88f7989dcefecae8a2cbb79c10b2e4163fba21
Natalia Huenchuman <natalia.huenchuman@gmail.com>
2019-11-15 09:14:38 -0300

    fix(carpetaPaciente): rutas return null

--- modules/carpetas/routes/ultimaCarpeta.ts
+++ modules/carpetas/routes/ultimaCarpeta.ts
@@ -1,49 +1,35 @@
 import * as express from 'express';
 import * as ultimaCarpeta from '../schemas/ultimaCarpeta';
 import * as mongoose from 'mongoose';
+import { Auth } from '../../../auth/auth.class';
 
 const router = express.Router();
 
-router.get('/ultimaCarpeta', (req: any, res, next) => {
-    if (req.user.organizacion && req.user.organizacion.id) {
-
+router.get('/ultimaCarpeta', async (req: any, res, next) => {
+    try {
         const idOrganizacion = new mongoose.Types.ObjectId(req.user.organizacion.id);
-        ultimaCarpeta.findOne({ idEfector: idOrganizacion }, (err, data: any) => {
-            if (err) {
-                return next(err);
-            }
-            if (data) {
-                data.ultimaCarpeta = data.ultimaCarpeta + 1;
-                res.json(data.ultimaCarpeta);
-            } else {
-                res.json(0);
-            }
-        });
+        const result: any = await ultimaCarpeta.findOne({ idEfector: idOrganizacion });
+        const ultima = (result && result.ultimaCarpeta) ? result.ultimaCarpeta + 1 : 0;
+        res.json(ultima);
+    } catch (error) {
+        return next(error);
     }
 });
 
-router.post('/incrementarCuenta', (req: any, res, next) => {
-    if (req.user.organizacion && req.user.organizacion.id) {
-        const idOrganizacion = new mongoose.Types.ObjectId(req.user.organizacion.id);
-        ultimaCarpeta.findOne({ idEfector: idOrganizacion }, (err, data: any) => {
-            if (err) {
-                return next(err);
-            }
-            if (data) {
-                const update = {
-                    ultimaCarpeta: data.ultimaCarpeta + 1
-                };
-                ultimaCarpeta.update({ idEfector: idOrganizacion }, { $set: update }, { new: true }, (errUpdate, dataUpdate) => {
-                    if (err) {
-                        return next(errUpdate);
-                    }
-                    res.json(dataUpdate);
-                });
-            } else {
-                res.json();
-            }
-        });
+router.post('/incrementarCuenta', async (req: any, res, next) => {
+    try {
+        const organizacion: any = Auth.getOrganization(req);
+        const idOrganizacion = new mongoose.Types.ObjectId(organizacion.id);
+        const result: any = await ultimaCarpeta.findOne({ idEfector: idOrganizacion });
+        const update: any = {
+            ultimaCarpeta: (result && result.ultimaCarpeta) ? result.ultimaCarpeta + 1 : 0
+        };
+        const updateCarpeta = await ultimaCarpeta.update({ idEfector: idOrganizacion }, { $set: update }, { new: true });
+        res.json(updateCarpeta);
+    } catch (error) {
+        return next(error);
     }
+
 });
 
 export = router;
