121c9b881bec1c1e8d521f4cfcc4a3c8e8c0438d
silviroa <silviroa@gmail.com>
2019-11-25 15:36:46 -0300

    fix(PUCO): refactor de rutas para comprobante sumar

--- modules/obraSocial/controller/sumar.ts
+++ modules/obraSocial/controller/sumar.ts
@@ -1,19 +1,10 @@
 import { sumar } from '../schemas/sumar';
 
-export async function pacienteSumar(documento) {
-    let rta: any = await sumar.findOne({ afidni: documento, activo: 'S ' }).exec();
-    const resultOS = [];
-    if (rta) {
-        resultOS[0] = { codigoPuco: null, nombre: null, financiador: 'SUMAR' };
-    }
-    return resultOS;
-}
-
 export async function getPacienteSumar(documento) {
     let rta: any = await sumar.findOne({ afidni: documento, activo: 'S ' }).exec();
     if (rta) {
-        return rta;
+        return [rta];
     } else {
-        return;
+        return [];
     }
 }
