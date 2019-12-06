e063ecdfdb24e45f4cfccbe3d6b3a9eff7b8750d
condorpiedra <natalia.huenchuman@gmail.com>
2019-12-06 11:35:29 -0300

    refactor(accesos): cambia  nombre de la ruta
    Co-Authored-By: Mariano Botta <marianoabotta@gmail.com>

--- modules/huds/hudsAccesos.routes.ts
+++ modules/huds/hudsAccesos.routes.ts
@@ -9,34 +9,24 @@ import { search } from './../../core/tm/controller/profesional';
 const router = express.Router();
 
 router.use(Auth.authenticate());
-router.get('/huds-accesos', asyncHandler(async (req: any, res) => {
+router.get('/accesos', asyncHandler(async (req: any, res) => {
     const result = await HudsAccesosCtr.search(req.query);
     res.json(result);
 }));
 
-router.post('/huds-accesos/huds-token', asyncHandler(async (req: any, res) => {
+router.post('/accesos/token', asyncHandler(async (req: any, res) => {
     // Persiste los datos de accesos y genera el token para acceder a la HUDS del paciente
     const organizacionId = Auth.getOrganization(req);
     // Busca la matricula del profesional
     const idProfesional = (req as any).user.profesional ? (req as any).user.profesional.id : null;
     let matricula = null;
     if (idProfesional) {
-        const datosProfesional: any  = await search({id: idProfesional}, { matricula: 1});
+        const datosProfesional: any = await search({ id: idProfesional }, { matricula: 1 });
         matricula = (datosProfesional.length && datosProfesional[0].matricula && datosProfesional[0].matricula.length) ? datosProfesional[0].matricula[0] : null;
     }
     logAcceso(req, req.body.paciente.id, matricula, req.body.motivo, req.body.idTurno, req.body.idPrestacion);
     return res.json({ token: Auth.generateHudsToken(req.body.usuario, organizacionId, req.body.paciente) });
 }));
 
-router.get('/huds-accesos/checkToken', Auth.authenticate(), (req: any, res, next) => {
-    let resp = false;
-    if (req.query.token && req.query.idPaciente) {
-        let tokenSettings = Auth.decode(req.query.token);
-        if (tokenSettings && String(tokenSettings.paciente) === String(req.query.idPaciente)) {
-            resp = true;
-        }
-    }
-    return res.json(resp);
-});
 
 export const HudsAccesoRouter = router;
