8926e6d075a442d56187f0af7372410ba26eb486
plammel <lammelpablo@gmail.com>
2020-01-22 14:52:15 -0300

    feat(TOP): Guarda historial de solicitud al liberarse turno

---
+++ modules/rup/schemas/prestacion.solicitud.historial.ts
@@ -0,0 +1,20 @@
+import { OrganizacionSchema } from './../../../core/tm/schemas/organizacion';
+import { tipoPrestacionSchema } from './../../../core/tm/schemas/tipoPrestacion';
+import { profesionalSchema } from './../../../core/tm/schemas/profesional';
+import * as turnoSchema from '../../../modules/turnos/schemas/turno';
+import * as mongoose from 'mongoose';
+import { AuditPlugin } from '@andes/mongoose-plugin-audit';
+
+export let PrestacionSolicitudHistorialschema = new mongoose.Schema({
+    tipoPrestacion: tipoPrestacionSchema,
+    turno: turnoSchema,
+    organizacion: OrganizacionSchema,
+    profesional: profesionalSchema,
+    accion: {
+        type: String,
+        enum: ['liberacionTurno', 'asignacionProfesional']
+    }
+});
+
+// Habilitar plugin de auditoría
+PrestacionSolicitudHistorialschema.plugin(AuditPlugin);
