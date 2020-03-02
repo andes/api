564b865973123b7d0fcf59614795d8cf56f722f7
damianl657 <damianl657@gmail.com>
2020-03-02 11:03:56 -0300

    fix (agenda): arreglo de linteo de codigo

--- modules/turnos/controller/agenda.ts
+++ modules/turnos/controller/agenda.ts
@@ -1436,19 +1436,19 @@ export async function verificarSolapamiento(data) {
                 let org: string; // nombre de la organizacion
                 let agenda_creada_por: string;
                 let prestaciones_agenta: string;
-                let prestaciones_agenta_ = [];
+                let prestaciones_agenda_ = [];
                 prestaciones_agenta = '';
                 for (let resultado of resultados) {
                     profesionales = profesionales.concat(resultado.profesionales);
                     org = resultado.organizacion.nombre;
                     agenda_creada_por = resultado.createdBy.nombreCompleto;
-                    for (let prestacion_ag of resultado.tipoPrestaciones){
-                        prestaciones_agenta_ = prestaciones_agenta_.concat(prestacion_ag.term);
+                    for (let prestacion_ag of resultado.tipoPrestaciones) {
+                        prestaciones_agenda_ = prestaciones_agenda_.concat(prestacion_ag.term);
                         prestaciones_agenta += prestacion_ag.term + ',';
                     }
                 }
                 if (profesionales.some(p => profesionalesIds.some(p2 => p2.toString() === p._id.toString()))) {
-                    response += ' Uno o más profesionales están asignados a otra agenda en ese horario. <br> <br> <br> Centro de Salud:<strong> ' + org +' </strong> <br> Prestacion: <strong>'+prestaciones_agenta_ +'</strong><br> Creada por: <strong>'+ agenda_creada_por+' </strong>';
+                    response += `Uno o más profesionales están asignados a otra agenda en ese horario. <br> <br> <br> Centro de Salud: <strong>${org}</strong> <br> Prestacion: <strong>${prestaciones_agenda_}</strong><br> Creada por: <strong>${agenda_creada_por}</strong>`;
                     // response += ' Uno o más profesionales están asignados a otra agenda en ese horario.';
                 }
             }
