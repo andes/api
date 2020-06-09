871090f609c53086ec3b6e1af2333ed0f13600dc
Gabo <cancellieri.gabriel@gmail.com>
2020-06-09 13:09:16 -0300

    feat(internacion): censo solo cuenta las camas censables

--- modules/rup/internacion/censo.controller.ts
+++ modules/rup/internacion/censo.controller.ts
@@ -89,77 +89,78 @@ async function realizarConteo(internaciones, unidadOrganizativa, timestampStart,
             }
         }
 
-        if (fechaEgreso) {
-            if ((primerUO === unidadOrganizativa) && (ultimaUO === unidadOrganizativa)) {
-                if (moment(fechaEgreso).isSame(fechaIngreso, 'day')) {
-                    ingresosYEgresos++;
-                    ingresoEgresoCargado = true;
-                    checkPaciente(ultimoMovimiento);
-                    tablaPacientes[ultimoMovimiento.paciente.id].actividad.push({
-                        ingreso: 'SI',
-                        egreso: informesInternacion.egreso.tipoEgreso.id,
-                    });
-                }
-            }
-            if (ultimaUO === String(unidadOrganizativa)) {
-                if (moment(fechaEgreso).isSameOrBefore(timestampEnd.toDate())) {
-                    diasEstada += informesInternacion.egreso.diasDeEstada;
-                    if (informesInternacion.egreso.tipoEgreso.id === 'Defunción') {
-                        defunciones++;
-                    } else {
-                        altas++;
-                    }
-                    if (!ingresoEgresoCargado) {
+        if (ultimoMovimiento.esCensable) {
+            if (fechaEgreso) {
+                if ((primerUO === unidadOrganizativa) && (ultimaUO === unidadOrganizativa)) {
+                    if (moment(fechaEgreso).isSame(fechaIngreso, 'day')) {
+                        ingresosYEgresos++;
+                        ingresoEgresoCargado = true;
                         checkPaciente(ultimoMovimiento);
                         tablaPacientes[ultimoMovimiento.paciente.id].actividad.push({
+                            ingreso: 'SI',
                             egreso: informesInternacion.egreso.tipoEgreso.id,
                         });
                     }
                 }
+                if (ultimaUO === String(unidadOrganizativa)) {
+                    if (moment(fechaEgreso).isSameOrBefore(timestampEnd.toDate())) {
+                        diasEstada += informesInternacion.egreso.diasDeEstada;
+                        if (informesInternacion.egreso.tipoEgreso.id === 'Defunción') {
+                            defunciones++;
+                        } else {
+                            altas++;
+                        }
+                        if (!ingresoEgresoCargado) {
+                            checkPaciente(ultimoMovimiento);
+                            tablaPacientes[ultimoMovimiento.paciente.id].actividad.push({
+                                egreso: informesInternacion.egreso.tipoEgreso.id,
+                            });
+                        }
+                    }
+                }
             }
-        }
 
-        if (primerUO === unidadOrganizativa) {
-            if (moment(fechaIngreso).isBefore(timestampStart.toDate())) {
-                existenciaALas0++;
-                checkPaciente(ultimoMovimientoUO);
-                tablaPacientes[ultimoMovimientoUO.paciente.id].actividad.push({
-                    ingreso: null,
-                    paseDe: null,
-                    egreso: null,
-                    paseA: null,
-                });
-            } else if (moment(fechaIngreso).isSameOrAfter(timestampStart.toDate())) {
-                ingresos++;
-                if (!ingresoEgresoCargado) {
+            if (primerUO === unidadOrganizativa) {
+                if (moment(fechaIngreso).isBefore(timestampStart.toDate())) {
+                    existenciaALas0++;
                     checkPaciente(ultimoMovimientoUO);
                     tablaPacientes[ultimoMovimientoUO.paciente.id].actividad.push({
-                        ingreso: 'SI',
+                        ingreso: null,
+                        paseDe: null,
+                        egreso: null,
+                        paseA: null,
                     });
-                }
-            }
-        }
-
-
-        let movimientoAnterior;
-        for (const movimiento of allMovimientos) {
-            if (movimientoAnterior && movimientoAnterior.unidadOrganizativa.conceptId !== movimiento.unidadOrganizativa.conceptId) {
-                if (movimientoAnterior.unidadOrganizativa.conceptId !== unidadOrganizativa) {
-                    pasesDe++;
-                    checkPaciente(movimiento);
-                    tablaPacientes[movimiento.paciente.id].actividad.push({
-                        paseDe: movimientoAnterior.unidadOrganizativa.term
-                    });
-                } else {
-                    pasesA++;
-                    checkPaciente(movimientoAnterior);
-                    tablaPacientes[movimiento.paciente.id].actividad.push({
-                        paseA: movimiento.unidadOrganizativa.term
-                    });
+                } else if (moment(fechaIngreso).isSameOrAfter(timestampStart.toDate())) {
+                    ingresos++;
+                    if (!ingresoEgresoCargado) {
+                        checkPaciente(ultimoMovimientoUO);
+                        tablaPacientes[ultimoMovimientoUO.paciente.id].actividad.push({
+                            ingreso: 'SI',
+                        });
+                    }
                 }
             }
 
-            movimientoAnterior = movimiento;
+            let movimientoAnterior;
+            for (const movimiento of allMovimientos) {
+                if (movimientoAnterior && movimientoAnterior.unidadOrganizativa.conceptId !== movimiento.unidadOrganizativa.conceptId) {
+                    if (movimientoAnterior.unidadOrganizativa.conceptId !== unidadOrganizativa) {
+                        pasesDe++;
+                        checkPaciente(movimiento);
+                        tablaPacientes[movimiento.paciente.id].actividad.push({
+                            paseDe: movimientoAnterior.unidadOrganizativa.term
+                        });
+                    } else {
+                        pasesA++;
+                        checkPaciente(movimientoAnterior);
+                        tablaPacientes[movimiento.paciente.id].actividad.push({
+                            paseA: movimiento.unidadOrganizativa.term
+                        });
+                    }
+                }
+
+                movimientoAnterior = movimiento;
+            }
         }
     });
 
@@ -217,7 +218,7 @@ export async function censoDiario({ organizacion, timestamp, unidadOrganizativa 
         const ultimoMov = camas[idCama][camas[idCama].length - 1];
         const esDisponible = (ultimoMov.estado !== 'bloqueada' && ultimoMov.estado !== 'inactiva');
         const estaUnidadOrganizativa = String(ultimoMov.unidadOrganizativa.conceptId) === unidadOrganizativa;
-        if (esDisponible && estaUnidadOrganizativa) {
+        if (esDisponible && estaUnidadOrganizativa && ultimoMov.esCensable) {
             camasDisponibles++;
         }
     });
