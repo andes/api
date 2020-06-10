489b86dd2fdb0c9598951448291054e48c9da430
Pancho <martinebucarey@gmail,com>
2020-06-10 12:34:50 -0300

    feat(permisos):visibilidad restricted

--- auth/permisos.ts
+++ auth/permisos.ts
@@ -462,6 +462,7 @@ export default [
     {
         key: 'monitoreo',
         title: 'Monitoreo',
+        visibility: 'restricted',
         child: [
             {
                 key: 'conceptosTurneables',
