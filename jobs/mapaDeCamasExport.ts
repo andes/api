2768a9687825328c8752dd4c1b57b5b3402ab18e
silviroa <silviroa@gmail.com>
2020-07-02 11:15:58 -0300

    feat(Internacion): Busca los datos de las organizaciones a exportar desde las queries de bi-queries

--- jobs/mapaDeCamasExport.ts
+++ jobs/mapaDeCamasExport.ts
@@ -1,63 +1,82 @@
 import * as configPrivate from './../config.private';
+import { log as andesLog } from '@andes/log';
 import { handleHttpRequest } from './../utils/requestHandler';
 
+let logRequest = {
+    user: {
+        usuario: { nombre: 'mapaDeCamasExport', apellido: 'mapaDeCamasExport' },
+        app: 'jobExportMapaDeCamas',
+        organizacion: 'Subsecretaría de salud'
+    },
+    ip: 'localhost',
+    connection: {
+        localAddress: ''
+    }
+};
+
+
 export async function exportInternacion(done) {
-    let organizaciones = [
-        {
-            idOrganizacion: '57e9670e52df311059bc8964',
-            capa: 'medica'
-        },
-        {
-            idOrganizacion: '5ee25d953e05fb20ef621e7a',
-            capa: 'medica'
-        },
-        {
-            idOrganizacion: '5bae6b7b9677f95a425d9ee8',
-            capa: 'estadistica'
+
+    let url_query = configPrivate.hosts.BI_QUERY + '/queries?nombre=camas_occupations';
+    let querys = await handleHttpRequest({
+        method: 'GET',
+        uri: url_query
+    });
+
+    if (querys && querys[0] === 200) {
+        let respuesta = JSON.parse(querys[1]);
+        let organizaciones = respuesta && respuesta[0].data;
+
+        const fechaActual = new Date();
+        let fechaDesde = new Date();
+        fechaDesde.setHours(fechaDesde.getHours() - 8);
+        let url_occupations = configPrivate.hosts.BI_QUERY + '/queries/camas_occupations/export';
+        let url_checkouts = configPrivate.hosts.BI_QUERY + '/queries/camas_checkouts/export';
+
+
+        for (let i = 0; i < organizaciones.length; i++) {
+            try {
+                let dataOccupations = {
+                    params: {
+                        organizacion: organizaciones[i].idOrganizacion,
+                        capa: organizaciones[i].capa,
+                        fecha: fechaActual
+                    }
+                };
+
+                let dataCheckouts = {
+                    params: {
+                        organizacion: organizaciones[i].idOrganizacion,
+                        capa: organizaciones[i].capa,
+                        fechaDesde,
+                        fechaHasta: fechaActual
+                    }
+                };
+
+                await handleHttpRequest({
+                    method: 'POST',
+                    uri: url_occupations,
+                    body: dataOccupations,
+                    json: true,
+                    timeout: 50000,
+                });
+
+                await handleHttpRequest({
+                    method: 'POST',
+                    uri: url_checkouts,
+                    body: dataCheckouts,
+                    json: true,
+                    timeout: 50000,
+                });
+
+            } catch (error) {
+                await andesLog(logRequest, 'andes:mapaDeCamas:bi', null, 'export', null, null, 'Error exportando mapa de camas');
+                return (done(error));
+            }
         }
-    ];
 
-
-    const fechaActual = new Date();
-    let fechaDesde = new Date();
-    fechaDesde.setHours(fechaDesde.getHours() - 8);
-    let url_occupations = configPrivate.hosts.BI_QUERY + '/queries/camas_occupations/export';
-    let url_checkouts = configPrivate.hosts.BI_QUERY + '/queries/camas_checkouts/export';
-
-    for (let i = 0; i < organizaciones.length; i++) {
-
-        let dataOccupations = {
-            params: {
-                organizacion: organizaciones[i].idOrganizacion,
-                capa: organizaciones[i].capa,
-                fecha: fechaActual
-            }
-        };
-
-        let dataCheckouts = {
-            params: {
-                organizacion: organizaciones[i].idOrganizacion,
-                capa: organizaciones[i].capa,
-                fechaDesde,
-                fechaHasta: fechaActual
-            }
-        };
-
-        await handleHttpRequest({
-            method: 'POST',
-            uri: url_occupations,
-            body: dataOccupations,
-            json: true,
-            timeout: 50000,
-        });
-
-        await handleHttpRequest({
-            method: 'POST',
-            uri: url_checkouts,
-            body: dataCheckouts,
-            json: true,
-            timeout: 50000,
-        });
+    } else {
+        await andesLog(logRequest, 'andes:mapaDeCamas:bi', null, 'export', null, null, 'Error exportando mapa de camas');
     }
 
     done();
