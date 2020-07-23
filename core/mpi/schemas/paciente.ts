1079e03d5e4c5e5c5453a754dce73dd7212c06fa
Lautaro Molina <negro89_nqn@hotmail.com>
2020-07-23 12:14:49 -0300

    fix(mpi): corrige esquema paciente para extranjeros

--- core/mpi/schemas/paciente.ts
+++ core/mpi/schemas/paciente.ts
@@ -69,7 +69,7 @@ export let pacienteSchema: mongoose.Schema = new mongoose.Schema({
     // ---------------------
     // Campos asociados a pacientes extranjeros
 
-    tipoIdentificacion: { type: constantes.IDENTIFICACION, required: false },  // pasaporte o documento extranjero
+    tipoIdentificacion: constantes.IDENTIFICACION,  // pasaporte o documento extranjero
     numeroIdentificacion: String,
     // --------------------
     relaciones: [{
