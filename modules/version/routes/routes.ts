f3fc2ba75650712610bda635e6226872be7a051a
juliotero <juli.otero01@gmail.com>
2020-07-24 13:04:44 -0300

    feat(citas): audita agenda al validar prestacion no nominalizada (#1029)
    * feat(rup): al validar prestación auditar agenda para no nominalizadas
    
    * feat(auth): getUserFromResource function
    
    * feat(version): add snomed branch
    
    Co-authored-by: Mariano Andres Botta <marianoabotta@gmail.com>

--- modules/version/routes/routes.ts
+++ modules/version/routes/routes.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
-
+import * as config from '../../../config.private';
 let router = express.Router();
 
 router.get('/', async (req, res) => {
     res.json({
-        version: require('../../../package.json').version
+        version: require('../../../package.json').version,
+        snomed: config.snomed.snowstormBranch
     });
 });
 
