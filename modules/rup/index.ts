523c2ed9b8609991c60330b313b10725da6e9c91
Mariano Andres Botta <marianoabotta@gmail.com>
2020-07-27 14:22:28 -0300

    feat(rup): ruteo

--- modules/rup/index.ts
+++ modules/rup/index.ts
@@ -1,6 +1,12 @@
-import { ElementoRUPRouter } from './elementos-rup.controller';
 import * as express from 'express';
+import { ElementoRUPRouter } from './elementos-rup.controller';
+import { CamasRouter, CensosRouter, EstadosRouter, InternacionRouter } from './internacion';
+
 
 export function setup(app: express.Application) {
     app.use('/api/modules/rup', ElementoRUPRouter);
+    app.use('/api/modules/rup/internacion', CamasRouter);
+    app.use('/api/modules/rup/internacion', CensosRouter);
+    app.use('/api/modules/rup/internacion', EstadosRouter);
+    app.use('/api/modules/rup/internacion', InternacionRouter);
 }
