4adbcbb65d56c6de5a892cb5c0793fc81528b4de
Lautaro Molina <negro89_nqn@hotmail.com>
2020-09-04 15:59:58 -0300

    ref(mpi): modificaciones en importacion de nacidos vivos

--- core/mpi/mpi.log.ts
+++ core/mpi/mpi.log.ts
@@ -1,4 +1,4 @@
 import { Connections } from '../../connections';
 import { Logger } from '@andes/log';
 
-export const mpiLog = new Logger({ connection: Connections.logs, module: 'mpi', type: 'mpi-corrector', application: 'andes' });
+export const mpiNacimientosLog = new Logger({ connection: Connections.logs, module: 'mpi', type: 'mpi-nacimientos', application: 'andes' });
