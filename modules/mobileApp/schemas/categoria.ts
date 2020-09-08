b19e48aff6e939ad6c24b511e2430915223f8f75
juliotero <juli.otero01@gmail.com>
2020-09-08 14:23:47 -0300

    feat(mobileApp): nuevo schema y rutas de categoría (#1063)

---
+++ modules/mobileApp/schemas/categoria.ts
@@ -0,0 +1,22 @@
+import * as mongoose from 'mongoose';
+
+export let CategoriaSchema = new mongoose.Schema({
+    titulo: {
+        type: String
+    },
+    expresionSnomed: {
+        type: String
+    },
+    descargaAdjuntos: {
+        type: Boolean
+    },
+    busquedaPor: {
+        type: Array,
+        items: {
+            type: String,
+            enum: ['registros', 'prestaciones', 'cdas']
+        }
+    }
+});
+
+export let Categoria = mongoose.model('categorias', CategoriaSchema, 'categorias');
