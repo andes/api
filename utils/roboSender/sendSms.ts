c5511a14fd956877df93327a065b2ace293c1397
sychus <hfernandez@neuquen.gov.ar>
2020-10-05 14:20:47 -0300

    feat(robosender): Add aws sms service

--- utils/roboSender/sendSms.ts
+++ utils/roboSender/sendSms.ts
@@ -10,16 +10,16 @@ export interface SmsOptions {
 }
 
 export async function sendSms(smsOptions: SmsOptions) {
-    try{
+    try {
         log('Enviando SMS a ', smsOptions.telefono);
         const argsOperador = {
-        telefono: smsOptions.telefono
-        }
+            telefono: smsOptions.telefono
+        };
         AWS.config.region = 'us-east-1';
         AWS.config.update({
-                accessKeyId: AWS_CONFIG.ST_AWS_ACCESS_KEY,
-                secretAccessKey: AWS_CONFIG.ST_AWS_SECRET_ACCESS_KEY
-            });
+            accessKeyId: AWS_CONFIG.ST_AWS_ACCESS_KEY,
+            secretAccessKey: AWS_CONFIG.ST_AWS_SECRET_ACCESS_KEY
+        });
         const sms: SNS = new AWS.SNS();
         const params: any = {
             Message: smsOptions.mensaje,
@@ -29,6 +29,6 @@ export async function sendSms(smsOptions: SmsOptions) {
         };
         return await sms.publish(params).promise();
     } catch (error) {
-        return error
+        return error;
     }
 }
