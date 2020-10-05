3f5f47b8b2efa7c34d3d4be748d92b56f8157930
sychus <hfernandez@neuquen.gov.ar>
2020-10-05 14:20:47 -0300

    feat(robosender): Add aws sms service

--- utils/roboSender/sendSms.ts
+++ utils/roboSender/sendSms.ts
@@ -4,29 +4,37 @@ import { SNS, config } from 'aws-sdk';
 import {AWS_CONFIG} from '../././../config.private';
 
 export interface SmsOptions {
+    prefijo: string;
     telefono: number;
     mensaje: string;
 }
 export async function sendSms(smsOptions: SmsOptions) {
     try {
         log('Enviando SMS a ', smsOptions.telefono);
+        const prefix = smsOptions.prefijo ? smsOptions.prefijo : '+54';
         const argsOperador = {
-            telefono: smsOptions.telefono
+            telefono: prefix + smsOptions.telefono
         };
-        config.region = 'us-east-1';
+        config.region = AWS_CONFIG.ST_AWS_REGION;
         config.update({
             accessKeyId: AWS_CONFIG.ST_AWS_ACCESS_KEY,
             secretAccessKey: AWS_CONFIG.ST_AWS_SECRET_ACCESS_KEY
         });
         const sms: SNS = new SNS();
+        config.region = AWS_CONFIG.ST_AWS_REGION;
+        config.update({
+            accessKeyId: AWS_CONFIG.ST_AWS_ACCESS_KEY,
+            secretAccessKey: AWS_CONFIG.ST_AWS_SECRET_ACCESS_KEY
+        });
         const params: any = {
             Message: smsOptions.mensaje,
             MessageStructure: 'string',
-            PhoneNumber: smsOptions.telefono,
+            PhoneNumber: argsOperador.telefono,
             Subject: 'ANDES'
         };
         return await sms.publish(params).promise();
     } catch (error) {
+        log('Error envio de sms ', error);
         return error;
     }
 }
