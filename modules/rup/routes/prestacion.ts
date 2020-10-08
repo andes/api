57441804ce2fee32f57c06b9197adaecdaebe030
Mariano Andres Botta <marianoabotta@gmail.com>
2020-10-08 11:53:46 -0300

    feat(rup): ajustes de filtros anulada

--- modules/rup/routes/prestacion.ts
+++ modules/rup/routes/prestacion.ts
@@ -358,7 +358,12 @@ router.get('/prestaciones', async (req: any, res, next) => {
     }
 
     if (req.query.sinEstado) {
-        query.where('estados.tipo').ne(req.query.sinEstado);
+        if (Array.isArray(req.query.sinEstado)) {
+            query.where('estados.tipo').nin(req.query.sinEstado);
+
+        } else {
+            query.where('estados.tipo').ne(req.query.sinEstado);
+        }
     }
     if (req.query.fechaDesde) {
         // query.where('createdAt').gte(moment(req.query.fechaDesde).startOf('day').toDate() as any);
