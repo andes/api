145265e26956b4fe49eae86e352c138ffc291251
plammel <lammelpablo@gmail.com>
2020-12-14 13:44:27 -0300

    fix(CIT): hotfix descarga arancelamiento

--- modules/facturacionAutomatica/controller/facturacionAutomaticaController.ts
+++ modules/facturacionAutomatica/controller/facturacionAutomaticaController.ts
@@ -3,11 +3,11 @@ import * as facturacionAutomaticaModel from './../schemas/configFacturacionAutom
 export async function getConfigFacturacionAutomatica(query) {
     let params = {};
     if ((query.idPrestacionEjecutada) && (query.idPrestacionEjecutada !== 'undefined') && (query.idPrestacionEjecutada !== 'null')) {
-        query['prestacionSnomed.conceptId'] = query.idPrestacionEjecutada;
+        params['prestacionSnomed.conceptId'] = query.idPrestacionEjecutada;
     }
 
     if ((query.idPrestacionTurneable) && (query.idPrestacionTurneable !== 'undefined') && (query.idPrestacionTurneable !== 'null')) {
-        query['conceptosTurneables.conceptId'] = query.idPrestacionTurneable;
+        params['conceptosTurneables.conceptId'] = query.idPrestacionTurneable;
     }
 
     return await facturacionAutomaticaModel.find(params);
