e943206b7193d3bfa524f1e75ed2379855468656
condorpiedra <natalia.huenchuman@gmail.com>
2020-12-17 09:11:53 -0300

    fix(arancelamiento): reestablece ruta (#1229)

--- modules/facturacionAutomatica/routes/configFacturacionAutomatica.ts
+++ modules/facturacionAutomatica/routes/configFacturacionAutomatica.ts
@@ -1,7 +1,20 @@
 import * as express from 'express';
 import { EventCore } from '@andes/event-bus';
+import { getConfigFacturacionAutomatica } from '../controller/facturacionAutomaticaController';
 
 let router = express.Router();
+
+/**
+ * Obtiene los datos de la obra social asociada a un paciente
+ *
+ * @param {any} conceptId
+ * @returns
+ */
+router.get('/configFacturacionAutomatica/', async (req, res, next) => {
+
+    const data = await getConfigFacturacionAutomatica(req.query);
+    res.json(data);
+});
 
 router.post('/facturaArancelamiento', async (req, res, next) => {
     let turno = req.body;
