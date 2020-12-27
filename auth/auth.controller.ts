d7a2307ba868df6cbbc94ebe79369da60ed12959
sychus <hfernandez@neuquen.gov.ar>
2020-12-27 13:03:20 -0300

    fix(auth): Se filtra por activo, sino usuarios inactivos igual pueden loguearse

--- auth/auth.controller.ts
+++ auth/auth.controller.ts
@@ -98,8 +98,8 @@ export async function getTokenPayload(token, userData) {
  */
 
 export async function findUser(username) {
-    const pAuth = AuthUsers.findOne({ usuario: username });
-    const pProfesional = Profesional.findOne({ documento: username }, { matriculas: true, especialidad: true });
+    const pAuth = AuthUsers.findOne({ usuario: username, activo: true });
+    const pProfesional = Profesional.findOne({ documento: username, activo: true }, { matriculas: true, especialidad: true });
     const [auth, prof] = await Promise.all([pAuth, pProfesional]);
     if (auth) {
         return {
