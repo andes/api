48bbcb4aa09e2aa1b7c6855477c9fead529965fc
sychus <hfernandez@neuquen.gov.ar>
2020-12-27 01:12:11 -0300

    feat(vacuna): Agregamos la direccion y se agreaga la variable de hora para darle inicio a la búsqueda

--- jobs/jobExportVacunaCovid19.ts
+++ jobs/jobExportVacunaCovid19.ts
@@ -1,7 +1,9 @@
 import { exportCovid19 } from './exportCovid19';
 
 function run(done) {
-    exportCovid19(done);
+    // Cantidad de horas inicio Ej: Si quiero todas las prestaciones desde hace 6 horas hasta este momento, envio el número 6.
+    const horas = process.env.HORAS;
+    exportCovid19(done, parseInt(horas, 10));
 }
 
 export = run;
