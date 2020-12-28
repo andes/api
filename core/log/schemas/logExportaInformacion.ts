2636eceecfbc3b81553a1972a918d5e2f59bedc9
Natalia Huenchuman <natalia.huenchuman@gmail.com>
2020-12-28 13:09:08 -0300

    feat(job): registro de aplicaciones de vacunas

---
+++ core/log/schemas/logExportaInformacion.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+
+export const informacionExportadaSchema = new mongoose.Schema({
+    fecha: {
+        type: Date
+    },
+    sistema: String,
+    key: String,
+    idPaciente: mongoose.Schema.Types.ObjectId,
+    info_enviada: { type: mongoose.Schema.Types.Mixed },
+    resultado: { type: mongoose.Schema.Types.Mixed }
+});
+
+informacionExportadaSchema.index({ fecha: 1, sistema: 1, key: 1 });
+informacionExportadaSchema.index({ fecha: 1, idPaciente: 1 });
+
+export const InformacionExportada = mongoose.model('nomivacInformacionExportada', informacionExportadaSchema,
+    'nomivacInformacionExportada');
