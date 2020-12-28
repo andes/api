9410ffc1dbd1e8332378a382b5a0a9c98ac1b067
condorpiedra <natalia.huenchuman@gmail.com>
2020-12-28 18:34:20 -0300

    feat(nomivac): registro de aplicaciones de vacunas (#1236)
    * feat(job): registro de aplicaciones de vacunas
    
    * fix(job): refactor query nomivac

---
+++ jobs/exportCovid19.ts
@@ -0,0 +1,187 @@
+import * as moment from 'moment';
+import { Prestacion } from '../modules/rup/schemas/prestacion';
+import { InformacionExportada } from '../core/log/schemas/logExportaInformacion';
+import { handleHttpRequest } from '../utils/requestHandler';
+import { sisa } from './../config.private';
+const fs = require('fs');
+
+const user = sisa.username;
+const clave = sisa.password;
+const urlSisa = sisa.url_nomivac;
+
+export async function exportCovid19(done, horas) {
+    const start = moment().subtract(horas, 'h').toDate();
+    const end = moment().toDate();
+    const pipelineVacunaCovid19 = [
+        {
+            $match: {
+                'solicitud.fecha': {
+                    $gte: start, $lte: end
+                },
+                'estadoActual.tipo': 'validada',
+                'ejecucion.registros.concepto.conceptId': '840534001'
+
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                idPaciente: '$paciente.id',
+                dni: '$paciente.documento',
+                sexo: '$paciente.sexo',
+                nombre: '$paciente.nombre',
+                apellido: '$paciente.apellido',
+                fechaNacimiento: '$paciente.fechaNacimiento',
+                fecha: '$solicitud.fecha',
+                idEfector: '$solicitud.organizacion.id',
+                vacuna: '$ejecucion.registros.valor.vacuna'
+            }
+        },
+        {
+            $lookup: {
+                from: 'organizacion',
+                localField: 'idEfector',
+                foreignField: '_id',
+                as: 'organizacion'
+            }
+        },
+        { $unwind: '$organizacion' },
+        {
+            $lookup: {
+                from: 'paciente',
+                localField: 'idPaciente',
+                foreignField: '_id',
+                as: 'dirPaciente'
+            }
+        },
+        { $unwind: '$dirPaciente' },
+        {
+            $addFields: {
+                direccion: {
+                    $arrayElemAt: [
+                        '$dirPaciente.direccion',
+                        0
+                    ]
+                }
+            }
+        },
+        {
+            $project: {
+                tipo: 'vacuna',
+                idPaciente: '$idPaciente',
+                dni: '$dni',
+                sexo: '$sexo',
+                nombre: '$nombre',
+                apellido: '$apellido',
+                direccion: '$direccion.valor',
+                fechaNacimiento: {
+                    $dateToString: {
+                        date: '$fechaNacimiento',
+                        format: '%d-%m-%Y',
+                        timezone: 'America/Argentina/Buenos_Aires'
+                    }
+                },
+                fecha: {
+                    $dateToString: {
+                        date: '$fecha',
+                        format: '%d-%m-%Y',
+                        timezone: 'America/Argentina/Buenos_Aires'
+                    }
+                },
+                idEfector: '$idEfector',
+                CodigoSisa: '$organizacion.codigo.sisa',
+                vacunas: '$vacuna',
+                fechaCarga: {
+                    $dateToString: {
+                        date: '$fecha',
+                        format: '%d-%m-%Y',
+                        timezone: 'America/Argentina/Buenos_Aires'
+                    }
+                },
+            }
+        }
+    ];
+    const prestaciones = await Prestacion.aggregate(pipelineVacunaCovid19);
+    for (let unaPrestacion of prestaciones) {
+        const data = {
+            ciudadano:
+            {
+                tipoDocumento: 1,
+                numeroDocumento: unaPrestacion.dni,
+                sexo: unaPrestacion.sexo === 'femenino' ? 'F' : (unaPrestacion.sexo === 'masculino') ? 'M' : '',
+                nombre: unaPrestacion.nombre,
+                apellido: unaPrestacion.apellido,
+                fechaNacimiento: unaPrestacion.fechaNacimiento,
+                calle: unaPrestacion.direccion ? unaPrestacion.direccion : '',
+                pais: 200,
+                provincia: 15,
+                departamento: 365  // Confluencia, luego updetear por el que corresponda
+            },
+            aplicacionVacuna:
+            {
+                establecimiento: unaPrestacion.CodigoSisa,
+                fechaAplicacion: unaPrestacion.vacunas[0].fechaAplicacion ? moment(unaPrestacion.vacunas[0].fechaAplicacion).format('DD-MM-YYYY') : null,
+                lote: unaPrestacion.vacunas[0].lote,
+                esquema: unaPrestacion.vacunas[0].esquema.codigo,
+                condicionAplicacion: unaPrestacion.vacunas[0].condicion.codigo,
+                vacuna: unaPrestacion.vacunas[0].vacuna.codigo,
+                ordenDosis: unaPrestacion.vacunas[0].dosis.codigo,
+                referenciaSistemaProvincial: '32342'   // faltaría ver bien que es esto, aunque no es obligatorio
+            }
+        };
+        const dto = {
+            username: user,
+            password: clave,
+            ciudadano: data.ciudadano,
+            aplicacionVacuna: data.aplicacionVacuna
+        };
+
+        let log = {
+            fecha: new Date(),
+            sistema: 'Nomivac',
+            key: unaPrestacion.tipo,
+            idPaciente: unaPrestacion.idPaciente,
+            info_enviada: data,
+            resultado: {}
+        };
+        try {
+            const options = {
+                uri: urlSisa,
+                method: 'POST',
+                body: dto,
+                headers: { 'Content-Type': 'application/json' },
+                json: true,
+            };
+            const resJson = await handleHttpRequest(options);
+            if (resJson && resJson.length > 0) {
+                const code = resJson[0];
+                const response = resJson[1];
+                log.resultado = {
+                    status: code ? code : '',
+                    resultado: response.resultado ? response.resultado : '',
+                    description: response.description ? response.description : '',
+                    error: response.errors ? response.errors : ''
+                };
+
+            } else {
+                log.resultado = {
+                    resultado: 'ERROR_DE_ENVIO',
+                    status: '',
+                    description: 'No se recibió ningún resultado'
+                };
+            }
+            let info = new InformacionExportada(log);
+            await info.save();
+
+        } catch (error) {
+            log.resultado = {
+                resultado: 'ERROR_DE_ENVIO',
+                status: 500,
+                description: error.toString()
+            };
+            let info = new InformacionExportada(log);
+            await info.save();
+        }
+    }
+    done();
+}
---
+++ jobs/jobExportVacunaCovid19.ts
@@ -0,0 +1,10 @@
+import { exportCovid19 } from './exportCovid19';
+
+function run(done) {
+    // Cantidad de horas inicio Ej: Si quiero todas las prestaciones desde hace 6 horas hasta este momento, envio el número 6.
+    const horas = process.env.HORAS || '1';
+    exportCovid19(done, parseInt(horas, 10));
+}
+
+export = run;
+
