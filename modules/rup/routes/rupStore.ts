d54d9f92065e6bfb3800f52c6002ba3b5529252c
juliotero <juli.otero01@gmail.com>
2020-12-28 12:53:38 -0300

    feat(top): subir archivos a andesDrive (#1228)

--- modules/rup/routes/rupStore.ts
+++ modules/rup/routes/rupStore.ts
@@ -1,15 +1,28 @@
 import { readFile, storeFile } from '../../../core/tm/controller/file-storage';
 import * as express from 'express';
 import { asyncHandler } from '@andes/api-tool';
+import { AndesDrive } from '@andes/drive';
 const router = express.Router();
 
 const CollectionName = 'RupStore';
 
-router.get('/store/:id', asyncHandler(async (req, res) => {
-    const fileData = await readFile(req.params.id, CollectionName);
-    res.contentType(fileData.file.contentType);
-    fileData.stream.pipe(res);
-}));
+router.get('/store/:id', async (req, res, next) => {
+    const fileDrive = await AndesDrive.find(req.params.id);
+    if (fileDrive) {
+        const stream1 = await AndesDrive.read(fileDrive);
+        res.contentType(fileDrive.mimetype);
+        stream1.pipe(res);
+    } else {
+        const data = await readFile(req.params.id, CollectionName);
+        res.contentType(data.file.contentType);
+        data.stream.on('data', (data2) => {
+            res.write(data2);
+        });
+        data.stream.on('end', () => {
+            res.end();
+        });
+    }
+});
 
 router.post('/store', asyncHandler(async (req, res) => {
     const file = req.body.file;
