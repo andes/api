10041c784d151a1e2bbecee1e2a4a99d73da24c4
Natalia Huenchuman <natalia.huenchuman@gmail.com>
2021-01-04 15:59:01 -0300

    fix(mpi): control georefencia un job

--- core/mpi/controller/updateValidados.ts
+++ core/mpi/controller/updateValidados.ts
@@ -13,7 +13,7 @@ export async function updateValidados(done) {
         const pacientes: any = await PacienteCtr.search({ estado: 'validado', activo: true, fechaUpdate: `<${fechaDesde}` }, options as any, userScheduler as any);
         let pacientesValidados = [];
 
-        for (let pac of pacientes) {
+        for (const pac of pacientes) {
             pacientesValidados.push(await validar(pac.documento, pac.sexo));
         }
         let pacientes_updates = [];
@@ -25,20 +25,18 @@ export async function updateValidados(done) {
                 if (persona_validada.fechaFallecimiento) {
                     data.fechaFallecimiento = persona_validada.fechaFallecimiento;
                 }
-                if (paciente.direccion?.[0]) {  // direccion legal
-                    if (persona_validada.direccion?.length) {
+                if (paciente.direccion?.[0]) {
+                    if (persona_validada.direccion?.length > 0) {
                         data.direccion[1] = persona_validada.direccion[1];
                     }
                 } else {
                     // si el paciente no tiene direccion le asignamos ambas con el valor de su direccion legal
                     data.direccion = persona_validada.direccion;
                 }
-                const dir = data.direccion[0].valor + ', ' + data.direccion[0].ubicacion.localidad.nombre + ', ' + data.direccion[0].ubicacion.provincia.nombre;
-                const geoRef: any = await geoReferenciar(dir, geoKey);
-                if (geoRef && Object.keys(geoRef).length) {
-                    data.direccion[0].geoReferencia = [geoRef.lat, geoRef.lng];
-                } else {
-                    data.direccion[0].geoReferencia = null;
+                if (!data.direccion[0].georeferencia || (data.direccion.length > 0 && data.direccion[0].georeferencia === null)) {
+                    const dir = data.direccion[0].valor + ', ' + data.direccion[0].ubicacion.localidad.nombre + ', ' + data.direccion[0].ubicacion.provincia.nombre;
+                    const geoRef: any = await geoReferenciar(dir, geoKey);
+                    data.direccion[0].geoReferencia = geoRef && Object.keys(geoRef).length > 0 ? [geoRef.lat, geoRef.lng] : null;
                 }
                 pacientes_updates.push(PacienteCtr.update(paciente.id, data, userScheduler as any));
             }
