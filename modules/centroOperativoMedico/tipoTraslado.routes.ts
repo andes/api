2e58d90d2f706d5183b92cda6377fdf9a20460e4
sychus <hfernandez@neuquen.gov.ar>
2021-02-09 11:05:40 -0300

    feat(com): agregamos el campo de búsqueda el tipo de traslado

--- modules/centroOperativoMedico/tipoTraslado.routes.ts
+++ modules/centroOperativoMedico/tipoTraslado.routes.ts
@@ -1,11 +1,14 @@
-import { TipoTraslado } from './schemas/tipoTraslado.schema';
-import { ResourceBase } from '@andes/core';
+import { MongoQuery, ResourceBase } from '@andes/core';
 import { Auth } from '../../auth/auth.class';
+import { TipoTraslado } from './schemas/tipoTraslado.schema';
 
 class TipoTrasladoResource extends ResourceBase {
     Model = TipoTraslado;
     resourceName = 'tipoTraslado';
     middlewares = [Auth.authenticate()];
+    searchFileds = {
+        nombre: MongoQuery.partialString,
+    };
 }
 
 export const TipoTrasladoCtr = new TipoTrasladoResource({});
