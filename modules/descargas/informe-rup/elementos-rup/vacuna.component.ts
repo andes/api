7f17980c083d708ca9af2f3da46af1e58c22d3af
waltergarrido <64211129+waltergarrido@users.noreply.github.com>
2021-02-18 10:00:53 -0300

    feat(rup): informe-pdf se agrega implementación de VacunasComponent (#1277)
    * Refactor(RUP): informe-pdf se agrega implementación de VacunasComponent
    
    * fix(RUP): correcciones informe-pdf

---
+++ modules/descargas/informe-rup/elementos-rup/vacuna.component.ts
@@ -0,0 +1,177 @@
+import { HTMLComponent } from '../../model/html-component.class';
+import * as moment from 'moment';
+export class VacunasComponent extends HTMLComponent {
+    template = `
+    <div class="nivel-1">
+    <p>
+        {{ registro.concepto.term }}
+        {{#if registro.esDiagnosticoPrincipal}}<small>(PROCEDIMIENTO / DIAGNÓSTICO PRINCIPAL)</small>{{/if}}:
+    </p>
+
+</div>
+
+<section class="contenedor-data-origen">
+    <div class="contenedor-secundario" style = "float: left;width:50%">
+        <div class="contenedor-bloque-texto">
+        {{#if vacunador}}
+            <h6 class="bolder">
+                Vacunador:
+            </h6>
+            <h6>
+                {{ vacunadorApellido }},{{ vacunadorNombre }}
+            </h6>
+        {{else}}
+          <h6 class="bolder">
+               Vacunador:
+         </h6>
+            <h6 class="bolder">
+            -
+            </h6>
+        {{/if}}
+        </div>
+        <br>
+        <div class="contenedor-bloque-texto">
+            <h6 class="bolder">
+                Categoria:
+            </h6>
+            <h6>
+                {{ categoria }}
+            </h6>
+
+        </div>
+        <br>
+        <div class="contenedor-bloque-texto">
+            <h6 class="bolder">
+                Vacuna:
+            </h6>
+            <h6>
+                {{ vacuna }}
+            </h6>
+
+        </div>
+        <br>
+        <div class="contenedor-bloque-texto">
+            <h6 class="bolder">
+                Dosis:
+            </h6>
+            <h6>
+                {{ dosis }}
+            </h6>
+
+        </div>
+        <br>
+        <div class="contenedor-bloque-texto">
+        {{#if laboratorio}}
+            <h6 class="bolder">
+                Laboratorio:
+            </h6>
+            <h6>
+                {{ laboratorio }}
+            </h6>
+            {{else}}
+            <h6 class="bolder">
+            Laboratorio:
+           </h6>
+              <h6 class="bolder">
+              -
+              </h6>
+          {{/if}}
+        </div>
+    </div>
+    <div class="contenedor-secundario" style = "float: left;width:50%">
+        <div class="contenedor-bloque-texto">
+            <h6 class="bolder">
+                Fecha de Aplicacion:
+            </h6>
+            <h6>
+                {{ fechaAplicacion }}
+            </h6>
+
+        </div>
+        <br>
+        <div class="contenedor-bloque-texto">
+            <h6 class="bolder">
+                Condicion:
+            </h6>
+            <h6>
+                {{ condicion }}
+            </h6>
+
+        </div>
+        <br>
+        <div class="contenedor-bloque-texto">
+            <h6 class="bolder">
+                Esquema:
+            </h6>
+            <h6>
+                {{ esquema }}
+            </h6>
+
+        </div>
+        <br>
+        <div class="contenedor-bloque-texto">
+            <h6 class="bolder">
+                Lote:
+            </h6>
+            <h6>
+                {{ lote }}
+            </h6>
+
+        </div>
+        <br>
+        <div class="contenedor-bloque-texto">
+            <h6 class="bolder">
+                Fecha Vencimiento:
+            </h6>
+            <h6>
+                {{ fechaVencimiento }}
+            </h6>
+
+        </div>
+
+    </div>
+
+
+</section>
+
+<div class="contenedor-bloque-texto">
+{{#if comentario}}
+    <h6 class="bolder">
+        Comentario:
+    </h6>
+    <h6>
+        {{ comentario }}
+    </h6>
+    {{/if}}
+</div>
+
+
+
+
+        `;
+    constructor(private prestacion, private registro, private params, private depth) {
+        super();
+    }
+
+    async process() {
+        const vacunador = this.registro.valor.vacuna.vacunador?.nombre || this.registro.valor.vacuna.vacunador?.apellido;
+        this.data = {
+            registro: this.registro,
+            fechaAplicacion: moment(this.registro.valor.vacuna.fechaAplicacion).format('DD/MM/YYYY'),
+            vacunadorNombre: this.registro.valor.vacuna.vacunador?.nombre,
+            vacunadorApellido: this.registro.valor.vacuna.vacunador?.apellido,
+            categoria: this.registro.valor.vacuna.categoria.nombre,
+            condicion: this.registro.valor.vacuna.condicion.nombre,
+            vacuna: this.registro.valor.vacuna.vacuna.nombre,
+            esquema: this.registro.valor.vacuna.esquema.nombre,
+            dosis: this.registro.valor.vacuna.dosis.nombre,
+            lote: this.registro.valor.vacuna.lote,
+            laboratorio: this.registro.valor.vacuna.laboratorio?.nombre,
+            fechaVencimiento: moment(this.registro.valor.vacuna.fechaVencimiento).format('DD/MM/YYYY'),
+            comentario: this.registro.valor.vacuna?.comentarios,
+            vacunador
+        };
+
+    }
+
+}
