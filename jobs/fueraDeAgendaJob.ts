2abbf710c7cc2658ff181d46144d6eff54b85d31
Carolina <celeste.carolina.s@gmail.com>
2021-03-17 13:03:42 -0300

    feat(pecas): Agrega parametros a job de fuera de agenda+indices en codificacion

--- jobs/fueraDeAgendaJob.ts
+++ jobs/fueraDeAgendaJob.ts
@@ -2,11 +2,14 @@ import * as fueraAgendaCtrl from './../modules/estadistica/pecas/controller/fuer
 import * as moment from 'moment';
 
 function run(done) {
-    // let start = moment(new Date(2020, 2, 24).setHours(0, 0, 0, 0)).format('YYYY-MM-DD HH:mm:ss');
-    // let end = moment(new Date(2020, 2, 26).setHours(23, 59, 0, 0)).format('YYYY-MM-DD HH:mm:ss');
-    let start = moment(new Date().setHours(0, 0, 0, 0)).subtract(1, 'day').format('YYYY-MM-DD HH:mm:ss');
-    let end = moment(new Date().setHours(23, 59, 0, 0)).subtract(1, 'day').format('YYYY-MM-DD HH:mm:ss');
-
+    const paramInicio = process.argv[3];
+    const paramFin = process.argv[4];
+    const start = paramInicio ?
+        moment(new Date(paramInicio).setHours(0, 0, 0, 0)).format('YYYY-MM-DD HH:mm:ss') :
+        moment(new Date().setHours(0, 0, 0, 0)).subtract(1, 'day').format('YYYY-MM-DD HH:mm:ss');
+    const end = paramFin ?
+        moment(new Date(paramFin).setHours(23, 59, 0, 0)).format('YYYY-MM-DD HH:mm:ss') :
+        moment(new Date().setHours(23, 59, 0, 0)).subtract(1, 'day').format('YYYY-MM-DD HH:mm:ss');
     fueraAgendaCtrl.fueraAgendaPecas(start, end, done);
 }
 
