02b415f67d70caf19055f623644f06b89258f060
juliotero <juli.otero01@gmail.com>
2021-03-22 09:06:20 -0300

    VAC: script para actualizar estado de vacunación en inscripciones (#1295)
    * feat(vac): script para actualizar estado de vacunación en inscripciones
    
    * Update scripts/VAC-9.ts
    
    Co-authored-by: Sychus <hfernandez@neuquen.gov.ar>
    
    * Agrega controles en find de inscripcion
    
    Co-authored-by: Sychus <hfernandez@neuquen.gov.ar>
    Co-authored-by: Carolina <celeste.carolina.s@gmail.com>

---
+++ scripts/VAC-9.ts
@@ -0,0 +1,28 @@
+import { InscripcionVacuna } from './../modules/vacunas/schemas/inscripcion-vacunas.schema';
+import { Prestacion } from './../modules/rup/schemas/prestacion';
+
+// Recorre todas las prestaciones de vacuna y agrega la fecha e id en la colección de inscripción.
+async function ActualizarInscripciones() {
+    const inscripciones: any[] = await InscripcionVacuna.find({fechaVacunacion: {$exists: false}, 'paciente.id': {$exists: true}});
+    for (const inscripcion of inscripciones) {
+        if (!inscripcion.fechaVacunacion || !inscripcion.idPrestacionVacuna) {
+            const prestaciones: any[] = await Prestacion.find({ 'ejecucion.registros.concepto.conceptId': '840534001', 'paciente.id': inscripcion.paciente.id });
+            if (prestaciones.length) {
+                try {
+                    inscripcion.fechaVacunacion = prestaciones[0].ejecucion.fecha;
+                    inscripcion.idPrestacionVacuna = prestaciones[0].id;
+                    await inscripcion.save();
+                } catch (e) {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+async function run(done) {
+    await ActualizarInscripciones();
+    done();
+}
+
+export = run;
