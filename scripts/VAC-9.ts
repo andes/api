35de06801fd1fc341caf30cb7b86a942f734c878
Carolina <celeste.carolina.s@gmail.com>
2021-03-22 08:53:04 -0300

    Agrega controles en find de inscripcion

--- scripts/VAC-9.ts
+++ scripts/VAC-9.ts
@@ -3,7 +3,7 @@ import { Prestacion } from './../modules/rup/schemas/prestacion';
 
 // Recorre todas las prestaciones de vacuna y agrega la fecha e id en la colección de inscripción.
 async function ActualizarInscripciones() {
-    const inscripciones: any[] = await InscripcionVacuna.find({});
+    const inscripciones: any[] = await InscripcionVacuna.find({fechaVacunacion: {$exists: false}, 'paciente.id': {$exists: true}});
     for (const inscripcion of inscripciones) {
         if (!inscripcion.fechaVacunacion || !inscripcion.idPrestacionVacuna) {
             const prestaciones: any[] = await Prestacion.find({ 'ejecucion.registros.concepto.conceptId': '840534001', 'paciente.id': inscripcion.paciente.id });
