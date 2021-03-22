e15e59acd51747d0b435e1865e6a81db44a68595
plammel <lammelpablo@gmail.com>
2021-03-22 13:32:55 -0300

    feat: Envio comprobante derivacion x mail (#1285)

---
+++ modules/centroOperativoMedico/controllers/com.controller.ts
@@ -0,0 +1,36 @@
+import * as configPrivate from '../../../config.private';
+import * as SendEmail from '../../../utils/roboSender/sendEmail';
+import { Derivacion } from '../../../modules/descargas/com/derivacion';
+const moment = require('moment');
+
+export async function sendMailComprobanteDerivacion(derivacion, email) {
+    let comprobante = new Derivacion({body: derivacion});
+    const opciones = { header: { height: '3cm' } };
+    const fileName: any = await comprobante.informe(opciones);
+    const fechaFinalizacion = moment(derivacion.historial.createdAt).format('DD/MM/YYYY');
+
+    let attachments = [{
+        filename: `comprobante derivacion ${derivacion.paciente.nombre}${derivacion.paciente.apellido}-${fechaFinalizacion}.pdf`,
+        path: fileName
+    }];
+
+    const handleBarsData = {
+        asunto: 'Aviso de derivación finalizada',
+        mensaje: `El paciente ${derivacion.paciente.nombre} ${derivacion.paciente.apellido}
+            DNI ${derivacion.paciente.documento}
+            ha arribado a destino a ${derivacion.organizacionDestino.nombre}
+            el día ${fechaFinalizacion}.
+            Descargue el comprobante adjunto.`
+    };
+    const html = await SendEmail.renderHTML('emails/emailGenerico.html', handleBarsData);
+    const data = {
+        from: `ANDES <${configPrivate.enviarMail.auth.user}>`,
+        to: email,
+        subject: `Comprobante de derivación ${derivacion.paciente.nombre} ${derivacion.paciente.apellido} ${fechaFinalizacion}`,
+        text: '',
+        html,
+        attachments
+    };
+
+    SendEmail.sendMail(data);
+}
