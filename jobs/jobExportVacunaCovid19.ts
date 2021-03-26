8135bae9082e94260e5aca67f392b4222b0eac42
Pancho <martinebucarey@gmail,com>
2021-03-26 13:55:38 -0300

    feat(exportCovid): params fecha desde y hasta

--- jobs/jobExportVacunaCovid19.ts
+++ jobs/jobExportVacunaCovid19.ts
@@ -1,9 +1,12 @@
 import { exportCovid19 } from './exportCovid19';
 
+
 function run(done) {
     // Cantidad de horas inicio Ej: Si quiero todas las prestaciones desde hace 6 horas hasta este momento, envio el número 6.
     const horas = process.env.HORAS || '1';
-    exportCovid19(done, parseInt(horas, 10));
+    const desde = process.env.FECHADESDE;
+    const hasta = process.env.FECHAHASTA;
+    exportCovid19(done, parseInt(horas, 10), desde, hasta);
 }
 
 export = run;
