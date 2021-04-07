91373274cf09d8071b6584e3ba1e1ad333e50f87
sychus <hfernandez@neuquen.gov.ar>
2021-04-07 01:13:21 -0300

    fix(ficha-epidemiologica): add unique and index

--- modules/forms/forms-epidemiologia/forms-epidemiologia-schema.ts
+++ modules/forms/forms-epidemiologia/forms-epidemiologia-schema.ts
@@ -2,8 +2,8 @@ import { AuditPlugin } from '@andes/mongoose-plugin-audit';
 import * as mongoose from 'mongoose';
 
 export const FormsEpidemiologiaSchema = new mongoose.Schema({
-    identifier: String,
-    type: String,
+    identifier: { type: String, unique: true },
+    type: { type: String },
     paciente: {
         id: { type: mongoose.Schema.Types.ObjectId, ref: 'paciente' },
         documento: String,
@@ -14,6 +14,10 @@ export const FormsEpidemiologiaSchema = new mongoose.Schema({
     secciones: [Object]
 });
 
+
+FormsEpidemiologiaSchema.index({
+    identifier: 1,
+});
 FormsEpidemiologiaSchema.plugin(AuditPlugin);
 
 export const FormsEpidemiologia = mongoose.model('formsEpidemiologia', FormsEpidemiologiaSchema, 'formsEpidemiologia');
