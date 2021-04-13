6a37507675ecd0b7351e0581252b5cedb4c2f08a
mcele <MCele>
2021-04-13 12:50:51 -0300

    fix(mpi): resuelve error por job que finaliza antes de lo esperado

--- core-v2/mpi/jobs/importarNacimientosJob.ts
+++ core-v2/mpi/jobs/importarNacimientosJob.ts
@@ -1,10 +1,8 @@
 import { importarNacimientos, importarDocumentosAsignados } from './nacimientosProcess';
 
 async function run(done) {
-    // PARAMETRO FECHA OPCIONAL PARA TESTEAR , el formato debe ser 'yyyy-mm-dd'
-    // let fecha='yyyy-mm-dd'
-    await importarNacimientos(done);
-    await importarDocumentosAsignados(done);
+    await importarNacimientos();
+    await importarDocumentosAsignados();
     done();
 }
 
