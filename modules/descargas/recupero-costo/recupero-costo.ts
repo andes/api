0c2ecfc0234753f93ba0aaa674fcd150fb28b864
rodrigokokot <44590646+rodrigokokot@users.noreply.github.com>
2021-06-04 14:48:46 -0300

    DESCARGAS - Recupero de costo (anexo 2) (#1417)
    * muestra de datos y correccion de diseño
    
    * fix lint
    
    * fix calculo edad
    
    * fix lint
    
    * feat(recupero-anexo-2): se mejora layout
    
    * feat(recupero-anexo-2): se agrega interface
    
    Co-authored-by: Andrés Boc-Ho <andresin@gmail.com>

--- modules/descargas/recupero-costo/recupero-costo.ts
+++ modules/descargas/recupero-costo/recupero-costo.ts
@@ -1,6 +1,10 @@
 import { InformePDF, getAssetsURL } from '../model/informe.class';
 import { RecuperoCostoBody } from './recupero-costo-body';
 import { RecuperoCostoHeader } from './recupero-costo-header';
+import { getTurnoById } from '../../../modules/turnos/controller/turnosController';
+import { Auth } from '../../../auth/auth.class';
+import { Organizacion } from '../../../core/tm/schemas/organizacion';
+import { Paciente } from '../../../core-v2/mpi/paciente/paciente.schema';
 
 export class RecuperoCosto extends InformePDF {
     constructor(public req) {
@@ -12,8 +16,16 @@ export class RecuperoCosto extends InformePDF {
     ];
 
     public async process() {
+
+        const dataTurno: any = await getTurnoById(this.req.body.turnoId);
+        const organizacionId = Auth.getOrganization(this.req);
+        const organizacion: any = await Organizacion.findById(organizacionId).select('nombre codigo');
+
+        const paciente = await Paciente.findById(dataTurno.turno.paciente.id);
+        dataTurno.turno.paciente.fechaFallecimiento = paciente.fechaFallecimiento;
+
         this.header = new RecuperoCostoHeader();
-        this.body = new RecuperoCostoBody(this.req.body);
+        this.body = new RecuperoCostoBody({ ...dataTurno, organizacion });
 
         await super.process();
     }
