eb31f3ba8f638e1939b44b8cf14dd9dfdecfc40e
Lautaro Molina <negro89_nqn@hotmail.com>
2021-06-18 14:45:18 -0300

    ref(in): agrega atributo esCensable al esquema de prestacion
    permite que las internaciones censables se tengan en cuenta durante los censos

--- modules/rup/schemas/prestacion.ts
+++ modules/rup/schemas/prestacion.ts
@@ -148,6 +148,10 @@ export const PrestacionSchema = new Schema({
         elementoRUP: SchemaTypes.ObjectId
 
     },
+    esCensable: {
+        type: Boolean,
+        required: false
+    },
     tags: Schema.Types.Mixed,
     // Historia de estado de la prestación
     estados: [PrestacionEstadoSchema],
