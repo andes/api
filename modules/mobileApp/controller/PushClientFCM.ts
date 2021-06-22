c60f4293722beba418710c83e6b31905f23e630a
Andrés Boc-Ho <andresin@gmail.com>
2021-06-22 15:15:20 -0300

    refactor: push notifications con request()

--- modules/mobileApp/controller/PushClientFCM.ts
+++ modules/mobileApp/controller/PushClientFCM.ts
@@ -1,31 +1,38 @@
-import fetch from 'node-fetch';
+import * as request from 'request';
 import { pushNotificationsSettings } from './../../../config.private';
 
 export async function sendPushNotification(token, notification: any) {
+    return new Promise((resolve, reject) => {
+        const payload = {
+            notification: {
+                title: notification.title || 'Andes Salud',
+                body: notification.body,
+                sound: 'default',
+                icon: 'fcm_push_icon',
+            },
+            data: {
+                extraData: notification.extraData
+            },
+            to: token,
+            priority: 'high',
+            restricted_package_name: '',
+            time_to_live: 0,
+        };
 
-    const payload = {
-        notification: {
-            title: notification.title || 'Lala',
-            body: notification.body,
-            sound: 'default',
-            icon: 'fcm_push_icon',
-        },
-        data: {
-            extraData: notification.extraData
-        },
-        to: token,
-        priority: 'high',
-        restricted_package_name: '',
-        time_to_live: 0,
-    };
 
-    return await fetch('https://fcm.googleapis.com/fcm/send', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `key=${pushNotificationsSettings.gcm.id}`,
-        },
-        body: JSON.stringify(payload)
-    });
+        request('https://fcm.googleapis.com/fcm/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `key=${pushNotificationsSettings.gcm.id}`,
+            },
+            body: JSON.stringify(payload)
+        }, (error, response, body) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve([response, body])
+        });
+    })
 
 }
