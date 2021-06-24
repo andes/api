9154ee33105e30fc869f7d649d9fe8e3f8c54fa9
Andrés Boc-Ho <andresin@gmail.com>
2021-06-24 12:09:22 -0300

    feat(push-notifications): usa andes-services

--- modules/mobileApp/controller/PushClientFCM.ts
+++ modules/mobileApp/controller/PushClientFCM.ts
@@ -1,38 +1,22 @@
-import * as request from 'request';
-import { pushNotificationsSettings } from './../../../config.private';
+import { services } from '../../../services';
+export interface IPushNotification {
+    title?: 'Andes Salud' | string;
+    body: string;
+    extraData?: any;
+}
 
-export async function sendPushNotification(token, notification: any) {
-    return new Promise((resolve, reject) => {
-        const payload = {
-            notification: {
-                title: notification.title || 'Andes Salud',
-                body: notification.body,
-                sound: 'default',
-                icon: 'fcm_push_icon',
-            },
-            data: {
-                extraData: notification.extraData
-            },
-            to: token,
-            priority: 'high',
-            restricted_package_name: '',
-            time_to_live: 0,
-        };
+export function sendPushNotification(token, notification: IPushNotification) {
+    const payload = {
+        title: notification.title || 'Andes Salud',
+        body: notification.body,
+        extraData: notification.extraData,
+        to: token,
+        priority: 'high',
+        restricted_package_name: '',
+        time_to_live: 0,
+    };
 
-
-        request('https://fcm.googleapis.com/fcm/send', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `key=${pushNotificationsSettings.gcm.id}`,
-            },
-            body: JSON.stringify(payload)
-        }, (error, response, body) => {
-            if (error) {
-                return reject(error);
-            }
-            return resolve([response, body])
-        });
-    })
+    const servicio = 'push-notifications-default';
+    return services.get(servicio).exec(payload);
 
 }
--- utils/roboSender/roboSender.ts
+++ utils/roboSender/roboSender.ts
@@ -5,6 +5,7 @@ import { RoboModel } from './roboSchema';
 import * as debug from 'debug';
 import { PushClient } from '../../modules/mobileApp/controller/PushClient';
 import { createFile } from '../../modules/huds/export-huds/exportHuds.controller';
+import { sendPushNotification } from '../../modules/mobileApp/controller/PushClientFCM';
 
 const log = debug('roboSender');
 
@@ -56,7 +57,13 @@ export function roboSender() {
                             await smsTools.sendSms(smsOptions);
                         }
                         if (env.device_id) {
-                            new PushClient().send(env.device_id, env.notificationData);
+                            try {
+                                new PushClient().send(env.device_id, env.notificationData);
+                                const pushn = await sendPushNotification(env.device_id[0], env.notificationData);
+                                console.log(pushn);
+                            } catch (err) {
+                                console.log(err);
+                            }
                         }
                         // Exportar HUDS
                         if (env.idExportHuds) {
@@ -77,12 +84,12 @@ export function roboSender() {
 
                     counter++;
                     if (finEjecucion(counter, enviosPendientes.length)) {
-                        return resolve();
+                        return resolve(true);
                     }
                 }
             } else {
                 log('Termina la ejecución');
-                return resolve();
+                return resolve(true);
             }
 
         }).catch((err) => {
@@ -103,7 +110,7 @@ function changeState(env, newState) {
             if (err) {
                 return reject();
             }
-            return resolve();
+            return resolve(true);
         });
     });
 }
