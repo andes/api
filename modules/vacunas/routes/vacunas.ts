2ca001a6913b525821a2f4bd63f4815b8ddfdac8
Lautaro Molina <negro89_nqn@hotmail.com>
2021-07-16 16:00:06 -0300

    feat(vac): elimina registros de vacunas y sus cda

--- modules/vacunas/routes/vacunas.ts
+++ modules/vacunas/routes/vacunas.ts
@@ -3,6 +3,7 @@ import * as vacunaCtr from '../controller/vacunas.controller';
 import { asyncHandler } from '@andes/api-tool';
 import { PacienteCtr } from '../../../core-v2/mpi/paciente/paciente.routes';
 import { Auth } from '../../../auth/auth.class';
+import { vacunasApi } from '../schemas/vacunasApi';
 
 const router = express.Router();
 
@@ -22,6 +23,17 @@ router.post('/paciente', Auth.authenticate(), asyncHandler(async (req: any, res,
     try {
         const pacienteId = req.body.paciente.id;
         await vacunaCtr.exportCovid19(null, pacienteId);
+        return res.json({ success: true });
+    } catch (err) {
+        return next(err);
+    }
+}));
+
+
+router.delete('/:idVacuna', Auth.authenticate(), asyncHandler(async (req: any, res, next) => {
+    try {
+        const idVacuna = req.params.idvacuna;
+        await vacunasApi.findOneAndRemove({ idvacuna: idVacuna });
         return res.json({ success: true });
     } catch (err) {
         return next(err);
