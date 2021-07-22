e688a2389571240a44eff7c60769e891e955bf1d
Lautaro Molina <negro89_nqn@hotmail.com>
2021-07-22 12:24:18 -0300

    feat(vac): script para migracion de vacunas

---
+++ scripts/VAC-94.ts
@@ -0,0 +1,36 @@
+
+import { Prestacion } from '../modules/rup/schemas/prestacion';
+import { sincronizarVacunas } from '../modules/vacunas/controller/vacunas.events';
+import { VacunasPacientes } from '../modules/vacunas/schemas/vacunas-pacientes.schema';
+
+async function run(done) {
+    const conceptVacuacionCovid = '840534001';
+    const fechaDesde = new Date('2020-12-01T10:56:10.300-03:00');
+    const hoy = new Date();
+    const query = {
+        'ejecucion.fecha': { $gte: fechaDesde, $lte: hoy },
+        'estadoActual.tipo': 'validada',
+        'ejecucion.registros.concepto.conceptId': conceptVacuacionCovid
+    };
+
+    const prestaciones = Prestacion.find(query).cursor({ batchSize: 100 });
+    const migrarPrestaciones = async (prestacion) => {
+        try {
+            const idPaciente = prestacion.paciente.id;
+            const registroExistente = await VacunasPacientes.findOne({ 'paciente.id': idPaciente });
+            if (!registroExistente) {
+                await sincronizarVacunas(idPaciente.toString());
+                await Prestacion.findOneAndRemove({ 'paciente.id': idPaciente });
+            }
+        } catch (err) {
+            // tslint:disable-next-line:no-console
+            console.error(err);
+        }
+    };
+    await prestaciones.eachAsync(async (prestacion) => {
+        await migrarPrestaciones(prestacion);
+    });
+    done();
+}
+
+export = run;
