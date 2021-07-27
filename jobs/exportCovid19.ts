6deb3962dd373772fca3049fecfe778ac40af693
Mariano Botta <marianoabotta@gmail.com>
2021-07-27 13:15:12 -0300

    feat(vacunas): ajuste de proceso covid (#1487)
    * feat(vacunas): ajuste de proceso covid
    
    * fix(vacunas): ajuste a export vacunas job

--- jobs/exportCovid19.ts
+++ jobs/exportCovid19.ts
@@ -1,6 +1,17 @@
-import * as vacunaCtr from '../modules/vacunas/controller/vacunas.controller';
+import { exportCovid19 } from '../modules/vacunas/controller/vacunas.controller';
 
-export async function exportCovid19(done, horas, desde, hasta) {
-    await vacunaCtr.exportCovid19(horas, null, desde, hasta);
+async function run(done) {
+    // Si length=5 entonces vienen dos argumentos (fechaDesde y fechaHasta)
+    if (process.argv.length === 5) {
+        const desde = process.argv[3];
+        const hasta = process.argv[4];
+        await exportCovid19(null, null, desde, hasta);
+    } else { // Si no es 5 entonces viene 1 solo argumento (horas) o sin argumentos (por defecto una hora)
+        const horas = process.argv[3] || '1';
+        await exportCovid19(horas, null, null, null);
+    }
     done();
 }
+
+export = run;
+
