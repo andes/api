b183779ebd5d6c8ba3d0dfdd9abe2e42520a5e6c
Mariano Andres Botta <marianoabotta@gmail.com>
2021-07-27 08:21:25 -0300

    fix(vacunas): ajuste a export vacunas job

--- jobs/exportCovid19.ts
+++ jobs/exportCovid19.ts
@@ -1,15 +1,16 @@
 import { exportCovid19 } from '../modules/vacunas/controller/vacunas.controller';
 
-function run(done) {
+async function run(done) {
     // Si length=5 entonces vienen dos argumentos (fechaDesde y fechaHasta)
     if (process.argv.length === 5) {
         const desde = process.argv[3];
         const hasta = process.argv[4];
-        exportCovid19(null, null, desde, hasta);
+        await exportCovid19(null, null, desde, hasta);
     } else { // Si no es 5 entonces viene 1 solo argumento (horas) o sin argumentos (por defecto una hora)
         const horas = process.argv[3] || '1';
-        exportCovid19(horas, null, null, null);
+        await exportCovid19(horas, null, null, null);
     }
+    done();
 }
 
 export = run;
