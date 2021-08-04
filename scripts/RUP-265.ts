8f00c59d8266449d10c06af35e375e4e4a74e4fd
Lautaro Molina <37875706+negro89@users.noreply.github.com>
2021-08-04 13:06:06 -0300

    feat(rup): script migracion prestaciones modificadas-anuladas (#1479)
    * feat(rup): script migracion prestaciones modificadas-anuladas
    
    * feat(rup): quita validacion en script
    
    Hay prestaciones viejas que no validan el schema
    
    Co-authored-by: Mariano Andres Botta <marianoabotta@gmail.com>

---
+++ scripts/RUP-265.ts
@@ -0,0 +1,33 @@
+import { Auth } from '../auth/auth.class';
+import { userScheduler } from '../config.private';
+import { Prestacion, PrestacionHistorial } from '../modules/rup/schemas/prestacion';
+
+async function run(done) {
+    const query = {
+        'estadoActual.tipo': { $in: ['modificada', 'anulada'] },
+        inicio: { $ne: 'top' }
+    };
+    const prestaciones = Prestacion.find(query).cursor({ batchSize: 100 });
+
+    const migrarPrestaciones = async (prestacion) => {
+        try {
+            const raw = prestacion.toJSON();
+            const newPrestacionHistorial = new PrestacionHistorial(raw);
+            Auth.audit(newPrestacionHistorial, userScheduler as any);
+            await newPrestacionHistorial.save({ validateBeforeSave: false });
+            await Prestacion.remove({ _id: prestacion.id });
+
+        } catch (err) {
+            // tslint:disable-next-line:no-console
+            console.error(err);
+        }
+    };
+
+    await prestaciones.eachAsync(async (prest: any) => {
+        await migrarPrestaciones(prest);
+    });
+
+    done();
+}
+
+export = run;
