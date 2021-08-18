5668b511108c48a2fa06e96cdf3a7bfb8434564b
Pancho <martinebucarey@gmail,com>
2021-08-18 11:06:15 -0300

    feat(script): agrego control del campo organizaciones

--- scripts/EP-111.ts
+++ scripts/EP-111.ts
@@ -7,22 +7,23 @@ async function run(done) {
     for await (const ficha of fichas) {
         const seccionUsuario = ficha.secciones.find(elem => elem.name === 'Usuario');
         const idOrganizacion = seccionUsuario.fields.find(field => (Object.keys(field))[0] === 'organizacion');
-        // Si la organizacion de la ficha es la que esta en el createdBy me ahorro ir a buscarla a la coleccion Organización
-        if (idOrganizacion.organizacion === ficha.createdBy.organizacion.id) {
-            setOrganizacion = {
-                id: ficha.createdBy.organizacion.id,
-                nombre: ficha.createdBy.organizacion.nombre
-            };
-            await updateFicha(ficha, setOrganizacion);
-        } else {
-            const organizacion: any = await Organizacion.findById(idOrganizacion.organizacion);
-            setOrganizacion = {
-                id: organizacion.id,
-                nombre: organizacion.nombre
-            };
-            await updateFicha(ficha, setOrganizacion);
+        if (!idOrganizacion.organizacion.nombre) {
+            // Si la organizacion de la ficha es la que esta en el createdBy me ahorro ir a buscarla a la coleccion Organización
+            if (idOrganizacion.organizacion === ficha.createdBy.organizacion.id) {
+                setOrganizacion = {
+                    id: ficha.createdBy.organizacion.id,
+                    nombre: ficha.createdBy.organizacion.nombre
+                };
+                await updateFicha(ficha, setOrganizacion);
+            } else {
+                const organizacion: any = await Organizacion.findById(idOrganizacion.organizacion);
+                setOrganizacion = {
+                    id: organizacion.id,
+                    nombre: organizacion.nombre
+                };
+                await updateFicha(ficha, setOrganizacion);
+            }
         }
-
     }
     done();
 }
