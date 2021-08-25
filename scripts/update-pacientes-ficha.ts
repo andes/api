6bc7fb3a2bab2346534613a4975ccaf226927b2c
Pancho <martinebucarey@gmail,com>
2021-08-25 13:19:45 -0300

    fix(form): find mongo

--- scripts/update-pacientes-ficha.ts
+++ scripts/update-pacientes-ficha.ts
@@ -1,19 +1,16 @@
-import { FormEpidemiologiaCtr } from '../modules/forms/forms-epidemiologia/forms-epidemiologia.routes';
 import { FormsEpidemiologia } from '../modules/forms/forms-epidemiologia/forms-epidemiologia-schema';
 import { Types } from 'mongoose';
 
 async function run(done) {
 
-    const fichas = await FormEpidemiologiaCtr.search({});
+    const fichas: any = await FormsEpidemiologia.find({ 'paciente.id': { $type: 'string' } });
     for await (const ficha of fichas) {
         const paciente = ficha.paciente;
-        if (typeof (paciente.id) === 'string') {
-            paciente.id = new Types.ObjectId(paciente.id);
-            await FormsEpidemiologia.update(
-                { _id: ficha.id },
-                { $set: { paciente } }
-            );
-        }
+        paciente.id = new Types.ObjectId(paciente.id);
+        await FormsEpidemiologia.update(
+            { _id: ficha.id },
+            { $set: { paciente } }
+        );
     }
     done();
 }
