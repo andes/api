a0a7ccbd9a21b89161c84cf47eb87e9844fd93f3
Pancho <martinebucarey@gmail,com>
2021-10-21 08:44:39 -0300

    ref(script): cantDosis

--- scripts/VAC-108.ts
+++ scripts/VAC-108.ts
@@ -1,9 +1,10 @@
 import { VacunasPacientes } from '../modules/vacunas/schemas/vacunas-pacientes.schema';
 
 async function run(done) {
+    const from = new Date('2021-02-01T00:00:00.000-03:00');
     const vacunasPacientes: any = VacunasPacientes.aggregate([
-        { $match: { 'aplicaciones.fechaAplicacion': { $gte: new Date('2021-10-03T00:00:00.000-03:00') } } },
-        { $group: { _id: '$paciente.id', count: { $sum: 1 }, id: { $first: '$_id' } } },
+        { $match: { 'aplicaciones.fechaAplicacion': { $gte: from }, cantDosis: 1 } },
+        { $group: { _id: '$paciente.id', count: { $sum: 1 }, id: { $last: '$_id' } } },
         { $match: { _id: { $ne: null }, count: { $gt: 1 } } },
         { $project: { _id: '$id' } }
     ]).cursor({ batchSize: 100 });
