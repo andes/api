177c306a80f38e02bcf6c61b4384578a44b1942f
martinebucarey <56874534+martinebucarey@users.noreply.github.com>
2021-10-26 08:14:54 -0300

    ref(script): vacunas duplicadas (#1590)
    Co-authored-by: Pancho <martinebucarey@gmail,com>

--- scripts/VAC-108.ts
+++ scripts/VAC-108.ts
@@ -1,21 +1,19 @@
+import { sincronizarVacunas } from '../modules/vacunas/controller/vacunas.events';
 import { VacunasPacientes } from '../modules/vacunas/schemas/vacunas-pacientes.schema';
 
 async function run(done) {
     const from = new Date('2021-02-01T00:00:00.000-03:00');
     const vacunasPacientes: any = VacunasPacientes.aggregate([
-        { $match: { 'aplicaciones.fechaAplicacion': { $gte: from }, cantDosis: 1 } },
-        { $group: { _id: '$paciente.id', count: { $sum: 1 }, id: { $last: '$_id' } } },
+        { $match: { 'aplicaciones.fechaAplicacion': { $gte: from } } },
+        { $group: { _id: '$paciente.id', count: { $sum: 1 } } },
         { $match: { _id: { $ne: null }, count: { $gt: 1 } } },
-        { $project: { _id: '$id' } }
+        { $project: { pacienteId: '$_id' } }
     ]).cursor({ batchSize: 100 });
 
-    const ids = [];
-    for await (const vac of vacunasPacientes) {
-        ids.push(vac._id);
+    for await (const pacienteId of vacunasPacientes) {
+        await VacunasPacientes.remove({ 'paciente.id': pacienteId._id });
+        await sincronizarVacunas(pacienteId._id);
     }
-
-    await VacunasPacientes.remove({ _id: { $in: ids } });
-
     done();
 }
 
