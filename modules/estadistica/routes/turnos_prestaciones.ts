329d519451624bcb6af54ca95cb6ff545b257577
mariano <marianocampe94@gmail.com>
2022-01-26 08:28:39 -0300

    feat(CIT-147): descarga del csv del listado de pacientes en turnos y prestaciones

--- modules/estadistica/routes/turnos_prestaciones.ts
+++ modules/estadistica/routes/turnos_prestaciones.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as mongoose from 'mongoose';
+import * as moment from 'moment';
 import { Auth } from '../../../auth/auth.class';
 import * as agendaController from './../controller/procesarAgendas';
 import * as fueraDeAgendaController from './../controller/procesarFueraDeAgenda';
@@ -37,5 +38,52 @@ router.get('/turnos_prestaciones', async (req, res, next) => {
         return next(error);
     }
 });
+
+router.post('/turnos_prestaciones/csv', async (req, res, next) => {
+    if (!Auth.check(req, 'turnosPrestaciones:buscar')) {
+        return next(403);
+    }
+
+    const csv = require('fast-csv');
+    const fs = require('fs');
+    const ws = fs.createWriteStream('/tmp/turnos-prestaciones.csv', { encoding: 'utf8' });
+
+    try {
+        csv
+            .write(req.body, {
+                headers: true, transform: (row) => {
+                    return {
+                        Fecha: (row.fecha) ? moment(row.fecha).format('DD-MM-YYYY') : '',
+                        Documento: (row.paciente) ? row.paciente.documento : '',
+                        Paciente: (row.paciente) ? row.paciente.apellido+', '+row.paciente.nombre : '',
+                        'Tipo de Prestación': (row.prestacion) ? row.prestacion.term : '',
+                        'Equipo de Salud': (row.profesionales.length) ? obtenerProfesionales(row.profesionales) : 'Profesional no asignado',
+                        Estado: (row.estado) ? row.estado : '',
+                        Financiador: (row.paciente.obraSocial) ? row.paciente.obraSocial.nombre : 'No posee'
+                    };
+                }
+            })
+            .pipe(ws)
+            .on('finish', () => {
+                res.download(('/tmp/turnos-prestaciones.csv' as string), (err) => {
+                    if (err) {
+                        next(err);
+                    } else {
+                        next();
+                    }
+                });
+            });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+function obtenerProfesionales(profesionales) {
+    let salida='';
+    profesionales.forEach(element => {
+        salida += element.apellido+', '+element.nombre+' - ';
+    });
+    return salida.slice(0,-3);
+}
 
 export = router;
