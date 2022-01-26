82a1c3ab91b618ea7e84fb706953f3b720f51775
mariano <marianocampe94@gmail.com>
2022-01-26 15:06:04 -0300

    feat(CIT-147): modificacion en la descarga del csv

---
+++ modules/estadistica/controller/turnos_prestacionesController.ts
@@ -0,0 +1,35 @@
+import * as agendaController from './../controller/procesarAgendas';
+import * as fueraDeAgendaController from './../controller/procesarFueraDeAgenda';
+import { Types } from 'mongoose';
+
+export async function armarListado(datos) {
+    const parametros = {
+        organizacion: Types.ObjectId(datos.organizacion),
+        fechaDesde: datos.fechaDesde,
+        fechaHasta: datos.fechaHasta,
+        prestacion: datos.prestacion,
+        estado: datos.estado,
+        profesional: datos.idProfesional,
+        financiador: datos.financiador,
+        estadoFacturacion: datos.estadoFacturacion,
+        documento: datos.documento,
+        ambito: datos.ambito
+    };
+    // Procesa los turnos aplicando los filtros
+    const _turnos = parametros?.ambito === 'internacion' ? [] : agendaController.procesar(parametros);
+    // Procesa las prestaciones fuera de agenda
+    const _prestaciones = fueraDeAgendaController.procesar(parametros);
+    const [turnos, prestaciones] = await Promise.all([_turnos, _prestaciones]);
+    const resultado: any = turnos.concat(prestaciones);
+    return resultado;
+
+}
+
+export function obtenerProfesionales(profesionales) {
+    let salida = '';
+    profesionales.forEach(element => {
+        salida += element.apellido + ', ' + element.nombre + ' - ';
+    });
+    return salida.slice(0,-3);
+}
+
--- modules/estadistica/routes/turnos_prestaciones.ts
+++ modules/estadistica/routes/turnos_prestaciones.ts
@@ -4,35 +4,18 @@ import * as moment from 'moment';
 import { Auth } from '../../../auth/auth.class';
 import * as agendaController from './../controller/procesarAgendas';
 import * as fueraDeAgendaController from './../controller/procesarFueraDeAgenda';
+import * as turnos_prestacionesConstroller from './../controller/turnos_prestacionesController';
 
 
 const router = express.Router();
 
 router.get('/turnos_prestaciones', async (req, res, next) => {
-
     if (!Auth.check(req, 'turnosPrestaciones:buscar')) {
         return next(403);
     }
 
-    const parametros = {
-        organizacion: new mongoose.Types.ObjectId(Auth.getOrganization(req)),
-        fechaDesde: req.query.fechaDesde,
-        fechaHasta: req.query.fechaHasta,
-        prestacion: req.query.prestacion,
-        estado: req.query.estado,
-        profesional: req.query.idProfesional,
-        financiador: req.query.financiador,
-        estadoFacturacion: req.query.estadoFacturacion,
-        documento: req.query.documento,
-        ambito: req.query.ambito
-    };
     try {
-        // Procesa los turnos aplicando los filtros
-        const _turnos = parametros?.ambito === 'internacion' ? [] : agendaController.procesar(parametros);
-        // Procesa las prestaciones fuera de agenda
-        const _prestaciones = fueraDeAgendaController.procesar(parametros);
-        const [turnos, prestaciones] = await Promise.all([_turnos, _prestaciones]);
-        const resultado: any = turnos.concat(prestaciones);
+        const resultado = await turnos_prestacionesConstroller.armarListado(req.query);
         res.json(resultado);
     } catch (error) {
         return next(error);
@@ -40,50 +23,35 @@ router.get('/turnos_prestaciones', async (req, res, next) => {
 });
 
 router.post('/turnos_prestaciones/csv', async (req, res, next) => {
+
     if (!Auth.check(req, 'turnosPrestaciones:buscar')) {
         return next(403);
     }
 
-    const csv = require('fast-csv');
-    const fs = require('fs');
-    const ws = fs.createWriteStream('/tmp/turnos-prestaciones.csv', { encoding: 'utf8' });
-
     try {
+        const csv = require('fast-csv');
+        const resultado = await turnos_prestacionesConstroller.armarListado(req.body);
+        res.set('Content-Type', 'text/csv');
+        res.setHeader('Content-disposition', 'attachment; filename=listado.csv');
         csv
-            .write(req.body, {
+            .write(resultado, {
                 headers: true, transform: (row) => {
                     return {
-                        Fecha: (row.fecha) ? moment(row.fecha).format('DD-MM-YYYY') : '',
+                        Fecha: (row.fecha) ? moment(row.fecha).format('YYYY-MM-DD') : '',
                         Documento: (row.paciente) ? row.paciente.documento : '',
-                        Paciente: (row.paciente) ? row.paciente.apellido+', '+row.paciente.nombre : '',
+                        Paciente: (row.paciente) ? row.paciente.apellido + ', ' + row.paciente.nombre : '',
                         'Tipo de Prestación': (row.prestacion) ? row.prestacion.term : '',
-                        'Equipo de Salud': (row.profesionales.length) ? obtenerProfesionales(row.profesionales) : 'Profesional no asignado',
+                        'Equipo de Salud': (row.profesionales.length) ? turnos_prestacionesConstroller.obtenerProfesionales(row.profesionales) : 'Profesional no asignado',
                         Estado: (row.estado) ? row.estado : '',
-                        Financiador: (row.paciente.obraSocial) ? row.paciente.obraSocial.nombre : 'No posee'
+                        Financiador: (row.paciente.obraSocial) ? row.paciente.obraSocial.nombre : 'No posee',
+                        Ambito: (row.ambito) ? row.ambito : ''
                     };
                 }
             })
-            .pipe(ws)
-            .on('finish', () => {
-                res.download(('/tmp/turnos-prestaciones.csv' as string), (err) => {
-                    if (err) {
-                        next(err);
-                    } else {
-                        next();
-                    }
-                });
-            });
+            .pipe(res);
     } catch (err) {
         return next(err);
     }
 });
-
-function obtenerProfesionales(profesionales) {
-    let salida='';
-    profesionales.forEach(element => {
-        salida += element.apellido+', '+element.nombre+' - ';
-    });
-    return salida.slice(0,-3);
-}
 
 export = router;
