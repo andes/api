6e2e60d9522e046e0af14495bc5fb39d61bdfd40
martinebucarey <martinebucarey@gmail.com>
2022-01-27 11:08:08 -0300

    ref(validar-certificado): idPaciente

--- modules/validar-certificado/routes/validar-certificado.ts
+++ modules/validar-certificado/routes/validar-certificado.ts
@@ -1,19 +1,26 @@
 import * as express from 'express';
 import * as moment from 'moment';
 import { Prestacion } from '../../rup/schemas/prestacion';
+import { Paciente } from '../../../core-v2/mpi/paciente/paciente.schema';
+import { IPacienteDoc } from '../../../core-v2/mpi/paciente/paciente.interface';
 
 const router = express.Router();
 
 
 router.get('/', async (req, res) => {
     const { fechaValidacion, documento, sexo } = req.query;
+    const paciente: IPacienteDoc = await Paciente.findOne({ documento, sexo });
+    const pacienteId = paciente?.id;
+    if (!pacienteId) {
+        return res.send(false);
+    }
     const fechaCondicion = {
         $gte: moment(fechaValidacion).startOf('day').toDate(),
         $lte: moment(fechaValidacion).endOf('day').toDate()
     };
     const match = {
         'estadoActual.createdAt': fechaCondicion,
-        'paciente.documento': documento,
+        'paciente.id': pacienteId,
         'paciente.sexo': sexo
     };
     const prestaciones = await Prestacion.find(match);
