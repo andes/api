dbaecc9db7c61bf970268a44848cbf44258b3c94
mariano <marianocampe94@gmail.com>
2022-08-08 12:53:00 -0300

    feat(MAT-110): Agregar campo created en BD

--- modules/matriculaciones/routes/turnoSolicitado.ts
+++ modules/matriculaciones/routes/turnoSolicitado.ts
@@ -1,19 +1,21 @@
 import { Profesional } from '../../../core/tm/schemas/profesional';
 import * as express from 'express';
 import { turnoSolicitado } from '../schemas/turnoSolicitado';
-
+import { Auth } from '../../../auth/auth.class';
+import { userScheduler } from '../../../config.private';
 const router = express.Router();
 
 router.post('/turnoSolicitados', async (req, res, next) => {
     const doc = req.body.documento;
     const sexo = req.body.sexo;
     const profesional = await Profesional.findOne({ documento: doc, sexo, profesionalMatriculado: false });
-    if (profesional && profesional._id) {
+    if (profesional?._id) {
         req.body._id = profesional._id;
         req.body.profesionalMatriculado = true;
     }
-    const newProfesional = new turnoSolicitado(req.body);
-    newProfesional.save(error => {
+    const newProfesional: any = new turnoSolicitado(req.body);
+    Auth.audit(newProfesional, userScheduler as any);
+    newProfesional.save((error) => {
         if (error) {
             return next('error-turno');
         }
