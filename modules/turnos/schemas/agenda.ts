41457c5e596c661f3b052205176dd0f25a7745b7
Fabio-Ramirez <fabioadiranr2@gmail.com>
2023-04-11 16:20:01 -0300

    (feat)CIT - Agregar configuracion de multiprofesional

--- modules/turnos/schemas/agenda.ts
+++ modules/turnos/schemas/agenda.ts
@@ -73,6 +73,10 @@ export const AgendaSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    multiprofesional: {
+        type: Boolean,
+        default: false
+    },
     cupo: Number
 }, { versionKey: false });
 
