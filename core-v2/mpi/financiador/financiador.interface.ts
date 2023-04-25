eba9c3eb1f3a14b8c4d95a37878f274e172cfc58
ma7payne <matich04@gmail.com>
2023-04-25 09:54:17 -0300

    feat(MPI): Agrega detalles al esquema de pacientes

--- core-v2/mpi/financiador/financiador.interface.ts
+++ core-v2/mpi/financiador/financiador.interface.ts
@@ -1,5 +1,6 @@
 import { Document } from 'mongoose';
 
+
 export interface IFinanciador {
     codigoPuco: Number;
     nombre: String;
@@ -7,6 +8,8 @@ export interface IFinanciador {
     numeroAfiliado: String;
     prepaga: Boolean;
     idObraSocial: Number;
+    origen: 'PUCO' | 'SUMAR' | 'ANDES';
+    fechaDeActualizacion: Date;
 }
 
 export interface IFinanciadorDoc extends Document, IFinanciador { }
