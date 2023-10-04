bbeddf0719ce51942cc3909cdfd7d1273907a24e
Lautaro Molina <lautaro.mlagos@gmail.com>
2023-10-04 12:40:34 -0300

    ref(mat): reemplaza callback por async/await

--- modules/matriculaciones/routes/turnoSolicitado.ts
+++ modules/matriculaciones/routes/turnoSolicitado.ts
@@ -8,30 +8,29 @@ const router = express.Router();
 router.post('/turnoSolicitados', async (req, res, next) => {
     const doc = req.body.documento;
     const sexo = req.body.sexo;
-    const profesional = await Profesional.findOne({ documento: doc, sexo, profesionalMatriculado: false });
-    if (profesional?._id) {
-        req.body._id = profesional._id;
-        req.body.profesionalMatriculado = true;
-    }
-    const newProfesional: any = new turnoSolicitado(req.body);
-    Auth.audit(newProfesional, userScheduler as any);
-    newProfesional.save((error) => {
-        if (error) {
-            return next('error-turno');
+    try {
+        const profesional = await Profesional.findOne({ documento: doc, sexo, profesionalMatriculado: false });
+        if (profesional?._id) {
+            req.body._id = profesional._id;
+            req.body.profesionalMatriculado = true;
         }
-        res.json(newProfesional);
-    });
+        const newProfesional = new turnoSolicitado(req.body);
+        Auth.audit(newProfesional, userScheduler as any);
+        const saved = await newProfesional.save();
+        res.json(saved);
+    } catch (err) {
+        return next('error-turno');
+    }
 });
 
-router.get('/turnoSolicitados/traePDni/:dni*?', (req: any, res, next) => {
+router.get('/turnoSolicitados/traePDni/:dni*?', async (req: any, res, next) => {
     const dni = req.params.dni;
-    turnoSolicitado.find({ _id: dni }, (err, data) => {
-        if (err) {
-            return next(err);
-        }
-        res.json(data[0]);
-    });
-
+    try {
+        const turno = await turnoSolicitado.find({ _id: dni });
+        res.json(turno[0]);
+    } catch (err) {
+        return next(err);
+    }
 });
 
 export = router;
