c29b70f171b116e66faf08dfbc689adb13734de8
maring0019 <57371507+maring0019@users.noreply.github.com>
2024-02-28 14:52:09 -0300

    feat(GDU): se desactiva usuario por organizacion (#1789)

--- core/tm/controller/authUser.ts
+++ core/tm/controller/authUser.ts
@@ -1,26 +1,35 @@
 /**
- * Funcion que verifica la ultima vez que se logueo un usuario y si es mayor a 3 meses se desactiva
- * cambiando la variable activo que se encuentra en cada organizacion a false.
+ * Función que verifica la última vez que se actualizó/logueó un usuario.
+ * Si es mayor o igual a 1 mes se desactiva,
+ * cambiando la variable activo que se encuentra en cada organización a false.
  */
 import * as moment from 'moment';
 import { AuthUsers } from '../../../auth/schemas/authUsers';
 
 export async function verificarUltimoLogueo(done) {
-    const usuarios: any = AuthUsers.find({
-        $and:[
-            { lastLogin:{ $lte: moment().subtract(3, 'months').toDate() } },
-            { 'organizaciones.activo':true }
-        ],
-    }).cursor({ batchSize: 100 });
+    const usuarios: any = AuthUsers.find({ 'organizaciones.activo': true }).cursor({ batchSize: 100 });
+    const fLimite = moment().subtract(1, 'months').toDate();
+    const fSinLogin = moment().subtract(2, 'months').toDate();
+    const esMenor = (fecha) => (fecha && fecha < fLimite);
     for await (const user of usuarios) {
-        await AuthUsers.update(
-            { _id:user._id },
+        for (const org of user.organizaciones) {
+            const lastUpdate = user.updatedAt || user.createdAt;
+            if (esMenor(lastUpdate) && esMenor(org.lastLogin)) {
+                org.activo = false;
+            }
+            if (!org.lastLogin && org.activo) {
+                org.lastLogin = user.lastLogin || lastUpdate || fSinLogin;
+            }
+        }
+        await AuthUsers.updateOne(
+            { _id: user._id },
             {
                 $set: {
-                    'organizaciones.$[].activo': false
+                    organizaciones: user.organizaciones
                 }
             }
         );
     }
+
     done();
 }
