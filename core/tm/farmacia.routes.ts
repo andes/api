c344abecd50bbfe239d1273a105b8d3439a05640
MarianoCampetella <marianocampe94@gmail.com>
2024-07-18 11:33:38 -0300

    feat(TM-36): API para ABM de Farmacias

---
+++ core/tm/farmacia.routes.ts
@@ -0,0 +1,24 @@
+import { MongoQuery, ResourceBase } from '@andes/core';
+import { Farmacia, IFarmaciaDoc } from './schemas/farmacia';
+import { EventCore } from '@andes/event-bus/';
+import { Auth } from '../../auth/auth.class';
+
+class FarmaciasResourse extends ResourceBase<IFarmaciaDoc> {
+    Model = Farmacia;
+    resourceName = 'farmacias';
+    resourceModule = 'farmacia';
+    middlewares = [Auth.authenticate()];
+    searchFileds = {
+        denominacion: MongoQuery.partialString,
+        razonSocial: MongoQuery.partialString,
+        cuit: MongoQuery.partialString,
+        DTResponsable: MongoQuery.partialString,
+        matriculaDTResponsable: MongoQuery.equalMatch,
+        disposicionAltaDT: MongoQuery.equalMatch,
+        asociadoA: MongoQuery.equalMatch
+    };
+    eventBus = EventCore;
+}
+
+export const FarmaciasCtr = new FarmaciasResourse({});
+export const FarmaciasRouter = FarmaciasCtr.makeRoutes();
---
+++ core/tm/schemas/farmacia.ts
@@ -0,0 +1,42 @@
+import * as mongoose from 'mongoose';
+import * as direccionSchema from './direccion';
+import { IFarmacia } from '../interfaces/IFarmacia';
+import { AndesDocWithAudit } from '@andes/mongoose-plugin-audit';
+
+const FarmaciaSchema = new mongoose.Schema({
+    denominacion: String,
+    razonSocial: String,
+    cuit: String,
+    DTResponsable: String,
+    matriculaDTResponsable: String,
+    disposicionAltaDT: String,
+    activo: Boolean,
+    farmaceuticosAuxiliares: [],
+    horarios: [],
+    domicilio: { type: direccionSchema },
+    contactos: [],
+    asociadoA: {
+        type: String,
+        enum: ['Colegio de Farmacéuticos', 'Farmacias Sociales', 'Camara de Farmacéuticos', 'Independientes']
+    },
+    disposicionHabilitacion: String,
+    fechaHabilitacion: Date,
+    fechaRenovacion: Date,
+    vencimientoHabilitacion: Date,
+    gabineteInyenctables: Boolean,
+    laboratoriosMagistrales: Boolean,
+    expedientePapel: String,
+    expedienteGDE: String,
+    nroCaja: String,
+    disposiciones: [{
+        numero: String,
+        descripcion: String
+    }],
+    sancion: [{
+        numero: String,
+        descripcion: String
+    }]
+});
+
+export type IFarmaciaDoc = AndesDocWithAudit<IFarmacia>;
+export const Farmacia = mongoose.model<IFarmaciaDoc>('farmacias', FarmaciaSchema, 'farmacias');
