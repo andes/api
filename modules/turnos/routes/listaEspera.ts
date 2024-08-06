99c21379f3a5c5823fc4e957c864596e9495f110
MarianoCampetella <marianocampe94@gmail.com>
2024-08-06 15:32:45 -0300

    feat(CIT-293)Cerrar demanda insatisfecha

--- modules/turnos/routes/listaEspera.ts
+++ modules/turnos/routes/listaEspera.ts
@@ -145,27 +145,60 @@ router.put('/listaEspera/:id', (req, res, next) => {
 });
 
 router.patch('/listaEspera/:id/:datoMod', async (req, res, next) => {
-    listaEspera.findById(req.params.id, (err, data: any) => {
+    listaEspera.findById(req.params.id, async (err, data: any) => {
         if (err) {
             return next(err);
         }
-        const datoMod = req.params.datoMod;
-        if (datoMod === 'demandas') {
-            data.demandas = req.body;
-        }
-        if (datoMod === 'estado') {
-            data.estado = req.body;
-        }
-        if (datoMod === 'llamados') {
-            data.llamados = req.body;
-        }
-        Auth.audit(data, req);
-        data.save((errUpdate) => {
-            if (errUpdate) {
-                return next(errUpdate);
+        try {
+            const datoMod = req.params.datoMod;
+            if (datoMod === 'demandas') {
+                data.demandas = req.body;
             }
-            res.json(data);
-        });
+            if (datoMod === 'estado') {
+                data.estado = req.body.estado;
+                data.resolucion = {
+                    fecha: req.body.fecha,
+                    motivo: req.body.motivo,
+                };
+                if (req.body.observacion) {
+                    data.resolucion.observacion = req.body.observacion;
+                }
+                if (req.body.turno) {
+                    await Agenda.findById(req.body.turno.idAgenda, (error, agenda: any) => {
+                        if (error) {
+                            return next(error);
+                        }
+                        const turnoAgenda = agenda.bloques.map(bloque => bloque.turnos.find(t => t._id.toString() === req.body.turno.id));
+                        const turno = {
+                            id: turnoAgenda[0]._id,
+                            horaInicio: turnoAgenda[0].horaInicio,
+                            tipo: turnoAgenda[0].tipoTurno,
+                            emitidoPor: turnoAgenda[0].emitidoPor,
+                            fechaHoraDacion: turnoAgenda[0].fechaHoraDacion,
+                            profesionales: agenda.profesionales,
+                            idAgenda: req.body.turno.idAgenda,
+                            organizacion: {
+                                id: agenda.organizacion.id,
+                                nombre: agenda.organizacion.nombre
+                            },
+                        };
+                        data.resolucion.turno = turno;
+                    });
+                }
+            }
+            if (datoMod === 'llamados') {
+                data.llamados = req.body;
+            }
+            Auth.audit(data, req);
+            data.save((errUpdate) => {
+                if (errUpdate) {
+                    return next(errUpdate);
+                }
+                res.json(data);
+            });
+        } catch (error) {
+            return next(error);
+        }
     });
 });
 
