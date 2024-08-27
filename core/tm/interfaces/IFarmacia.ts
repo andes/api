a6e2167249dd9c8e463513e465c703df95e70f35
MarianoCampetella <65697028+MarianoCampetella@users.noreply.github.com>
2024-08-27 13:43:21 -0300

    feat(TM-36): API para ABM de Farmacias (#1926)

---
+++ core/tm/interfaces/IFarmacia.ts
@@ -0,0 +1,34 @@
+import { IContacto } from './IContacto';
+import { IDireccion } from './IDireccion';
+
+export interface IFarmacia {
+    denominacion: string;
+    razonSocial: string;
+    cuit: string;
+    DTResponsable: Date;
+    matriculaDTResponsable: string;
+    disposicionAltaDT: string;
+    activo: boolean;
+    farmaceuticosAuxiliares: [];
+    horarios: [];
+    domicilio: IDireccion;
+    contactos: IContacto;
+    asociadoA: string;
+    disposicionHabilitacion: string;
+    fechaHabilitacion: Date;
+    fechaRenovacion: Date;
+    vencimientoHabilitacion: Date;
+    gabineteInyenctables: Boolean;
+    laboratoriosMagistrales: Boolean;
+    expedientePapel: string;
+    expedienteGDE: string;
+    nroCaja: string;
+    disposiciones: [{
+        numero: string;
+        descripcion: string;
+    }];
+    sancion: [{
+        numero: string;
+        descripcion: string;
+    }];
+}
