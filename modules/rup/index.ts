9f9e3e2d074fc9ee778b58acf71fb4e58d29c2d0
ponjaBob <jiramirez_nqn@hotmail.com>
2024-10-23 15:59:27 -0300

    RUP (FEAT) Nuevo esquema receta

--- modules/rup/index.ts
+++ modules/rup/index.ts
@@ -1,6 +1,9 @@
 import * as express from 'express';
 import { ElementoRUPRouter } from './elementos-rup.controller';
 import { CamasRouter, CensosRouter, EstadosRouter, InternacionResumenRouter, InternacionRouter, PlanIndicacionesEventosRouter, PlanIndicacionesRouter, SalaComunRouter } from './internacion';
+
+require('./controllers/rup.events');
+
 export function setup(app: express.Application) {
     app.use('/api/modules/rup', ElementoRUPRouter);
     app.use('/api/modules/rup/internacion', CamasRouter);
@@ -11,8 +14,6 @@ export function setup(app: express.Application) {
     app.use('/api/modules/rup/internacion', InternacionResumenRouter);
     app.use('/api/modules/rup/internacion', PlanIndicacionesRouter);
     app.use('/api/modules/rup/internacion', PlanIndicacionesEventosRouter);
-
-
 }
 
 export { hudsPaciente } from './controllers/prestacion';
---
+++ modules/rup/schemas/receta-schema.ts
@@ -0,0 +1,67 @@
+import { AuditPlugin } from '@andes/mongoose-plugin-audit';
+import * as mongoose from 'mongoose';
+import { PacienteSubSchema } from '../../../core-v2/mpi/paciente/paciente.schema';
+
+const estadosSchema = new mongoose.Schema({
+    estado: {
+        type: String,
+        enum: ['vigente', 'dispensada', 'vencida'],
+        required: true,
+        default: 'vigente'
+    },
+});
+estadosSchema.plugin(AuditPlugin);
+
+export const recetaSchema = new mongoose.Schema({
+    organizacion: {
+        id: mongoose.SchemaTypes.ObjectId,
+        nombre: String
+    },
+    profesional: {
+        id: mongoose.SchemaTypes.ObjectId,
+        nombre: String,
+        apellido: String,
+        documento: String,
+        profesion: String,
+        especialidad: String,
+        matricula: Number
+    },
+    fechaRegistro: Date,
+    fechaPrestacion: Date,
+    idPrestacion: String,
+    idRegistro: String,
+    medicamento: {
+        conceptId: String,
+        term: String, // (Descripción)
+        presentacion: String,
+        unidades: String, // (mg, cc, etc.)
+        cantidad: Number,
+        cantEnvases: Number,
+        dosisDiaria: {
+            dosis: String,
+            frecuencia: mongoose.SchemaTypes.Mixed,
+            dias: Number,
+            notaMedica: String
+        },
+        tratamientoProlongado: Boolean,
+        tiempoTratamiento: mongoose.SchemaTypes.Mixed,
+    },
+    estados: [estadosSchema],
+    estadoActual: {
+        type: String,
+        required: true,
+        default: 'vigente'
+    },
+    paciente: PacienteSubSchema,
+    renovacion: String, // (referencia al registro original)
+    vinculoRecetar: String,
+    vinculoSifaho: String
+});
+
+recetaSchema.plugin(AuditPlugin);
+
+recetaSchema.index({
+    idPrestacion: 1,
+});
+
+export const Receta = mongoose.model('receta', recetaSchema, 'receta');
