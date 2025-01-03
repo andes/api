4b764a5729c6571157f268e33c75f7637523a10a
mcele <celeste.ramos@fi.uncoma.edu.ar>
2025-01-03 12:10:11 -0300

    fix(CITAS): Cambia fecha fin job actualizar agendas

--- jobs/actualizarAgendasJob.ts
+++ jobs/actualizarAgendasJob.ts
@@ -4,7 +4,7 @@ import moment = require('moment');
 function run(done) {
     const fechaActualizar = moment(new Date());
     const start = (moment(fechaActualizar).startOf('day').subtract(1, 'days').toDate() as any);
-    const end = (moment(fechaActualizar).endOf('day').toDate() as any);
+    const end = (moment(start).endOf('day').toDate() as any);
     Promise.all([
         agendaCtrl.actualizarTiposDeTurno(),
         agendaCtrl.actualizarEstadoAgendas(start, end)
