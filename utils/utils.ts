d711ca7560a4f04849dd62526b02d063f95744b1
Fabio-Ramirez <fabioadiranr2@gmail.com>
2025-01-14 13:10:31 -0300

    FIX - fechaNacimiento al momento de generar credencial

--- utils/utils.ts
+++ utils/utils.ts
@@ -221,7 +221,12 @@ export function xmlToJson(xmlString) {
     return _xmlToJson(parser.parseFromString(xmlString, 'application/xml'));
 }
 
-export function getDateStr(date: Date): String {
+export function getDateStr(date: string | Date): string {
     const dt = new Date(date);
-    return dt.getDate() + '/' + (dt.getMonth() + 1) + '/' + dt.getFullYear();
+
+    const day = dt.getUTCDate(); // Día en UTC
+    const month = dt.getUTCMonth() + 1; // Mes en UTC (getUTCMonth es base 0)
+    const year = dt.getUTCFullYear(); // Año en UTC
+
+    return `${day}/${month}/${year}`;
 }
