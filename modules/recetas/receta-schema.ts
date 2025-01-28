194a95f049a341039df5528fe8aa120b768ad322
ma7payne <matich04@gmail.com>
2025-01-28 15:57:20 -0300

    feat(RUP): implementa suspension de recetas

--- modules/recetas/receta-schema.ts
+++ modules/recetas/receta-schema.ts
@@ -1,7 +1,29 @@
 import { AuditPlugin } from '@andes/mongoose-plugin-audit';
 import * as mongoose from 'mongoose';
 import { PacienteSubSchema } from '../../core-v2/mpi/paciente/paciente.schema';
+import { Profesional } from '../../core/tm/schemas/profesional';
 import { SnomedConcept } from '../rup/schemas/snomed-concept';
+
+export const motivosRecetaSchema = new mongoose.Schema({
+    label: {
+        type: String,
+        required: true
+    },
+    descripcion: {
+        type: String,
+        required: false
+    }
+});
+
+// schema de organizacion con id y nombre
+// id: {
+//     type: String,
+//         required: false
+// },
+// nombre: {
+//     type: String,
+//         required: false
+// }
 
 const estadosSchema = new mongoose.Schema({
     tipo: {
@@ -9,8 +31,31 @@ const estadosSchema = new mongoose.Schema({
         enum: ['vigente', 'finalizada', 'vencida', 'suspendida', 'rechazada'],
         required: true,
         default: 'vigente'
+    },
+    motivo: {
+        type: String,
+        required: false
+    },
+    observacion: {
+        type: String,
+        required: false
+    },
+    profesional: {
+        type: Profesional.schema,
+        required: false
+    },
+    organizacionExterna: {
+        id: {
+            type: String,
+            required: false
+        },
+        nombre: {
+            type: String,
+            required: false
+        }
     }
 });
+
 estadosSchema.plugin(AuditPlugin);
 
 const estadoDispensaSchema = new mongoose.Schema({
@@ -63,13 +108,16 @@ export const recetaSchema = new mongoose.Schema({
     },
     dispensa: [
         {
-            codigo: String,
             descripcion: String,
             cantidad: Number,
+            medicamento: SnomedConcept,
+            presentacion: String,
+            unidades: String,
+            cantidadEnvases: Number,
             organizacion: {
-                id: mongoose.SchemaTypes.ObjectId,
+                id: String,
                 nombre: String
-            }
+            },
         }
     ],
     estados: [estadosSchema],
@@ -81,6 +129,19 @@ export const recetaSchema = new mongoose.Schema({
     appNotificada: [{ app: String, fecha: Date }]
 });
 
+recetaSchema.pre('save', function (next) {
+    const receta: any = this;
+
+    if (receta.estados && receta.estados.length > 0) {
+        receta.estadoActual = receta.estados[receta.estados.length - 1];
+    }
+    if (receta.estadosDispensa && receta.estadosDispensa.length > 0) {
+        receta.estadoDispensaActual = receta.estadosDispensa[receta.estadosDispensa.length - 1];
+    }
+
+    next();
+});
+
 recetaSchema.plugin(AuditPlugin);
 
 recetaSchema.index({
@@ -88,3 +149,4 @@ recetaSchema.index({
 });
 
 export const Receta = mongoose.model('receta', recetaSchema, 'receta');
+export const MotivosReceta = mongoose.model('motivosReceta', motivosRecetaSchema, 'motivosReceta');
--- modules/recetas/recetas.routes.ts
+++ modules/recetas/recetas.routes.ts
@@ -1,15 +1,13 @@
 import { asyncHandler, Request, Response } from '@andes/api-tool';
-
 import { MongoQuery, ResourceBase } from '@andes/core';
 import { Auth } from '../../auth/auth.class';
 import { Receta } from './receta-schema';
-import { BuscarRecetas } from './recetasController';
-
+import { buscarRecetas, getMotivosReceta, setEstadoDispensa, suspender } from './recetasController';
 
 class RecetasResource extends ResourceBase {
     Model = Receta;
     resourceName = 'recetas';
-    routesEnable = ['get'];
+    routesEnable = ['get, patch'];
     middlewares = [Auth.authenticate()];
     searchFileds = {
         paciente: {
@@ -28,16 +26,40 @@ class RecetasResource extends ResourceBase {
 }
 
 export const get = async (req, res) => {
-    try {
-        const receta = await BuscarRecetas(req);
+    const result = await buscarRecetas(req);
 
-        res.json(receta);
-    } catch (err) {
-        return res.json(err);
+    res.json(result);
+};
+
+export const getMotivos = async (req, res) => {
+    const result = await getMotivosReceta(req);
+
+    res.json(result);
+};
+
+
+export const patch = async (req, res) => {
+    switch (req.body.op) {
+        case 'suspender':
+            const result = await suspender(req);
+            return res.json(result);
+        case 'dispensar':
+        case 'dispensa-parcial':
+        case 'rechazar':
+            const resultDispensa = await setEstadoDispensa(req);
+            return res.json(resultDispensa);
+        default:
+            return res.status(400).json({ error: 'Operación no soportada' });
     }
 };
 
 export const RecetasCtr = new RecetasResource({});
 export const RecetasRouter = RecetasCtr.makeRoutes();
 
-RecetasRouter.get('/recetas', Auth.authorize('recetas:read'), asyncHandler(get));
+const authorizeByToken = async (req: Request, res: Response, next) =>
+    Auth.authorizeByToken(req, res, next, ['huds:visualizacionHuds', 'recetas:read']);
+
+RecetasRouter.use(Auth.authenticate());
+RecetasRouter.get('/recetas', authorizeByToken, asyncHandler(get));
+RecetasRouter.get('/recetas/motivos', asyncHandler(getMotivos));
+RecetasRouter.patch('/recetas', authorizeByToken, asyncHandler(patch));
