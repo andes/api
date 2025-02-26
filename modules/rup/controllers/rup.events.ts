4f3fdadad97272fe3bc01d207154c2aa4cb76999
MarianoCampetella <marianocampe94@gmail.com>
2025-02-26 14:48:19 -0300

    feat(RUP-458): Modificar el concept snomed de receta

--- modules/rup/controllers/rup.events.ts
+++ modules/rup/controllers/rup.events.ts
@@ -5,8 +5,7 @@ import * as mongoose from 'mongoose';
 import { userScheduler } from '../../../config.private';
 import { Receta } from '../../recetas/receta-schema';
 import { rupEventsLog as logger } from './rup.events.log';
-
-const conceptIds = ['33633005', '103742009']; // Receta y renovación de receta
+import { ECLQueries } from '../../../core/tm/schemas/eclqueries.schema';
 
 EventCore.on('prestacion:receta:create', async (prestacion) => {
     try {
@@ -33,7 +32,11 @@ EventCore.on('prestacion:receta:create', async (prestacion) => {
             id: prestacion.ejecucion.organizacion.id,
             nombre: prestacion.ejecucion.organizacion.nombre
         };
-
+        const conceptIds = [];
+        const query: any = await ECLQueries.find({ key: /prescripcion/ig });
+        for (const item of query) {
+            conceptIds.push(item.valor);
+        }
         for (const registro of registros) {
             if (conceptIds.includes(registro.concepto.conceptId)) {
                 for (const medicamento of registro.valor.medicamentos) {
