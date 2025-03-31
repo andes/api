2f27a3ab242e7118993a90a9ef6dab4d9724a7d5
Fabio-Ramirez <fabioadiranr2@gmail.com>
2025-03-31 11:50:39 -0300

    HUDS - Agregar estado y tipo de muestra

--- modules/descargas/laboratorio/laboratorio-body.ts
+++ modules/descargas/laboratorio/laboratorio-body.ts
@@ -6,19 +6,30 @@ export class FarmaciaBody extends HTMLComponent {
       <main>
         <table border="1" width="100%" class="tabla-header">
           <tr>
-          <td width="35%"><b>Paciente: </b>{{encabezado.data.apellido }} {{encabezado.data.nombre}}</td>
-          <td width="15%"><b>Fecha de Nacimiento: </b>{{encabezado.data.fechanacimiento}}</td>
+          <td width="30%"><b>Paciente: </b>{{encabezado.data.apellido }}   {{encabezado.data.nombre}}</td>
+          <td width="20%"><b>Fecha de Nacimiento: </b>{{encabezado.data.fechanacimiento}}</td>
           <td width="15%"><b>Género: </b>{{encabezado.data.sexo}}</td>
           <td width="15%"><b>DNI: </b>{{encabezado.data.documento}}</td>
         </tr>
         <tr>
-          <td width="35%"><b>Solicitante: </b>{{encabezado.data.medicoSolicitante}}</td>
-          <td width="15%"><b>Orden Nro: </b> {{encabezado.data.numero}}</td>
+          <td width="30%"><b>Solicitante: </b>{{encabezado.data.medicoSolicitante}}</td>
+          <td width="20%"><b>Orden Nro: </b> {{encabezado.data.numero}}</td>
           <td width="15%"><b>Fecha: </b>{{encabezado.data.fecha}}</td>
           <td width="15%"><b>Origen: </b>{{encabezado.data.origen}}</td>
         </tr>
-          <td colspan="1"><b>Efector solicitante: </b>{{encabezado.data.efectorSolicitante}}</td>
-          <td colspan="3"><b>Laboratorio: </b>{{encabezado.data.Laboratorio}}</td>  	
+        <tr>
+          <td width="30%"><b>Efector solicitante: </b>{{encabezado.data.efectorSolicitante}}</td>
+          <td width="20%"><b>Laboratorio: </b>{{encabezado.data.Laboratorio}}</td>
+          {{#if encabezado}}
+                  {{#if encabezado.data}}
+                        {{#if encabezado.data.estado}}
+                                <td width="15%"><b>Estado: </b>{{encabezado.data.estado}}</td>
+                        {{/if}}
+                  {{/if}}
+          {{/if}}
+          <td width="15%"><b>Tipo de muestra: </b>{{encabezado.data.tipoMuestra}}</td>
+        </tr>
+         <td width="50%"><b>Prácticas solicitadas: </b>{{encabezado.data.practicasSolicitadas}}</td>
       </table>
 
       {{#each areas}}
@@ -54,6 +65,12 @@ export class FarmaciaBody extends HTMLComponent {
         this.encabezado.data.fecha = moment(this.encabezado.data.fecha).format('DD-MM-YYYY');
         this.encabezado.data.fechanacimiento = moment(this.encabezado.data.fechanacimiento).format('DD-MM-YYYY');
         this.encabezado.data.sexo = this.paciente[0].genero;
+        if (this.encabezado.data.estado !== 'EnProceso') {
+            this.encabezado.data.estado = null;
+        } else {
+            this.encabezado.data.estado = 'En Proceso';
+        }
+
         if (this.paciente[0].alias) {
             this.encabezado.data.nombre = this.paciente[0].alias;
         }
