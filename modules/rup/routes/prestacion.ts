9a19da9017093f36d7863a42647fc7e11fe9c713
Pablo Silveira <psilveira@villagemd.com>
2025-06-03 20:16:47 -0300

    lint

--- modules/rup/routes/prestacion.ts
+++ modules/rup/routes/prestacion.ts
@@ -2,6 +2,7 @@ import { asyncHandler, Request } from '@andes/api-tool';
 import { MongoQuery } from '@andes/core';
 import { EventCore } from '@andes/event-bus';
 import * as express from 'express';
+import { saveTurnoProfesional } from '../../turnos/controller/agenda';
 import * as moment from 'moment';
 import { Types } from 'mongoose';
 import { AppCache } from '../../../connections';
@@ -10,7 +11,6 @@ import { SnomedCtr } from '../../../core/term/controller/snomed.controller';
 import { getObraSocial } from '../../../modules/obraSocial/controller/obraSocial';
 import { removeDiacritics } from '../../../utils/utils';
 import { getVisualizadorURL } from '../../pacs';
-import { saveTurnoProfesional } from '../../turnos/controller/agenda';
 import { getTurnoById } from '../../turnos/controller/turnosController';
 import { elementosRUPAsSet } from '../controllers/elementos-rup.controller';
 import { dashboardSolicitudes } from '../controllers/estadisticas';
@@ -286,15 +286,7 @@ router.get('/prestaciones/solicitudes', async (req: any, res, next) => {
         if (req.query.estados) {
             match.$and.push({ 'estadoActual.tipo': { $in: (typeof req.query.estados === 'string') ? [req.query.estados] : req.query.estados } });
         }
-
         pipeline.push({ $match: match });
-
-        if (req.query.conceptoAsociado) {
-            pipeline.push({
-                $match: { 'solicitud.registros.valor.solicitudPrestacion.conceptoAsociado.conceptId': req.query.conceptoAsociado }
-            });
-        }
-
         pipeline.push({ $addFields: { registroSolicitud: { $arrayElemAt: ['$solicitud.registros', 0] } } });
         const project = {
             $project: {
@@ -316,7 +308,7 @@ router.get('/prestaciones/solicitudes', async (req: any, res, next) => {
                         then: -1,
                         else: 1
                     }
-                },
+                }
             }
         };
 
@@ -419,10 +411,7 @@ router.get('/prestaciones', async (req: any, res, next) => {
         query.where('solicitud.profesional.id').equals(req.query.idProfesional);
     }
     if (req.query.idPaciente) {
-        let paciente = await PacienteCtr.findById(req.query.idPaciente);
-        if (paciente.idPacientePrincipal) {
-            paciente = await PacienteCtr.findById(paciente.idPacientePrincipal);
-        }
+        const paciente: any = await PacienteCtr.findById(req.query.idPaciente);
         if (paciente) {
             query.where('paciente.id').in(paciente.vinculos);
         }
@@ -623,10 +612,6 @@ router.patch('/prestaciones/:id', (req: Request, res, next) => {
                     if (req.body.estado.tipo === 'rechazada') {
                         data.solicitud.organizacion = req.body.organizacion;
                     }
-                    if (req.body.estado.tipo === 'auditoria') {
-                        delete data.solicitud.registros[0].valor.solicitudPrestacion.prioridad;
-                        data.solicitud.registros[0].markModified('valor');
-                    }
                     data.estados.push(req.body.estado);
                     if (req.body.estado.tipo === 'asignada') {
                         if (req.body.profesional) {
@@ -772,10 +757,6 @@ router.patch('/prestaciones/:id', (req: Request, res, next) => {
                     return console.error(errFrec);
                 });
 
-            }
-
-            if (req.body.estado?.tipo === 'auditoria') {
-                await prestacion.save();
             }
 
             if (req.body.op === 'romperValidacion') {
