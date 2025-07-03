58dbdda9aa85b0c2a1619db818bdc819f7b9e58b
PonjaBob <83436047+JuanIRamirez@users.noreply.github.com>
2025-07-03 09:21:40 -0300

    LAB - Actualizar solicitud para pacientes temporales (#2069)
    * LAB - Actualizar solicitud para pacientes temporales
    
    * correcciones
    
    ---------
    
    Co-authored-by: laucharin <negro89_nqn@hotmail.com>

--- modules/rup/routes/protocolosLab.ts
+++ modules/rup/routes/protocolosLab.ts
@@ -4,31 +4,29 @@ import * as laboratorioController from '../laboratorios.controller';
 
 const router = express.Router();
 
-router.get('/protocolosLab/:id?', async (req, res, next) => {
-    let dataSearch = {};
-    const service = 'get-LABAPI';
-    if (req.params.id) {
-        dataSearch = { idProtocolo: req.params.id };
-    } else {
-        dataSearch = {
-            estado: req.query.estado,
-            dni: req.query.dni,
-            fechaNac: req.query.fecNac,
-            apellido: req.query.apellido,
-            fechaDesde: req.query.fechaDde,
-            fechaHasta: req.query.fechaHta
-        };
+router.get('/protocolosLab', async (req, res, next) => {
+    if (!req.query.pacienteId) {
+        return next('Faltan parámetros requeridos');
     }
+    const response = await laboratorioController.searchByDocumento(req.query.pacienteId, req.query.fechaDesde, req.query.fechaHasta);
+    if (response.err) {
+        await laboratorioLog.error('busqueda-idPaciente', response.dataSearch, response.err, req);
+        return res.json([]);
+    } else {
+        return res.json(response);
+    }
+});
+
+router.get('/protocolosLab/:id?', async (req, res, next) => {
     try {
-        const response = await laboratorioController.search(dataSearch);
-        if (!response.length) {
-            throw new Error(response || service);
+        const response = await laboratorioController.search({ idProtocolo: req.params.id });
+        if (response) {
+            return res.json(response);
         }
-        res.json(response);
+        return next(404);
     } catch (err) {
-        dataSearch['id'] = req.params.id;
-        await laboratorioLog.error('resultado-protocolo', dataSearch, err, req);
-        res.json('error:' + err.message);
+        await laboratorioLog.error('busqueda-idProtocolo', { idProtocolo: req.params.id }, err, req);
+        res.json(err);
     }
 });
 
