ad651ad6766f297f195879f6efa5b6a4ee6c8eb2
silviroa <silviroa@gmail.com>
2025-07-16 17:08:16 -0300

    fix(RUP): eliminar recetas pendientes cuando se suspende la vigente del grupo de tratamiento prolongado

--- modules/rup/controllers/rup.events.ts
+++ modules/rup/controllers/rup.events.ts
@@ -35,55 +35,55 @@ EventCore.on('prestacion:receta:create', async (prestacion) => {
         for (const registro of registros) {
             if (conceptIds.includes(registro.concepto.conceptId)) {
                 for (const medicamento of registro.valor.medicamentos) {
-                        let receta: any = await Receta.findOne({
-                                'medicamento.concepto.conceptId': medicamento.generico.conceptId,
-                                idRegistro: registro._id
-                            });
-                        if (!receta) {
-                            const cantRecetas = medicamento.tratamientoProlongado ? parseInt(medicamento.tiempoTratamiento.id) : 1;
-                            for (let i = 0; i < cantRecetas; i++) {
+                    let receta: any = await Receta.findOne({
+                        'medicamento.concepto.conceptId': medicamento.generico.conceptId,
+                        idRegistro: registro._id
+                    });
+                    if (!receta) {
+                        const cantRecetas = medicamento.tratamientoProlongado ? parseInt(medicamento.tiempoTratamiento.id, 10) : 1;
+                        for (let i = 0; i < cantRecetas; i++) {
 
-                                try {
-                                    receta = new Receta();
-                                    receta.organizacion = organizacion;
-                                    receta.profesional = profesional;
-                                    receta.fechaRegistro = moment(prestacion.ejecucion.fecha).add(i * 30, 'days').toDate();
-                                    receta.fechaPrestacion = moment(prestacion.ejecucion.fecha).toDate();
-                                    receta.idPrestacion = idPrestacion;
-                                    receta.idRegistro = registro._id;
-                                    receta.diagnostico = medicamento.diagnostico;
-                                    receta.medicamento = {
-                                        concepto: medicamento.generico,
-                                        presentacion: medicamento.presentacion.term,
-                                        unidades: medicamento.unidades,
-                                        cantidad: medicamento.cantidad,
-                                        cantEnvases: medicamento.cantEnvases,
-                                        dosisDiaria: {
-                                            dosis: medicamento.dosisDiaria.dosis,
-                                            intervalo: medicamento.dosisDiaria.intervalo,
-                                            dias: medicamento.dosisDiaria.dias,
-                                            notaMedica: medicamento.dosisDiaria.notaMedica
-                                        },
-                                        tratamientoProlongado: medicamento.tratamientoProlongado,
-                                        tiempoTratamiento: medicamento.tiempoTratamiento,
-                                        ordenTratamiento: i,
-                                        tipoReceta: medicamento.tipoReceta?.id || medicamento.tipoReceta || 'simple',
-                                        serie: medicamento.serie,
-                                        numero: medicamento.numero
-                                    };
-                                    receta.estados = i < 1 ? [{ tipo: 'vigente' }] : [{ tipo: 'pendiente' }];
-                                    receta.estadoActual = i < 1 ? { tipo: 'vigente' } : { tipo: 'pendiente' };
-                                    receta.estadosDispensa = [{ tipo: 'sin-dispensa', fecha: moment().toDate() }];
-                                    receta.estadoDispensaActual = { tipo: 'sin-dispensa', fecha: moment().toDate() };
-                                    receta.paciente = prestacion.paciente;
-                                    receta.audit(prestacion.createdBy);
-                                    await receta.save();
-                                } catch (err) {
-                                    logger.error('prestacion:receta:create', prestacion, err);
-                                }
+                            try {
+                                receta = new Receta();
+                                receta.organizacion = organizacion;
+                                receta.profesional = profesional;
+                                receta.fechaRegistro = moment(prestacion.ejecucion.fecha).add(i * 30, 'days').toDate();
+                                receta.fechaPrestacion = moment(prestacion.ejecucion.fecha).toDate();
+                                receta.idPrestacion = idPrestacion;
+                                receta.idRegistro = registro._id;
+                                receta.diagnostico = medicamento.diagnostico;
+                                receta.medicamento = {
+                                    concepto: medicamento.generico,
+                                    presentacion: medicamento.presentacion.term,
+                                    unidades: medicamento.unidades,
+                                    cantidad: medicamento.cantidad,
+                                    cantEnvases: medicamento.cantEnvases,
+                                    dosisDiaria: {
+                                        dosis: medicamento.dosisDiaria.dosis,
+                                        intervalo: medicamento.dosisDiaria.intervalo,
+                                        dias: medicamento.dosisDiaria.dias,
+                                        notaMedica: medicamento.dosisDiaria.notaMedica
+                                    },
+                                    tratamientoProlongado: medicamento.tratamientoProlongado,
+                                    tiempoTratamiento: medicamento.tiempoTratamiento,
+                                    ordenTratamiento: i,
+                                    tipoReceta: medicamento.tipoReceta?.id || medicamento.tipoReceta || 'simple',
+                                    serie: medicamento.serie,
+                                    numero: medicamento.numero
+                                };
+                                receta.estados = i < 1 ? [{ tipo: 'vigente' }] : [{ tipo: 'pendiente' }];
+                                receta.estadoActual = i < 1 ? { tipo: 'vigente' } : { tipo: 'pendiente' };
+                                receta.estadosDispensa = [{ tipo: 'sin-dispensa', fecha: moment().toDate() }];
+                                receta.estadoDispensaActual = { tipo: 'sin-dispensa', fecha: moment().toDate() };
+                                receta.paciente = prestacion.paciente;
+                                receta.audit(prestacion.createdBy);
+                                await receta.save();
+                            } catch (err) {
+                                logger.error('prestacion:receta:create', prestacion, err);
                             }
                         }
-                    
+                    }
+
                 }
             }
         }
