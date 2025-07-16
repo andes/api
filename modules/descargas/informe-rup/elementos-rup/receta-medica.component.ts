1011aa2b1ff8675dd2aae299ab6945f4f8661212
silviroa <silviroa@gmail.com>
2025-07-16 14:27:59 -0300

    feat(RUP): Agrega codigo de barras al PDF de la receta (#2076)

--- modules/descargas/informe-rup/elementos-rup/receta-medica.component.ts
+++ modules/descargas/informe-rup/elementos-rup/receta-medica.component.ts
@@ -1,11 +1,27 @@
 import { HTMLComponent } from '../../model/html-component.class';
-
+import { generateBarcodeBase64 } from '../../model/barcode';
 export class RecetaMedicaComponent extends HTMLComponent {
     template = `
     <div class="nivel-1">
-    <p>
-        {{#if registro.valor.medicamentos}}<b>RECETA MÉDICA</b>{{/if}}
-    </p>
+     <table class="table" style="width:100%; border:0px none; table-layout:fixed;">
+     <tbody>
+        <tr>
+        <br>
+            <td style="width:30%; vertical-align:top;border:0px none;">
+                <p>
+                    {{#if registro.valor.medicamentos}}<b>RECETA MÉDICA</b>{{/if}}
+                </p>
+            </td>
+            <td style="width:70%; vertical-align:top; padding:0;border:0px none;">
+                <div class="barcode">
+                {{#if esReceta}}
+                    <img src="data:image/png;base64,{{barcodeBase64}}" alt="{{registro.id}}" />
+                     {{/if}}
+                </div>
+            </td>
+        </tr>
+     </tbody>
+     </table>
     </div>
     <table class="table table-bordered">
         <thead>
@@ -15,10 +31,10 @@ export class RecetaMedicaComponent extends HTMLComponent {
                 <th>Cantidad</th>
                 <th>Dosis diaria</th>
                 <th>Diagnóstico</th>
-                <th>Observaciones</th>
             </tr>
         </thead>
         <tbody>
+        <br>
             {{#each registro.valor.medicamentos}}
                 <tr>
                     <td> {{generico.term}} </td>
@@ -40,27 +56,28 @@ export class RecetaMedicaComponent extends HTMLComponent {
                     <td>
                     {{ diagnostico.term }}
                     </td>
-                    <td>
-                    {{#if tratamientoProlongado }}
-                        Tratamiento prolongado |
-                    {{/if}}
-                    {{#if tipoReceta }}
-                    {{ tipoReceta.nombre }}
-                      {{/if}}
-                    </td>
                 </tr>
             {{/each}}
+             <tr>
+                <td colspan="6" style="font-weight: bold;font-style: italic;">
+                    {{#if esReceta}}
+                Esta receta fue creada por emisor inscripto y valido en el Registro de Recetarios Electrónicos
+del Ministerio de Salud de la Nación - RL-2025-24026558-APN-SSVEIYES#MS
+{{/if}}
+                </td>
+            </tr>
         </tbody>
     </table>
-    `;
-
+    <br>`;
     constructor(private prestacion, private registro, private params, private depth) {
         super();
     }
 
     async process() {
         this.data = {
-            registro: this.registro
+            registro: this.registro,
+            esReceta: this.depth ? 1 : 0, // Si es 0 no muestra el código de barras
+            barcodeBase64: await generateBarcodeBase64(this.registro.id, 'code128')
         };
     }
 }
