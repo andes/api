d6a6db338956943921cd30375b035c6f9196ff35
Celeste <celeste.ramos@fi.uncoma.edu.ar>
2026-01-08 15:33:17 -0300

    fix(turnoSolicitado): cambia ruta get id en vez dni  y agrega controles al post (#2169)

--- modules/matriculaciones/routes/turnoSolicitado.ts
+++ modules/matriculaciones/routes/turnoSolicitado.ts
@@ -11,26 +11,34 @@ router.post('/turnoSolicitados', async (req, res, next) => {
     const sexo = req.body.sexo;
     try {
         const profesional = await Profesional.findOne({ documento: doc, sexo, profesionalMatriculado: false });
+        let solicitud = null;
         if (profesional?._id) {
             req.body._id = profesional._id;
             req.body.profesionalMatriculado = true;
+            solicitud = await turnoSolicitado.findOne({ _id: profesional._id });
         }
-        const newProfesional = new turnoSolicitado(req.body);
-        Auth.audit(newProfesional, userScheduler as any);
-        const saved = await newProfesional.save();
+        // genera otro documento
+        if (!solicitud) {
+            solicitud = new turnoSolicitado(req.body);
+        } else {
+            solicitud.set(req.body);
+        }
+        Auth.audit(solicitud, userScheduler as any);
+        const saved = await solicitud.save();
         res.json(saved);
     } catch (err) {
-        matriculacionLog.error('matriculaciones:turnoSolicitados', req.body, err);
+        matriculacionLog.error('matriculaciones:turnoSolicitados', req.body, err.errors);
         return next('error-turno');
     }
 });
 
-router.get('/turnoSolicitados/traePDni/:dni*?', async (req: any, res, next) => {
-    const dni = req.params.dni;
+router.get('/turnoSolicitados/:id', async (req: any, res, next) => {
+    const id = req.params.id;
     try {
-        const turno = await turnoSolicitado.find({ _id: dni });
+        const turno = await turnoSolicitado.find({ _id: id });
         res.json(turno[0]);
     } catch (err) {
+        matriculacionLog.error('turnoSolicitados:id', req.params.id, err.errors);
         return next(err);
     }
 });
