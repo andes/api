28ecdbbbb86ffefd566f272fb0718bea42d79b05
mcele <19591224+MCele@users.noreply.github.com>
2026-01-15 17:01:57 -0300

    feat(user): se actualiza fechas limite en job desactivarUsuarios

--- core/tm/controller/authUser.ts
+++ core/tm/controller/authUser.ts
@@ -1,27 +1,24 @@
 /**
  * Función que verifica la última vez que se actualizó/logueó un usuario.
- * Si es mayor o igual a 3 mes se desactiva,
+ * Si es mayor o igual a 3 meses se desactiva,
  * cambiando la variable activo que se encuentra en cada organización a false.
  */
 import * as moment from 'moment';
 import { AuthUsers } from '../../../auth/schemas/authUsers';
 
 export async function verificarUltimoLogueo(done) {
-    const usuarios: any = AuthUsers.find({ 'organizaciones.activo': true }).cursor({ batchSize: 100 });
+    const usuarios = AuthUsers.find({ 'organizaciones.activo': true }).cursor({ batchSize: 100 });
     const fLimite = moment().subtract(3, 'months').toDate();
     const fSinLogin = moment().subtract(4, 'months').toDate();
+    const esMenor = (fecha) => (fecha && fecha < fLimite);
     for await (const user of usuarios) {
         for (const org of user.organizaciones) {
-            if (!org.lastLogin && !user.lastLogin && !user.updatedAt && !user.createdAt) {
-                org.lastLogin = fSinLogin;
-            }
-
-            const lastUpdate = [org.lastLogin, user.lastLogin, user.updatedAt, user.createdAt]
-                .filter(date => date)
-                .sort((a, b) => moment(b).valueOf() - moment(a).valueOf())[0];
-
-            if (moment(lastUpdate).isBefore(fLimite)) {
+            const lastUpdate = org.updatedAt || org.createdAt || user.updatedAt || user.createdAt;
+            if (esMenor(lastUpdate) && esMenor(org.lastLogin)) {
                 org.activo = false;
+            }
+            if (!org.lastLogin && org.activo) {
+                org.lastLogin = user.lastLogin || lastUpdate || fSinLogin;
             }
         }
         await AuthUsers.updateOne(
