79b3c2bde5c5fcfe000645fc797ce0dcd880e25e
laucharin <negro89_nqn@hotmail.com>
2026-05-21 16:02:06 -0300

    invierte el ordenamiento de las notas segun lo pedido

--- core/tm/routes/profesional.ts
+++ core/tm/routes/profesional.ts
@@ -1094,7 +1094,7 @@ router.patch('/profesionales/:id?', Auth.authenticate(), async (req, res, next) 
                             usuario: req.body.agente,
                             fecha: new Date()
                         };
-                        resultado.notas.push(nota);
+                        resultado.notas.unshift(nota);
                     }
                     break;
                 case 'updateSancion':
