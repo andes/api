540fc8339c1a19b80b9835d0910387bdd6b8012d
nicolasarana <90768149+nicolasarana@users.noreply.github.com>
2026-06-30 10:03:04 -0300

    HUDS : Armar api o controladores para situaciones activas y antecedentes

--- modules/huds/index.ts
+++ modules/huds/index.ts
@@ -1,2 +1,3 @@
 export { HudsAccesosCtr } from './hudsAccesos.controller';
 export { HudsAccesoRouter } from './hudsAccesos.routes';
+export { HudsSituacionesRouter } from './hudsSituaciones.routes';
