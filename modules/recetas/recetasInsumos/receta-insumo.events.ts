405d6e0631a612225edf135b915d9e1b0e8653a9
mcele <19591224+MCele@users.noreply.github.com>
2026-07-24 11:29:14 -0300

    Fix de busqueda de profesional en insumos

--- modules/recetas/recetasInsumos/receta-insumo.events.ts
+++ modules/recetas/recetasInsumos/receta-insumo.events.ts
@@ -4,38 +4,45 @@ import { getProfesionActualizada } from '../../recetas/recetasController';
 import * as moment from 'moment';
 import { RecetaInsumo } from './receta-insumo.schema';
 import { createLog } from './../recetaLogs';
+import { Profesional } from '../../../core/tm/schemas/profesional';
 
 EventCore.on('prestacion:recetaInsumo:create', async ({ prestacion, registro }) => {
-    const idRegistro = registro._id;
-    const profPrestacion = prestacion.solicitud.profesional;
-    const { profesionGrado, matriculaGrado, especialidades } = await getProfesionActualizada(profPrestacion.id);
-    const profesional = {
-        id: profPrestacion.id,
-        nombre: profPrestacion.nombre,
-        apellido: profPrestacion.apellido,
-        documento: profPrestacion.documento,
-        profesion: profesionGrado,
-        especialidad: especialidades,
-        matricula: matriculaGrado
-    };
-
-    const organizacion = {
-        id: prestacion.ejecucion.organizacion.id,
-        nombre: prestacion.ejecucion.organizacion.nombre
-    };
-
-    const dataReceta = {
-        idPrestacion: prestacion.id,
-        idRegistro,
-        fechaRegistro: prestacion.ejecucion.fecha || moment().toDate(),
-        fechaPrestacion: prestacion.ejecucion.fecha,
-        paciente: prestacion.paciente,
-        profesional,
-        organizacion,
-        insumo: null,
-        diagnostico: null,
-    };
+    let dataReceta: any = {};
+    let profesional = {};
     try {
+        const idRegistro = registro._id;
+        const documentoProfesional = prestacion.estadoActual.createdBy?.documento ? prestacion.estadoActual.createdBy?.documento : prestacion.solicitud.profesional.documento;
+        const profPrestacion = await Profesional.findOne({ documento: documentoProfesional });
+        if (!profPrestacion) {
+            createLog.error('create', { dataReceta, prestacion, profesional: null }, null, { prestacion, registro });
+            return;
+        }
+        const { profesionGrado, matriculaGrado, especialidades } = await getProfesionActualizada(profPrestacion.id);
+        profesional = {
+            id: profPrestacion.id,
+            nombre: profPrestacion.nombre,
+            apellido: profPrestacion.apellido,
+            documento: profPrestacion.documento,
+            profesion: profesionGrado,
+            especialidad: especialidades,
+            matricula: matriculaGrado
+        };
+        const organizacion = {
+            id: prestacion.ejecucion.organizacion.id,
+            nombre: prestacion.ejecucion.organizacion.nombre
+        };
+        dataReceta = {
+            idPrestacion: prestacion.id,
+            idRegistro,
+            fechaRegistro: prestacion.ejecucion.fecha || moment().toDate(),
+            fechaPrestacion: prestacion.ejecucion.fecha,
+            paciente: prestacion.paciente,
+            profesional,
+            organizacion,
+            insumo: null,
+            diagnostico: null,
+        };
+
         for (const insumo of registro.valor.insumos) {
             const receta: any = await RecetaInsumo.findOne({
                 'insumo.id': insumo.generico.id,
@@ -44,7 +51,7 @@ EventCore.on('prestacion:recetaInsumo:create', async ({ prestacion, registro }) 
             });
             if (!receta) {
                 dataReceta.insumo = insumo;
-                await crearRecetaInsumo(dataReceta, prestacion.createdBy); // falta return
+                await crearRecetaInsumo(dataReceta, prestacion.createdBy);
             }
 
         }
