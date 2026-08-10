196c6520364bdd98d7775a967f98608398a70953
laucharin <negro89_nqn@hotmail.com>
2026-08-10 13:15:25 -0300

    reemplazo de node-sass por sass

--- modules/descargas/model/html-component.class.ts
+++ modules/descargas/model/html-component.class.ts
@@ -14,8 +14,8 @@ export abstract class HTMLComponent {
         await this.process();
         const templateHTML = this.getTemplate();
         const template = handlebars.compile(templateHTML);
-        const html = template(this.data);
-        return html;
+        const data = this.toPlainObject(this.data);
+        return template(data);
     }
 
     private getTemplate() {
@@ -26,6 +26,39 @@ export abstract class HTMLComponent {
         } else {
             throw new Error('no template');
         }
+    }
+
+    private toPlainObject(value: any): any {
+        if (value === null || value === undefined) {
+            return value;
+        }
+
+        if (value instanceof Date) {
+            return value;
+        }
+
+        if (Array.isArray(value)) {
+            return value.map(item => this.toPlainObject(item));
+        }
+
+        if (typeof value.toObject === 'function') {
+            return this.toPlainObject(value.toObject({
+                getters: true,
+                virtuals: true
+            }));
+        }
+
+        if (typeof value === 'object') {
+            const result: any = {};
+
+            Object.keys(value).forEach(key => {
+                result[key] = this.toPlainObject(value[key]);
+            });
+
+            return result;
+        }
+
+        return value;
     }
 
     public async process() { }
