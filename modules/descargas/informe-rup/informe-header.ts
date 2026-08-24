2a23fc375926a7c0785bbe8b4110073f58c653d4
Fabio-Ramirez <fabioadiranr2@gmail.com>
2026-08-24 10:26:35 -0300

    Se revierte cambio, para respetar la edad al momento de la prestacion

--- modules/descargas/informe-rup/informe-header.ts
+++ modules/descargas/informe-rup/informe-header.ts
@@ -159,7 +159,8 @@ export class InformeRupHeader extends HTMLComponent {
         // [TODO] helpers date formats en Handlerbars
 
         const fechaNacimiento = paciente.fechaNacimiento ? moment(paciente.fechaNacimiento).format('DD/MM/YYYY') : 's/d';
-        const edad = paciente.fechaNacimiento && moment().diff(moment(paciente.fechaNacimiento), 'years');
+        const fechaPrestacion = moment(prestacion.ejecucion.fecha);
+        const edad = paciente.fechaNacimiento && fechaPrestacion.diff(moment(paciente.fechaNacimiento), 'years');
         const organizacionId = String(prestacion.ejecucion.organizacion.id);
         const origenTop = (prestacion.inicio === 'top');
         const solicitudOrigen = prestacion.solicitud;
