623f0256a559a26ab31d8535e68e6a6cdf7d91b7
laucharin <negro89_nqn@hotmail.com>
2026-09-11 13:10:38 -0300

    elimina condicion redundante

--- modules/rup/internacion/camas.routes.ts
+++ modules/rup/internacion/camas.routes.ts
@@ -178,30 +178,30 @@ router.patch('/camaEstados/:idCama', Auth.authenticate(), capaMiddleware, asyncH
                 },
                 { arrayFilters: [{ 'elemento.valor.informeIngreso.fechaIngreso': moment(req.body.fechaIngreso).toDate() }] }
             );
-            if (edicionFinanciador) {
-                result = await CamaEstados.update(
-                    {
-                        'estados.idInternacion': Types.ObjectId(req.body.idInternacion)
-                    },
-                    {
-                        $set: {
-                            'estados.$[elemento].paciente.obraSocial': req.body.paciente.obraSocial
-                        }
-                    },
-                    {
-                        arrayFilters: [{ 'elemento.idInternacion': Types.ObjectId(req.body.idInternacion) }],
-                        multi: true
+            result = await CamaEstados.update(
+                {
+                    'estados.idInternacion': Types.ObjectId(req.body.idInternacion)
+                },
+                {
+                    $set: {
+                        'estados.$[elemento].paciente.obraSocial': req.body.paciente.obraSocial
                     }
-                );
-            }
-        }
-        if (!edicionFinanciador) {
-
+                },
+                {
+                    arrayFilters: [{ 'elemento.idInternacion': Types.ObjectId(req.body.idInternacion) }],
+                    multi: true
+                }
+            );
+        } else {
             const organizacion = {
                 _id: Auth.getOrganization(req),
                 nombre: Auth.getOrganization(req, 'nombre')
             };
-            const data = { ...req.body, organizacion, id: req.params.idCama };
+            const data = {
+                ...req.body,
+                organizacion,
+                id: req.params.idCama
+            };
             result = await CamasController.patchEstados(data, req);
         }
     } catch (err) {
