151edb15c9d6065afdc0c183d911e085d90700b0
PonjaBob <83436047+JuanIRamirez@users.noreply.github.com>
2026-09-18 09:06:05 -0300

    FIX - A la fechaHora de fin de turno Sumar 8 hs (#2311)
    de la fechaHora del inicio del turno.

--- modules/webex/webex.controller.ts
+++ modules/webex/webex.controller.ts
@@ -8,19 +8,16 @@ export async function generateWebexLinks(turno: any, agenda: any, usuario: strin
         const params = {
             idTurno: String(turno._id),
             fechaHoraDesde: Math.floor(turno.horaInicio.getTime() / 1000.0),
-            fechaHoraHasta: Math.floor((agenda.horaFin.getTime() + 60 * 60 * 1000) / 1000)
+            fechaHoraHasta: Math.floor((turno.horaInicio.getTime() + 60 * 60 * 1000 * 8) / 1000)
         };
-
         const servicio = services.get('teleconsulta');
         const result: any = await servicio.exec(params);
-
         if (result && result.baseUrl && result.host?.[0] && result.guest?.[0]) {
             return {
                 professionalLink: result.baseUrl + result.host[0].short,
                 patientLink: result.baseUrl + result.guest[0].short,
             };
         }
-
         return null;
     } catch (error) {
         dbgWebex('Error generating Webex links: %O', error?.message || error);
